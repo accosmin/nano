bc3c9cabbe486e0a74a706fe0f95a939e6639f05
catanasoaei <accosmin@gmail.com>
2013-07-02 21:16:26 +0200

    test program to show all the registered objects

---
+++ app/app_ncv_info_object.cpp
@@ -0,0 +1,36 @@
+#include "ncv.h"
+
+template
+<
+        typename tobject
+>
+void print(const ncv::string_t& name, const ncv::manager_t<tobject>& manager)
+{
+        const ncv::strings_t ids = manager.ids();
+        const ncv::strings_t descriptions = manager.descriptions();
+
+        std::cout << ncv::string_t(120, '-') << std::endl;
+        std::cout << ncv::text::resize(name + " id", 16, ncv::align::left)
+                  << ncv::text::resize(name + " description", 64, ncv::align::left) << std::endl;
+
+        std::cout << ncv::string_t(120, '=') << std::endl;
+        for (ncv::size_t i = 0; i < ids.size(); i ++)
+        {
+                std::cout << ncv::text::resize(ids[i], 16) << descriptions[i] << std::endl;
+        }
+        std::cout << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+        ncv::init();
+
+        print("loss",           ncv::loss_manager_t::instance());
+        print("activation",     ncv::activation_manager_t::instance());
+        print("task",           ncv::task_manager_t::instance());
+        print("model",          ncv::model_manager_t::instance());
+
+        // OK
+        ncv::log_info() << ncv::done;
+        return EXIT_SUCCESS;
+}
