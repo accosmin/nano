34943e9ab617e9417a852bdbd0eebe4c39c371b9
accosmin <accosmin@gmail.com>
2013-12-03 21:08:28 +0100

    improved gcc debug options, generic trainer script

--- stochastic.sh
+++
@@ -1,7 +0,0 @@
-params=""
-params=${params}" --task mnist --task-dir /home/cosmin/experiments/databases/mnist/"
-#params=${params}" --loss classnll --model forward-network --model-params conv8x8:convs=16;snorm;conv8x8:convs=8;snorm;conv8x8:convs=8;snorm"
-params=${params}" --loss classnll --model forward-network"
-params=${params}" --trainer stochastic --trainer-params iters=8096,depth=16 --trials 1"
-
-./build/ncv_trainer ${params}
---
+++ trainer.sh
@@ -0,0 +1,15 @@
+model="--model forward-network"
+model="--model forward-network --model-params conv8x8:convs=16;snorm"
+model="--model forward-network --model-params conv8x8:convs=16;snorm;conv8x8:convs=8;snorm"
+model="--model forward-network --model-params conv8x8:convs=16;snorm;conv8x8:convs=8;snorm;conv8x8:convs=8;snorm"
+
+trainer="--trainer batch --trainer-params opt=lbfgs,iters=1024,eps=1e-6"
+trainer="--trainer minibatch --trainer-params opt=cgd,iters=2,batch=1024,epoch=256"
+trainer="--trainer stochastic --trainer-params iters=8096,depth=16"
+
+params=""
+params=${params}" --task mnist --task-dir /home/cosmin/experiments/databases/mnist/"
+params=${params}" --loss classnll --trials 1"
+params=${params}" ${trainer} ${model}"
+
+./build/ncv_trainer ${params}
