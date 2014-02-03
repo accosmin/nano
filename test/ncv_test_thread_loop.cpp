e2e62ba8ad0bd13ca2a3584d06b9cf87dedc25fd
Cosmin Atanasoaei <catanasoaei>
2014-02-03 19:40:07 +0100

    remove openmp dependency

--- test/ncv_test_thread_loop.cpp
+++ test/ncv_test_thread_loop.cpp
@@ -1,5 +1,4 @@
 #include "ncv.h"
-#include <omp.h>
 
 // run loop for the given number of trials using no threads
 template
@@ -18,31 +17,7 @@ ncv::stats_t<ncv::scalar_t> test(int size, int trials, toperator op)
                         op(i);
                 }
 
-                timings.add(timer.miliseconds());
-        }
-
-        return timings;
-}
-
-// run loop for the given number of trials using OpenMP
-template
-<
-        typename toperator
->
-ncv::stats_t<ncv::scalar_t> test_omp(int size, int trials, toperator op)
-{
-        ncv::stats_t<ncv::scalar_t> timings;
-        for (int t = 0; t < trials; t ++)
-        {
-                const ncv::timer_t timer;
-
-                #pragma omp parallel for
-                for (int i = 0; i < size; i ++)
-                {
-                        op(i);
-                }
-
-                timings.add(timer.miliseconds());
+                timings(timer.miliseconds());
         }
 
         return timings;
@@ -62,7 +37,7 @@ ncv::stats_t<ncv::scalar_t> test_ncv(int size, int trials, toperator op)
 
                 ncv::thread_loop(size, op);
 
-                timings.add(timer.miliseconds());
+                timings(timer.miliseconds());
         }
 
         return timings;
@@ -78,14 +53,12 @@ ncv::string_t to_string(const ncv::stats_t<ncv::scalar_t>& timings, ncv::size_t 
 // display the formatted timing statistics
 void print(const ncv::string_t& header,
            const ncv::stats_t<ncv::scalar_t>& timings,
-           const ncv::stats_t<ncv::scalar_t>& timings_omp,
            const ncv::stats_t<ncv::scalar_t>& timings_ncv)
 {
         static const ncv::size_t col_size = 32;
 
         std::cout << ncv::text::resize(header, col_size)
                   << to_string(timings, col_size)
-                  << to_string(timings_omp, col_size)
                   << to_string(timings_ncv, col_size)
                   << std::endl;
 }
@@ -98,9 +71,7 @@ int main(int argc, char *argv[])
                         1000,
                         10000,
                         100000,
-                        1000000,
-                        10000000,
-                        100000000
+                        1000000
                       };
 
         // test for different problems size
@@ -127,15 +98,11 @@ int main(int argc, char *argv[])
                 std::cout << "sum = " << std::accumulate(std::begin(results), std::end(results), 0.0) << std::endl;
 
                 std::fill(std::begin(results), std::end(results), 0.0);
-                const ncv::stats_t<ncv::scalar_t> timings_omp = test_omp(size, 16, op);
-                std::cout << "sum = " << std::accumulate(std::begin(results), std::end(results), 0.0) << std::endl;
-
-                std::fill(std::begin(results), std::end(results), 0.0);
                 const ncv::stats_t<ncv::scalar_t> timings_ncv = test_ncv(size, 16, op);
                 std::cout << "sum = " << std::accumulate(std::begin(results), std::end(results), 0.0) << std::endl;
 
                 print("test1 [" + ncv::text::to_string(size) + "]",
-                      timings, timings_omp, timings_ncv);
+                      timings, timings_ncv);
         });
 
         // OK
