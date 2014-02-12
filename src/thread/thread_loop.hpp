1122fdfd8ed47753246157e70b3ba25fcf628045
Cosmin Atanasoaei <catanasoaei>
2014-02-12 21:07:56 +0100

    reverted thread_loop implementation (still heavier than openmp)

--- src/thread/thread_loop.hpp
+++ src/thread/thread_loop.hpp
@@ -15,16 +15,15 @@ namespace ncv
         >
         void thread_loop(tsize N, toperator op, thread_pool_t& pool)
         {
-                const tsize n_workers = static_cast<tsize>(pool.n_workers());
-                const tsize task_size = std::max(tsize(1), N / n_workers / 16);
+                const tsize n_tasks = static_cast<tsize>(pool.n_workers());
 
-                for (tsize i = 0; i < N; i += task_size)
+                for (tsize t = 0; t < n_tasks; t ++)
                 {
                         pool.enqueue([=,&op]()
                         {
-                                for (size_t t = 0; t + i < N; t ++)
+                                for (tsize i = t; i < N; i += n_tasks)
                                 {
-                                        op(t + i);
+                                        op(i);
                                 }
                         });
                 }
