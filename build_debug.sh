2740f9a50f7c645992e7eaaaab14493ac48af5a3
Cosmin Atanasoaei <catanasoaei>
2014-04-09 21:39:12 +0200

    explicit OpenCL-support toggling in build scripts

--- build_debug.sh
+++ build_debug.sh
@@ -1,7 +1,7 @@
 mkdir -p ./build-debug
 cd ./build-debug/
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug ../
+cmake -DCMAKE_BUILD_TYPE=Debug -DNANOCV_HAVE_OPENCL=FALSE ../
 make -j 8
 cd ..
 
--- build_release.sh
+++ build_release.sh
@@ -1,7 +1,7 @@
 mkdir -p ./build-release
 cd ./build-release/
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Release -DNANOCV_HAVE_OPENCL=TRUE ../
+cmake -DCMAKE_BUILD_TYPE=Release -DNANOCV_HAVE_OPENCL=FALSE ../
 make -j 8
 cd ..
 
