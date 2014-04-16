a2560dbd50affc85c132d177a24b2ac4ae784a51
Cosmin Atanasoaei <catanasoaei>
2014-04-16 22:38:55 +0200

    OpenCL support by default on release

--- build_release.sh
+++ build_release.sh
@@ -1,7 +1,7 @@
 mkdir -p ./build-release
 cd ./build-release/
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Release -DNANOCV_HAVE_OPENCL=OFF ../
+cmake -DCMAKE_BUILD_TYPE=Release -DNANOCV_HAVE_OPENCL=ON ../
 make -j 8
 cd ..
 
