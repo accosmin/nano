72212822e8d7a1253c13ff95d1c0296b68903095
Cosmin Atanasoaei <accosmin@pix4d.com>
2014-05-06 16:52:33 +0200

    faster convolution

--- src/common/convolution.hpp
+++ src/common/convolution.hpp
@@ -7,6 +7,30 @@ namespace ncv
 {
         namespace math
         {
+                namespace detail
+                {
+                       ///
+                        /// 2D convolution: odata += idata @ kdata (using Eigen 2D blocks)
+                        ///
+                        template
+                        <
+                                int trows, int tcols,
+                                typename tmatrix,
+                                typename tscalar = typename tmatrix::Scalar
+                        >
+                        void conv(const tmatrix& idata, const tmatrix& kdata, tmatrix& odata)
+                        {
+                                for (auto r = 0; r < odata.rows(); r ++)
+                                {
+                                        for (auto c = 0; c < odata.cols(); c ++)
+                                        {
+                                                odata(r, c) +=
+                                                kdata.cwiseProduct(idata.block(r, c, trows, tcols)).sum();
+                                        }
+                                }
+                        } 
+                }
+                
                 ///
                 /// 2D convolution: odata += idata @ kdata (using Eigen 2D blocks)
                 ///
@@ -17,6 +41,22 @@ namespace ncv
                 >
                 void conv(const tmatrix& idata, const tmatrix& kdata, tmatrix& odata)
                 {
+                        if (kdata.rows() == kdata.cols())
+                        {
+                                if (kdata.rows() == 4) { detail::conv<4, 4>(idata, kdata, odata); return; }
+                                else if (kdata.rows() == 5) { detail::conv<5, 5>(idata, kdata, odata); return; }
+                                else if (kdata.rows() == 6) { detail::conv<6, 6>(idata, kdata, odata); return; }
+                                else if (kdata.rows() == 7) { detail::conv<7, 7>(idata, kdata, odata); return; }
+                                else if (kdata.rows() == 8) { detail::conv<8, 8>(idata, kdata, odata); return; }
+                                else if (kdata.rows() == 9) { detail::conv<9, 9>(idata, kdata, odata); return; }
+                                else if (kdata.rows() == 10) { detail::conv<10, 10>(idata, kdata, odata); return; }
+                                else if (kdata.rows() == 11) { detail::conv<11, 11>(idata, kdata, odata); return; }
+                                else if (kdata.rows() == 12) { detail::conv<12, 12>(idata, kdata, odata); return; }
+                                else if (kdata.rows() == 13) { detail::conv<13, 13>(idata, kdata, odata); return; }
+                                else if (kdata.rows() == 14) { detail::conv<14, 14>(idata, kdata, odata); return; }
+                                else if (kdata.rows() == 15) { detail::conv<15, 15>(idata, kdata, odata); return; }
+                        }
+                                
                         for (auto r = 0; r < odata.rows(); r ++)
                         {
                                 for (auto c = 0; c < odata.cols(); c ++)
@@ -25,6 +65,7 @@ namespace ncv
                                         kdata.cwiseProduct(idata.block(r, c, kdata.rows(), kdata.cols())).sum();
                                 }
                         }
+                        
                 }
 
                 ///
