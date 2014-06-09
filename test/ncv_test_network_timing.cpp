6f9f114d46f8068d4910da5fc5482470aceed1d1
accosmin <accosmin@gmail.com>
2014-06-09 21:38:31 +0200

    update test program with convolution networks

--- test/ncv_test_network_timing.cpp
+++ test/ncv_test_network_timing.cpp
@@ -58,14 +58,21 @@ int main(int argc, char *argv[])
         const string_t lmodel3 = lmodel2 + "linear:dims=64;snorm;";
         const string_t lmodel4 = lmodel3 + "linear:dims=64;snorm;";
         const string_t lmodel5 = lmodel4 + "linear:dims=64;snorm;";
-
-        const string_t cmodel0;
-        const string_t cmodel1 = cmodel0 + "conv:dims=16,rows=8,cols=8;snorm;";
-        const string_t cmodel2 = cmodel1 + "conv:dims=16,rows=7,cols=7;snorm;";
-        const string_t cmodel3 = cmodel2 + "conv:dims=16,rows=6,cols=6;snorm;";
-        const string_t cmodel4 = cmodel3 + "conv:dims=16,rows=5,cols=5;snorm;";
-        const string_t cmodel5 = cmodel4 + "conv:dims=16,rows=4,cols=4;snorm;";
-        const string_t cmodel6 = cmodel5 + "conv:dims=16,rows=3,cols=3;snorm;";
+        
+        string_t cmodel1;
+        cmodel1 = cmodel1 + "conv:dims=16,rows=7,cols=7;snorm;pool-abs;";
+        cmodel1 = cmodel1 + "conv:dims=16,rows=4,cols=4;snorm;pool-abs;";
+        cmodel1 = cmodel1 + "conv:dims=16,rows=4,cols=4;snorm;";
+        
+        string_t cmodel2;
+        cmodel2 = cmodel2 + "conv:dims=16,rows=7,cols=7;snorm;pool-abs;";
+        cmodel2 = cmodel2 + "conv:dims=16,rows=6,cols=6;snorm;pool-abs;";
+        cmodel2 = cmodel2 + "conv:dims=16,rows=3,cols=3;snorm;";
+        
+        string_t cmodel3;
+        cmodel3 = cmodel3 + "conv:dims=16,rows=5,cols=5;snorm;pool-abs;";
+        cmodel3 = cmodel3 + "conv:dims=16,rows=5,cols=5;snorm;pool-abs;";
+        cmodel3 = cmodel3 + "conv:dims=16,rows=4,cols=4;snorm;";
         
         const string_t outlayer = "linear:dims=" + text::to_string(cmd_outputs) + ";softmax:type=global;";
 
@@ -77,13 +84,10 @@ int main(int argc, char *argv[])
                 lmodel3 + outlayer,
                 lmodel4 + outlayer,
                 lmodel5 + outlayer
-//                ,
-//                cmodel1 + outlayer,
-//                cmodel2 + outlayer,
-//                cmodel3 + outlayer,
-//                cmodel4 + outlayer,
-//                cmodel5 + outlayer,
-//                cmodel6 + outlayer
+                ,
+                cmodel1 + outlayer,
+                cmodel2 + outlayer,
+                cmodel3 + outlayer
         };
 
         const rloss_t rloss = loss_manager_t::instance().get("class-ratio");
