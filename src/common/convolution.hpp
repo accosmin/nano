16b97e7f3f040f77774d12f7c797c7a83d3cce7c
accosmin <accosmin@gmail.com>
2014-06-10 21:28:50 +0200

    conv & conv_sum implementations

--- src/common/convolution.hpp
+++ src/common/convolution.hpp
@@ -17,13 +17,36 @@ namespace ncv
                         typename tmatrixo = tmatrixi,
                         typename tscalar = typename tmatrixi::Scalar
                 >
-                void conv(const tmatrixi& idata, const tmatrixk& kdata, tmatrixo& odata)
+                void conv_sum(const tmatrixi& idata, const tmatrixk& kdata, tmatrixo& odata)
                 {
                         for (auto r = 0; r < odata.rows(); r ++)
                         {
                                 for (auto c = 0; c < odata.cols(); c ++)
                                 {
                                         odata(r, c) +=
+                                        kdata.cwiseProduct(idata.block(r, c, kdata.rows(), kdata.cols())).sum();
+                                }
+                        }
+                        
+                }
+                
+                ///
+                /// 2D convolution: odata = idata @ kdata (using Eigen 2D blocks)
+                ///
+                template
+                <
+                        typename tmatrixi,
+                        typename tmatrixk = tmatrixi,
+                        typename tmatrixo = tmatrixi,
+                        typename tscalar = typename tmatrixi::Scalar
+                >
+                void conv(const tmatrixi& idata, const tmatrixk& kdata, tmatrixo& odata)
+                {
+                        for (auto r = 0; r < odata.rows(); r ++)
+                        {
+                                for (auto c = 0; c < odata.cols(); c ++)
+                                {
+                                        odata(r, c) =
                                         kdata.cwiseProduct(idata.block(r, c, kdata.rows(), kdata.cols())).sum();
                                 }
                         }
@@ -40,7 +63,7 @@ namespace ncv
                         typename tmatrixo = tmatrixi,
                         typename tscalar = typename tmatrixi::Scalar
                 >
-                void wconv(const tmatrixi& idata, const tmatrixk& kdata, tscalar weight, tmatrixo& odata)
+                void wconv_sum(const tmatrixi& idata, const tmatrixk& kdata, tscalar weight, tmatrixo& odata)
                 {
                         for (auto r = 0; r < odata.rows(); r ++)
                         {
@@ -50,7 +73,29 @@ namespace ncv
                                         kdata.cwiseProduct(idata.block(r, c, kdata.rows(), kdata.cols())).sum();
                                 }
                         }
-                }                
+                }        
+                
+                ///
+                /// 2D convolution: odata = weight * idata @ kdata (using Eigen 2D blocks)
+                ///
+                template
+                <
+                        typename tmatrixi,
+                        typename tmatrixk = tmatrixi,
+                        typename tmatrixo = tmatrixi,
+                        typename tscalar = typename tmatrixi::Scalar
+                >
+                void wconv(const tmatrixi& idata, const tmatrixk& kdata, tscalar weight, tmatrixo& odata)
+                {
+                        for (auto r = 0; r < odata.rows(); r ++)
+                        {
+                                for (auto c = 0; c < odata.cols(); c ++)
+                                {
+                                        odata(r, c) = weight *
+                                        kdata.cwiseProduct(idata.block(r, c, kdata.rows(), kdata.cols())).sum();
+                                }
+                        }
+                }       
         }
 }
 
