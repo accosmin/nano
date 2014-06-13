a9514b6c265cc983506b594804b7dffac339906d
Cosmin Atanasoaei <accosmin@pix4d.com>
2014-06-13 11:17:07 +0200

    better formated object description

--- apps/ncv_info.cpp
+++ apps/ncv_info.cpp
@@ -1,24 +1,26 @@
 #include "nanocv.h"
 
+using namespace ncv;
+
 template
 <
         typename tobject
 >
-void print(const ncv::string_t& name, const ncv::manager_t<tobject>& manager)
+void print(const string_t& name, const manager_t<tobject>& manager)
 {
-        const ncv::strings_t ids = manager.ids();
-        const ncv::strings_t descriptions = manager.descriptions();
+        const strings_t ids = manager.ids();
+        const strings_t descriptions = manager.descriptions();
 
-        std::cout << ncv::string_t(120, '-') << std::endl;
-        std::cout << ncv::text::resize(name + " id", 16, ncv::align::left)
-                  << ncv::text::resize(name + " description", 64, ncv::align::left) << std::endl;
+        std::cout << string_t(120, '-') << std::endl;
+        std::cout << text::resize(name + " id", 24, align::left)
+                  << text::resize(name + " description", 64, align::left) << std::endl;
 
-        std::cout << ncv::string_t(120, '=') << std::endl;
-        for (ncv::size_t i = 0; i < ids.size(); i ++)
+        std::cout << string_t(120, '=') << std::endl;
+        for (size_t i = 0; i < ids.size(); i ++)
         {
-                std::cout << ncv::text::resize(ids[i], 16) << descriptions[i] << std::endl;
+                std::cout << text::resize(ids[i], 24) << descriptions[i] << std::endl;
         }
-        std::cout << std::endl;
+        std::cout << std::endl << std::endl;
 }
 
 int main(int argc, char *argv[])
