5f6b96b28d748bc8ebbb63c4ed98488af2e2b8f8
accosmin <accosmin@gmail.com>
2014-07-17 20:33:01 +0200

    cleanup build scripts & use ninja by default

--- build_debug.sh
+++ build_debug.sh
@@ -1,8 +1,15 @@
 mkdir -p ./build-debug
 cd ./build-debug/
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Debug -DNANOCV_HAVE_OPENCL=OFF -G "Unix Makefiles" ../
-make -j 8
+
+cmake_params=""
+cmake_params=${cmake_params}" -DCMAKE_BUILD_TYPE=Debug"
+cmake_params=${cmake_params}" -DNANOCV_HAVE_OPENCL=OFF"
+cmake_params=${cmake_params}" -G Ninja"
+
+cmake ${cmake_params} ../
+
+ninja -j 8
 cd ..
 
 
--- build_release.sh
+++ build_release.sh
@@ -1,8 +1,15 @@
 mkdir -p ./build-release
 cd ./build-release/
 rm -rf *
-cmake -DCMAKE_BUILD_TYPE=Release -DNANOCV_HAVE_OPENCL=OFF -G "Unix Makefiles" ../
-make -j 8
+
+cmake_params=""
+cmake_params=${cmake_params}" -DCMAKE_BUILD_TYPE=Release"
+cmake_params=${cmake_params}" -DNANOCV_HAVE_OPENCL=OFF"
+cmake_params=${cmake_params}" -G Ninja"
+
+cmake ${cmake_params} ../
+
+ninja -j 8
 cd ..
 
 
