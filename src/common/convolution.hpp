ef220f867533445dbd7b7ab72749536a74dabaa9
accosmin <accosmin@gmail.com>
2014-07-30 21:39:09 +0200

    split dot products

--- src/common/convolution.hpp
+++ src/common/convolution.hpp
@@ -1,8 +1,8 @@
 #ifndef NANOCV_CONVOLUTION_H
 #define NANOCV_CONVOLUTION_H
 
-#include <eigen3/Eigen/Core>
 #include <functional>
+#include "dot.hpp"
 
 namespace ncv
 {
@@ -75,44 +75,6 @@ namespace ncv
 
                 namespace detail
                 {
-                        ///
-                        /// \brief general dot-product
-                        ///
-                        template
-                        <
-                                typename tscalar,
-                                typename tsize
-                        >
-                        tscalar dot(const tscalar* vec1, const tscalar* vec2, tsize size)
-                        {
-                                tscalar sum = 0;
-                                for (auto i = 0; i < size; i ++)
-                                {
-                                        sum += vec1[i] * vec2[i];
-                                }
-
-                                return sum;
-                        }
-
-                        ///
-                        /// \brief fixed-size dot-product
-                        ///
-                        template
-                        <
-                                typename tscalar,
-                                int tsize
-                        >
-                        tscalar dot(const tscalar* vec1, const tscalar* vec2)
-                        {
-                                tscalar sum = 0;
-                                for (auto i = 0; i < tsize; i ++)
-                                {
-                                        sum += vec1[i] * vec2[i];
-                                }
-
-                                return sum;
-                        }
-
                         ///
                         /// \brief 2D convolution using a dot-operator
                         ///
---
+++ src/common/dot.hpp
@@ -0,0 +1,49 @@
+#ifndef NANOCV_DOT_H
+#define NANOCV_DOT_H
+
+namespace ncv
+{
+        namespace math
+        {
+                ///
+                /// \brief general dot-product
+                ///
+                template
+                <
+                        typename tscalar,
+                        typename tsize
+                >
+                tscalar dot(const tscalar* vec1, const tscalar* vec2, tsize size)
+                {
+                        tscalar sum = 0;
+                        for (auto i = 0; i < size; i ++)
+                        {
+                                sum += vec1[i] * vec2[i];
+                        }
+
+                        return sum;
+                }
+
+                ///
+                /// \brief fixed-size dot-product
+                ///
+                template
+                <
+                        typename tscalar,
+                        int tsize
+                >
+                tscalar dot(const tscalar* vec1, const tscalar* vec2)
+                {
+                        tscalar sum = 0;
+                        for (auto i = 0; i < tsize; i ++)
+                        {
+                                sum += vec1[i] * vec2[i];
+                        }
+
+                        return sum;
+                }
+        }
+}
+
+#endif // NANOCV_DOT_H
+
