e25eb7237ec75b964f9496b470e19ae6ac191f1f
Cosmin Atanasoaei <accosmin@pix4d.com>
2014-08-05 09:42:26 +0200

    criterion interface

---
+++ src/nanocv/criterion.cpp
@@ -0,0 +1,163 @@
+#include "criterion.h"
+#include "loss.h"
+#include "task.h"
+#include <cassert>
+
+namespace ncv
+{        
+        criterion_t::criterion_t(
+                const std::string& configuration,
+                const std::string& description)
+                :       clonable_t<criterion_t>(configuration, description),
+                        m_lambda(0.0),
+                        m_type(type::value),
+                        m_value(0.0),
+                        m_error(0.0),
+                        m_count(0)
+        {
+        }
+
+        criterion_t& criterion_t::reset(const rmodel_t& rmodel)
+        {
+                assert(rmodel);
+                return reset(*rmodel);
+        }
+
+        criterion_t& criterion_t::reset(const model_t& model)
+        {
+                m_model = model.clone();
+                m_params = model.params();
+                reset();
+                return *this;
+        }
+
+        criterion_t& criterion_t::reset(const vector_t& params)
+        {
+                assert(m_model->psize() == params.size());
+                m_model->load_params(params);
+                m_params = params;
+                reset();
+                return *this;
+        }
+
+        criterion_t& criterion_t::reset(type t)
+        {
+                m_type = t;
+                return *this;
+        }
+
+        criterion_t& criterion_t::reset(scalar_t lambda)
+        {
+                m_lambda = lambda;
+                return *this;
+        }
+
+        criterion_t& criterion_t::reset()
+        {
+                m_value = 0.0;
+                m_error = 0.0;
+                m_vgrad.resize(dimensions());
+                m_vgrad.setZero();
+                m_count = 0;
+                return *this;
+        }
+
+        criterion_t& criterion_t::operator+=(const criterion_t& other)
+        {
+                m_value += other.m_value;
+                m_vgrad += other.m_vgrad;
+                m_error += other.m_error;
+                m_count += other.m_count;
+                return *this;
+        }
+
+        void criterion_t::cumulate(
+                const vector_t& output, const vector_t& target, const loss_t& loss)
+        {
+                assert(static_cast<size_t>(output.size()) == m_model->osize());
+                assert(static_cast<size_t>(target.size()) == m_model->osize());
+                
+                // loss value
+                m_value += loss.value(target, output);
+                m_error += loss.error(target, output);
+                m_count ++;
+                
+                // loss gradient
+                switch (m_type)
+                {
+                case type::value:
+                        break;
+
+                case type::vgrad:
+                        m_vgrad += m_model->pgrad(loss.vgrad(target, output));
+                        break;
+                }
+        }
+        
+        void criterion_t::update(const task_t& task, const sample_t& sample, const loss_t& loss)
+        {
+                assert(sample.m_index < task.n_images());
+                
+                const image_t& image = task.image(sample.m_index);
+                const vector_t& target = sample.m_target;                
+                const vector_t& output = m_model->output(image, sample.m_region).vector();
+                
+                cumulate(output, target, loss);
+        }
+        
+        void criterion_t::update(const tensor_t& input, const vector_t& target, const loss_t& loss)
+        {
+                const vector_t& output = m_model->output(input).vector();
+                
+                cumulate(output, target, loss);
+        }
+        
+        void criterion_t::update(const vector_t& input, const vector_t& target, const loss_t& loss)
+        {
+                const vector_t& output = m_model->output(input).vector();
+                
+                cumulate(output, target, loss);
+        }
+        
+        scalar_t criterion_t::value() const
+        {
+                assert(count() > 0);
+
+                return m_value / count();
+        }
+
+        scalar_t criterion_t::error() const
+        {
+                assert(count() > 0);
+
+                return m_error / count();
+        }
+
+        vector_t criterion_t::vgrad() const
+        {
+                assert(count() > 0);
+
+                return m_vgrad / count();
+        }
+
+        size_t criterion_t::count() const
+        {
+                return m_count;
+        }
+
+        size_t criterion_t::dimensions() const
+        {
+                return static_cast<size_t>(m_params.size());
+        }
+
+        scalar_t criterion_t::lambda() const
+        {
+                return m_lambda;
+        }
+
+        scalars_t criterion_t::lambdas() const
+        {
+                return { 0.1, 0.2, 0.5, 1.0 };
+        }
+}
+	
---
+++ src/nanocv/criterion.h
@@ -0,0 +1,120 @@
+#ifndef NANOCV_CRITERION_H
+#define NANOCV_CRITERION_H
+
+#include "model.h"
+
+namespace ncv
+{        
+        class criterion_t;
+        class loss_t;
+        class task_t;
+        struct sample_t;
+
+        ///
+        /// \brief stores registered prototypes
+        ///
+        typedef manager_t<criterion_t>                  criterion_manager_t;
+        typedef criterion_manager_t::robject_t          rcriterion_t;
+
+        ///
+        /// \brief accumulate sample evaluations (loss value, error and gradient)
+        ///
+        class criterion_t : clonable_t<criterion_t>
+        {
+        public:
+
+                ///
+                /// \brief processing method
+                ///
+                enum class type : int
+                {
+                        value = 0,              ///< compute loss value (faster)
+                        vgrad                   ///< compute loss value and gradient (slower)
+                };
+
+                ///
+                /// \brief constructor
+                ///
+                criterion_t(const std::string& configuration,
+                            const std::string& description);
+                
+                ///
+                /// \brief reset statistics and settings
+                ///
+                criterion_t& reset(const rmodel_t& rmodel);
+                criterion_t& reset(const model_t& model);
+                criterion_t& reset(const vector_t& params);
+                criterion_t& reset(type t);
+                criterion_t& reset(scalar_t lambda);
+                virtual criterion_t& reset();
+
+                ///
+                /// \brief update statistics with a new sample
+                ///
+                void update(const task_t& task, const sample_t& sample, const loss_t& loss);
+                void update(const tensor_t& input, const vector_t& target, const loss_t& loss);
+                void update(const vector_t& input, const vector_t& target, const loss_t& loss);
+
+                ///
+                /// \brief cumulate statistics
+                ///
+                virtual criterion_t& operator+=(const criterion_t&);
+
+                ///
+                /// \brief average loss value
+                ///
+                virtual scalar_t value() const;
+
+                ///
+                /// \brief average error value
+                ///
+                virtual scalar_t error() const;
+
+                ///
+                /// \brief average gradient
+                ///
+                virtual vector_t vgrad() const;
+                
+                ///
+                /// \brief total number of processed samples
+                ///
+                size_t count() const;
+
+                ///
+                /// \brief number of dimensions
+                ///
+                size_t dimensions() const;
+
+                ///
+                /// \brief regularization weight (if any)
+                ///
+                scalar_t lambda() const;
+
+                ///
+                /// \brief regularization weights (to tune)
+                ///
+                scalars_t lambdas() const;
+
+        protected:
+
+                ///
+                /// \brief update statistics with a new sample
+                ///
+                virtual void cumulate(const vector_t& input, const vector_t& target, const loss_t& loss);
+
+        protected:
+
+                // attributes
+                rmodel_t                m_model;        ///< current model
+                vector_t                m_params;       ///< current model parameters
+                scalar_t                m_lambda;       ///< regularization weight (if any)
+
+                type                    m_type;         ///<
+                scalar_t                m_value;        ///< cumulated loss value
+                vector_t                m_vgrad;        ///< cumulated gradient
+                scalar_t                m_error;        ///< cumulated loss error
+                size_t                  m_count;        ///< #processed samples
+        };
+}
+
+#endif // NANOCV_CRITERION_H
