30fe123142186c9633d1dc7c63c7b88a792e4071
accosmin <accosmin@gmail.com>
2014-08-13 21:03:46 +0200

    unrolling does not help

--- src/common/mad.hpp
+++ src/common/mad.hpp
@@ -13,16 +13,7 @@ namespace ncv
         >
         void mad(const tscalar* idata, tscalar weight, tsize size, tscalar* odata)
         {
-                const tsize size4 = size - (size & 3);
-                
-                for (auto i = 0; i < size4; i += 4)
-                {
-                        odata[i + 0] += idata[i + 0] * weight;
-                        odata[i + 1] += idata[i + 1] * weight;
-                        odata[i + 2] += idata[i + 2] * weight;
-                        odata[i + 3] += idata[i + 3] * weight;
-                }
-                for (auto i = size4; i < size; i ++)
+                for (auto i = 0; i < size; i ++)
                 {
                         odata[i] += idata[i] * weight;
                 }
