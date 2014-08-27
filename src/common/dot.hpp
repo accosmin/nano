bb9af497f51ab06174a21a1082c1667f3f9c55ab
Cosmin Atanasoaei <accosmin@pix4d.com>
2014-08-27 12:12:26 +0200

    fix dot produc

--- src/common/dot.hpp
+++ src/common/dot.hpp
@@ -34,20 +34,20 @@ namespace ncv
         {
                 const tsize size4 = size & tsize(~3);
 
-                tscalar sum = 0;
+                tscalar sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0;
                 for (auto i = 0; i < size4; i += 4)
                 {
-                        sum += vec1[i + 0] * vec2[i + 0] +
-                               vec1[i + 1] * vec2[i + 1] +
-                               vec1[i + 2] * vec2[i + 2] +
-                               vec1[i + 3] * vec2[i + 3];
+                        sum0 += vec1[i + 0] * vec2[i + 0];
+                        sum1 += vec1[i + 1] * vec2[i + 1];
+                        sum2 += vec1[i + 2] * vec2[i + 2];
+                        sum3 += vec1[i + 3] * vec2[i + 3];
                 }
                 for (auto i = size4; i < size; i ++)
                 {
-                        sum += vec1[i] * vec2[i];
+                        sum0 += vec1[i] * vec2[i];
                 }
 
-                return sum;
+                return (sum0 + sum1) + (sum2 + sum3);
         }
 
         ///
@@ -61,33 +61,33 @@ namespace ncv
         tscalar dot_unroll8(const tscalar* vec1, const tscalar* vec2, tsize size)
         {
                 const tsize size8 = size & tsize(~7);
-                const tsize size4 = size & tsize(~4);
+                const tsize size4 = size & tsize(~3);
 
-                tscalar sum = 0;
+                tscalar sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0, sum7 = 0;
                 for (auto i = 0; i < size8; i += 8)
                 {
-                        sum += vec1[i + 0] * vec2[i + 0] +
-                               vec1[i + 1] * vec2[i + 1] +
-                               vec1[i + 2] * vec2[i + 2] +
-                               vec1[i + 3] * vec2[i + 3] +
-                               vec1[i + 4] * vec2[i + 4] +
-                               vec1[i + 5] * vec2[i + 5] +
-                               vec1[i + 6] * vec2[i + 6] +
-                               vec1[i + 7] * vec2[i + 7];
+                        sum0 += vec1[i + 0] * vec2[i + 0];
+                        sum1 += vec1[i + 1] * vec2[i + 1];
+                        sum2 += vec1[i + 2] * vec2[i + 2];
+                        sum3 += vec1[i + 3] * vec2[i + 3];
+                        sum4 += vec1[i + 4] * vec2[i + 4];
+                        sum5 += vec1[i + 5] * vec2[i + 5];
+                        sum6 += vec1[i + 6] * vec2[i + 6];
+                        sum7 += vec1[i + 7] * vec2[i + 7];
                 }
                 for (auto i = size8; i < size4; i += 4)
                 {
-                        sum += vec1[i + 0] * vec2[i + 0] +
-                               vec1[i + 1] * vec2[i + 1] +
-                               vec1[i + 2] * vec2[i + 2] +
-                               vec1[i + 3] * vec2[i + 3];
+                        sum0 += vec1[i + 0] * vec2[i + 0];
+                        sum1 += vec1[i + 1] * vec2[i + 1];
+                        sum2 += vec1[i + 2] * vec2[i + 2];
+                        sum3 += vec1[i + 3] * vec2[i + 3];
                 }
                 for (auto i = size4; i < size; i ++)
                 {
-                        sum += vec1[i] * vec2[i];
+                        sum0 += vec1[i] * vec2[i];
                 }
 
-                return sum;
+                return (sum0 + sum1) + (sum2 + sum3) + (sum4 + sum5) + (sum6 + sum7);
         }
 
         ///
