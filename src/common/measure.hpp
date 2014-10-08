65b636e999329f0dd152ff502d5a478d672df503
accosmin <accosmin@gmail.com>
2014-10-08 19:23:53 +0200

    moved function call measuring to its own file

---
+++ src/common/measure.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "common/logger.h"
+#include "common/timer.h"
+
+namespace ncv
+{
+        ///
+        /// \brief measure function call
+        ///
+        template
+        <
+                typename toperator
+        >
+        void measure_call(const toperator& op, const std::string& msg)
+        {
+                const timer_t timer;
+                op();
+                log_info() << msg << " [" << timer.elapsed() << "].";
+        }
+
+        ///
+        /// \brief measure function call (and exit if any error)
+        ///
+        template
+        <
+                typename toperator
+        >
+        void measure_critical_call(const toperator& op, const std::string& msg_success, const std::string& msg_failure)
+        {
+                const timer_t timer;
+                if (op())
+                {
+                        log_info() << msg_success << " (" << timer.elapsed() << ").";
+                }
+                else
+                {
+                        log_error() << msg_failure << " (" << timer.elapsed() << ")!";
+                        exit(EXIT_FAILURE);
+                }
+        }
+}
--- src/nanocv.h
+++ src/nanocv.h
@@ -9,8 +9,7 @@
 #include "optimize/opt_gd.hpp"
 #include "optimize/opt_cgd.hpp"
 #include "optimize/opt_lbfgs.hpp"
-#include "common/logger.h"
-#include "common/timer.h"
+#include "common/measure.hpp"
 #include "common/math.hpp"
 #include "common/stats.hpp"
 #include "common/thread_loop.hpp"
@@ -25,44 +24,9 @@ namespace ncv
         string_t version();
 
         ///
-        /// \brief initialize library (register objects, start worker pool, initialize OpenCL ...)
+        /// \brief initialize library (setup flags, register default objects ...)
         ///
         void init();
-
-        ///
-        /// measure function call
-        ///
-        template
-        <
-                typename toperator
-        >
-        void measure_call(const toperator& op, const string_t& msg)
-        {
-                const timer_t timer;
-                op();
-                log_info() << msg << " [" << timer.elapsed() << "].";
-        }
-
-        ///
-        /// \brief measure function call (and exit if any error)
-        ///
-        template
-        <
-                typename toperator
-        >
-        void measure_critical_call(const toperator& op, const string_t& msg_success, const string_t& msg_failure)
-        {
-                const timer_t timer;
-                if (op())
-                {
-                        log_info() << msg_success << " (" << timer.elapsed() << ").";
-                }
-                else
-                {
-                        log_error() << msg_failure << " (" << timer.elapsed() << ")!";
-                        exit(EXIT_FAILURE);
-                }
-        }
 
         ///
         /// \brief evaluate a model (compute the average loss value & error)
