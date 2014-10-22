238609901a4ab44b4155933a9d1951258c55c7ca
accosmin <accosmin@gmail.com>
2014-10-22 21:27:09 +0200

    load binary data from libarchive's structures (todo - recursive loading)

--- src/common/io_arch.cpp
+++ src/common/io_arch.cpp
@@ -2,14 +2,55 @@
 #include "io_bzip.h"
 #include "io_gzip.h"
 #include "logger.h"
-#include <fstream>
-#include <cmath>
-#include <cstdint>
 #include <archive.h>
 #include <archive_entry.h>
+#include <boost/algorithm/string.hpp>
 
 namespace ncv
 {
+        enum class archive_type : int
+        {
+                tar,
+                tar_gz,
+                tar_bz2,
+                gz,
+                bz2,
+                unknown
+        };
+
+        static archive_type decode_archive_type(const std::string& path)
+        {
+
+                if (    boost::algorithm::iends_with(path, ".tar.gz") ||
+                        boost::algorithm::iends_with(path, ".tgz"))
+                {
+                        return archive_type::tar_gz;
+                }
+
+                else if (boost::algorithm::iends_with(path, ".tar.bz2") ||
+                         boost::algorithm::iends_with(path, ".tbz") ||
+                         boost::algorithm::iends_with(path, ".tbz2") ||
+                         boost::algorithm::iends_with(path, ".tb2"))
+                {
+                        return archive_type::tar_bz2;
+                }
+
+                else if (boost::algorithm::iends_with(path, ".tar"))
+                {
+                        return archive_type::tar;
+                }
+
+                else if (boost::algorithm::iends_with(path, ".gz"))
+                {
+                        return archive_type::gz;
+                }
+
+                else
+                {
+                        return archive_type::unknown;
+                }
+        }
+
 //        static bool io_decode(const io::data_t& orig_data, const std::string& path, io::data_t& data)
 //        {
 //                const archive_type type = decode_archive_type(path);
@@ -145,45 +186,99 @@ namespace ncv
 //                return true;
 //        }
 
+        bool copy(archive* ar, io::data_t& data)
+        {
+                while (true)
+                {
+                        const void* buff;
+                        size_t size;
+                        off_t offset;
+
+                        const int r = archive_read_data_block(ar, &buff, &size, &offset);
+                        if (r == ARCHIVE_EOF)
+                                return true;
+                        if (r != ARCHIVE_OK)
+                                return false;
+
+                        data.insert(data.end(), (const char*)buff, (const char*)buff + size);
+                }
+
+                return true;
+        }
+
         bool io::decode(const std::string& path, const std::string& log_header, const data_callback_t& callback)
         {
                 int r;
 
-                archive* a = archive_read_new();
-//                archive_read_support_format_tar(a);
-                archive_read_support_format_all(a);
+                archive* ar = archive_read_new();
 
-                if ((r = archive_read_open_filename(a, path.c_str(), 10240)))
+                archive_read_support_filter_all(ar);
+                archive_read_support_format_all(ar);
+                archive_read_support_format_raw(ar);
+
+                if ((r = archive_read_open_filename(ar, path.c_str(), 10240)))
                 {
                         log_error() << log_header << "failed to open archive <" << path << ">!";
-                        log_error() << log_header << "error <" << archive_error_string(a) << ">!";
+                        log_error() << log_header << "error <" << archive_error_string(ar) << ">!";
                         return false;
                 }
 
                 bool ok = true;
-                while (true)
+                while (ok)
                 {
                         archive_entry* entry;
-                        r = archive_read_next_header(a, &entry);
+                        r = archive_read_next_header(ar, &entry);
 
                         if (r == ARCHIVE_EOF)
                                 break;
                         if (r != ARCHIVE_OK)
                         {
-                                log_warning() << log_header << "failed to read archive!";
-                                log_warning() << log_header << "error <" << archive_error_string(a) << ">!";
+                                log_error() << log_header << "failed to read archive!";
+                                log_error() << log_header << "error <" << archive_error_string(ar) << ">!";
                                 ok = false;
                                 break;
                         }
 
                         const std::string filename = archive_entry_pathname(entry);
-                        log_info() << log_header << "filename = <" << filename << ">";
+                        const archive_type filetype = decode_archive_type(filename);
+                        const int64_t filesize = archive_entry_size(entry);
 
-//                        copy_data(a, ext);
+                        log_info() << log_header << "filename = <" << filename << ">, filesize = <" << filesize << ">";
+
+                        switch (filetype)
+                        {
+                        case archive_type::tar:
+                        case archive_type::tar_gz:
+                        case archive_type::tar_bz2:
+                        case archive_type::gz:
+                        case archive_type::bz2:
+                                // todo
+                                break;
+
+                        default:
+                                {
+                                        data_t data;
+                                        if (copy(ar, data))
+                                        {
+                                                log_info() << log_header
+                                                           << "filename = <" << filename
+                                                           << ">, filesize = <" << data.size()
+                                                           << "/" << filesize << ">";
+                                                callback(filename, data);
+                                        }
+                                        else
+                                        {
+                                                log_error() << log_header << "failed to read entry!";
+                                                log_error() << log_header << "error <" << archive_error_string(ar) << ">!";
+                                                ok = false;
+                                        }
+                                }
+                                break;
+                        }
                 }
 
-                archive_read_close(a);
-                archive_read_free(a);
+                archive_read_close(ar);
+                archive_read_free(ar);
 
                 // OK
                 return ok;
