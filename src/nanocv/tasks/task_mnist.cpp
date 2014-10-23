7f21b9768ea48ed0eb3a4203c411d822f43e099e
accosmin <accosmin@gmail.com>
2014-10-23 22:12:12 +0200

    use original .gz files to load the MNIST task

--- src/nanocv/tasks/task_mnist.cpp
+++ src/nanocv/tasks/task_mnist.cpp
@@ -2,8 +2,9 @@
 #include "common/logger.h"
 #include "common/math.hpp"
 #include "common/cast.hpp"
+#include "common/io_arch.h"
+#include "common/io_stream.h"
 #include "loss.h"
-#include <fstream>
 
 namespace ncv
 {
@@ -14,12 +15,12 @@ namespace ncv
 
         bool mnist_task_t::load(const string_t& dir)
         {
-                const string_t test_ifile = dir + "/t10k-images-idx3-ubyte";
-                const string_t test_gfile = dir + "/t10k-labels-idx1-ubyte";
+                const string_t test_ifile = dir + "/t10k-images-idx3-ubyte.gz";
+                const string_t test_gfile = dir + "/t10k-labels-idx1-ubyte.gz";
                 const size_t n_test_samples = 10000;
 
-                const string_t train_ifile = dir + "/train-images-idx3-ubyte";
-                const string_t train_gfile = dir + "/train-labels-idx1-ubyte";
+                const string_t train_ifile = dir + "/train-images-idx3-ubyte.gz";
+                const string_t train_gfile = dir + "/train-labels-idx1-ubyte.gz";
                 const size_t n_train_samples = 60000;
 
                 clear_memory(n_train_samples + n_test_samples);
@@ -30,52 +31,66 @@ namespace ncv
 
         size_t mnist_task_t::load(const string_t& ifile, const string_t& gfile, protocol p)
         {
+                size_t icnt = 0, isize = m_images.size();
+                size_t gcnt = 0;
+
                 std::vector<char> vbuffer(n_rows() * n_cols());
                 char* buffer = vbuffer.data();
                 char label[2];
 
-                log_info() << "MNIST: loading file <" << ifile << "> ...";
-
-                // image and label data streams
-                std::ifstream fimage(ifile.c_str(), std::ios::in | std::ios::binary);
-                std::ifstream flabel(gfile.c_str(), std::ios::in | std::ios::binary);
-
-                if (!fimage.is_open() || !flabel.is_open())
+                // load image file
+                const auto iop = [&] (const string_t&, const io::data_t& data)
                 {
-                        log_error() << "MNIST: failed to load files!";
+                        io::stream_t stream(data);
+
+                        stream.read(buffer, 16);
+                        while (stream.read(buffer, vbuffer.size()) && stream.gcount() == vbuffer.size())
+                        {
+                                image_t image;
+                                image.load_luma(buffer, n_rows(), n_cols());
+                                m_images.push_back(image);
+
+                                ++ icnt;
+                        }
+                };
+
+                log_info() << "MNIST: loading file <" << ifile << "> ...";
+                if (!io::decode(ifile, "MNIST: ", iop))
+                {
+                        log_error() << "MNIST: failed to load file <" << ifile << ">!";
                         return 0;
                 }
 
-                // read headers
-                fimage.read(buffer, 16);
-                flabel.read(buffer, 8);
-
-                // load annotations and images
-                size_t cnt = 0;
-                while ( flabel.read(label, 1) &&
-                        fimage.read(buffer, vbuffer.size()))
+                // load ground truth file
+                const auto gop = [&] (const string_t&, const io::data_t& data)
                 {
-                        const size_t ilabel = math::cast<size_t>(label[0]);
-                        if (ilabel >= n_outputs())
+                        io::stream_t stream(data);
+
+                        stream.read(buffer, 8);
+                        while (stream.read(label, 1) && stream.gcount() == 1)
                         {
-                                continue;
+                                const size_t ilabel = math::cast<size_t>(label[0]);
+
+                                sample_t sample(isize, sample_region(0, 0));
+                                sample.m_label = "digit" + text::to_string(ilabel);
+                                sample.m_target = ncv::class_target(ilabel, n_outputs());
+                                sample.m_fold = { 0, p };
+                                m_samples.push_back(sample);
+
+                                ++ gcnt;
+                                ++ isize;
                         }
+                };
 
-                        sample_t sample(m_images.size(), sample_region(0, 0));
-                        sample.m_label = "digit" + text::to_string(ilabel);
-                        sample.m_target = ncv::class_target(ilabel, n_outputs());
-                        sample.m_fold = { 0, p };
-                        m_samples.push_back(sample);
-
-                        image_t image;
-                        image.load_luma(buffer, n_rows(), n_cols());
-                        m_images.push_back(image);
-
-                        ++ cnt;
+                log_info() << "MNIST: loading file <" << gfile << "> ...";
+                if (!io::decode(gfile, "MNIST: ", gop))
+                {
+                        log_error() << "MNIST: failed to load file <" << gfile << ">!";
+                        return 0;
                 }
 
-                log_info() << "MNIST: loaded " << cnt << " samples.";
-
-                return cnt;
+                // OK
+                log_info() << "MNIST: loaded " << icnt << "/" << gcnt << " samples.";
+                return std::max(icnt, gcnt);
         }
 }
