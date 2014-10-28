5576481b466121082b470660df9221a3fb817e56
accosmin <accosmin@gmail.com>
2014-10-28 21:17:23 +0100

    O3 produces faster code, update test program with larger vectors

--- test/ncv_test_unroll.cpp
+++ test/ncv_test_unroll.cpp
@@ -28,7 +28,7 @@ namespace ncv
         >
         void mad_eig(const tscalar* idata, tscalar weight, int size, tscalar* odata)
         {
-                tensor::make_vector(odata, size) += weight * tensor::make_vector(idata, size);
+                tensor::make_vector(odata, size) += tensor::make_vector(idata, size) * weight;
         }
 }
 
@@ -58,20 +58,11 @@ template
         typename tscalar = typename tvector::Scalar
 >
 tscalar test_dot(
-        top op, const char* name, size_t n_tests,
-        const tvector& vec1, const tvector& vec2)
+        top op, const char* name, const tvector& vec1, const tvector& vec2)
 {
-        const size_t size = vec1.size();
-        const size_t vsize = size / n_tests;
-
         const ncv::timer_t timer;
 
-        // run multiple tests
-        tscalar ret = 0;
-        for (size_t t = 0; t < size; t += vsize)
-        {
-                ret += op(vec1.data() + t, vec2.data() + t, vsize);
-        }
+        const tscalar ret = op(vec1.data(), vec2.data(), vec1.size());
         
         const size_t milis = static_cast<size_t>(timer.miliseconds());
         std::cout << name << "= " << text::resize(text::to_string(milis), 4, align::right) << "ms  ";
@@ -86,22 +77,14 @@ template
         typename tscalar = typename tvector::Scalar
 >
 tscalar test_mad(
-        top op, const char* name, size_t n_tests,
-        const tvector& vec1, const tvector& vec2, tscalar wei)
+        top op, const char* name, const tvector& vec1, const tvector& vec2, tscalar wei)
 {
         vector_t cvec1 = vec1;
         vector_t cvec2 = vec2;
 
-        const size_t size = vec1.size();
-        const size_t vsize = size / n_tests;
-
         const ncv::timer_t timer;
 
-        // run multiple tests
-        for (size_t t = 0; t < size; t += vsize)
-        {
-                op(cvec1.data() + t, wei, vsize, cvec2.data() + t);
-        }
+        op(cvec1.data(), wei, cvec1.size(), cvec2.data());
 
         const size_t milis = static_cast<size_t>(timer.miliseconds());
         std::cout << name << "= " << text::resize(text::to_string(milis), 4, align::right) << "ms  ";
@@ -122,24 +105,24 @@ void check(tscalar result, tscalar baseline, const char* name)
         }
 }
 
-void test_dot(size_t size, size_t n_tests)
+void test_dot(size_t size)
 {
         test_vector_t vec1, vec2;
-        rand_vector(size * n_tests, vec1);
-        rand_vector(size * n_tests, vec2);
+        rand_vector(size, vec1);
+        rand_vector(size, vec2);
 
-        const string_t header = (boost::format("%1% x (%2%): ") % n_tests % size).str();
+        const string_t header = (boost::format("(%1%): ") % size).str();
         std::cout << text::resize(header, 20);
 
-        const test_scalar_t dot    = test_dot(ncv::dot<test_scalar_t>, "dot", n_tests, vec1, vec2);
-        const test_scalar_t dotul2 = test_dot(ncv::dot_unroll<test_scalar_t, 2>, "dotul2", n_tests, vec1, vec2);
-        const test_scalar_t dotul3 = test_dot(ncv::dot_unroll<test_scalar_t, 3>, "dotul3", n_tests, vec1, vec2);
-        const test_scalar_t dotul4 = test_dot(ncv::dot_unroll<test_scalar_t, 4>, "dotul4", n_tests, vec1, vec2);
-        const test_scalar_t dotul5 = test_dot(ncv::dot_unroll<test_scalar_t, 5>, "dotul5", n_tests, vec1, vec2);
-        const test_scalar_t dotul6 = test_dot(ncv::dot_unroll<test_scalar_t, 6>, "dotul6", n_tests, vec1, vec2);
-        const test_scalar_t dotul7 = test_dot(ncv::dot_unroll<test_scalar_t, 7>, "dotul7", n_tests, vec1, vec2);
-        const test_scalar_t dotul8 = test_dot(ncv::dot_unroll<test_scalar_t, 8>, "dotul8", n_tests, vec1, vec2);
-        const test_scalar_t doteig = test_dot(ncv::dot_eig<test_scalar_t>, "doteig", n_tests, vec1, vec2);
+        const test_scalar_t dot    = test_dot(ncv::dot<test_scalar_t>, "dot", vec1, vec2);
+        const test_scalar_t dotul2 = test_dot(ncv::dot_unroll<test_scalar_t, 2>, "dotul2", vec1, vec2);
+        const test_scalar_t dotul3 = test_dot(ncv::dot_unroll<test_scalar_t, 3>, "dotul3", vec1, vec2);
+        const test_scalar_t dotul4 = test_dot(ncv::dot_unroll<test_scalar_t, 4>, "dotul4", vec1, vec2);
+        const test_scalar_t dotul5 = test_dot(ncv::dot_unroll<test_scalar_t, 5>, "dotul5", vec1, vec2);
+        const test_scalar_t dotul6 = test_dot(ncv::dot_unroll<test_scalar_t, 6>, "dotul6", vec1, vec2);
+        const test_scalar_t dotul7 = test_dot(ncv::dot_unroll<test_scalar_t, 7>, "dotul7", vec1, vec2);
+        const test_scalar_t dotul8 = test_dot(ncv::dot_unroll<test_scalar_t, 8>, "dotul8", vec1, vec2);
+        const test_scalar_t doteig = test_dot(ncv::dot_eig<test_scalar_t>, "doteig", vec1, vec2);
         std::cout << std::endl;
 
         check(dot,      dot, "dot");
@@ -153,28 +136,28 @@ void test_dot(size_t size, size_t n_tests)
         check(doteig,   dot, "doteig");
 }
 
-void test_mad(size_t size, size_t n_tests)
+void test_mad(size_t size)
 {
         test_vector_t vec1, vec2;
-        rand_vector(size * n_tests, vec1);
-        rand_vector(size * n_tests, vec2);
+        rand_vector(size, vec1);
+        rand_vector(size, vec2);
 
         test_scalar_t wei;
 
         wei = vec1(0) + vec2(3);
 
-        const string_t header = (boost::format("%1% x (%2%): ") % n_tests % size).str();
+        const string_t header = (boost::format("(%1%): ") % size).str();
         std::cout << text::resize(header, 20);
 
-        const test_scalar_t mad    = test_mad(ncv::mad<test_scalar_t>, "mad", n_tests, vec1, vec2, wei);
-        const test_scalar_t madul2 = test_mad(ncv::mad_unroll<test_scalar_t, 2>, "madul2", n_tests, vec1, vec2, wei);
-        const test_scalar_t madul3 = test_mad(ncv::mad_unroll<test_scalar_t, 3>, "madul3", n_tests, vec1, vec2, wei);
-        const test_scalar_t madul4 = test_mad(ncv::mad_unroll<test_scalar_t, 4>, "madul4", n_tests, vec1, vec2, wei);
-        const test_scalar_t madul5 = test_mad(ncv::mad_unroll<test_scalar_t, 5>, "madul5", n_tests, vec1, vec2, wei);
-        const test_scalar_t madul6 = test_mad(ncv::mad_unroll<test_scalar_t, 6>, "madul6", n_tests, vec1, vec2, wei);
-        const test_scalar_t madul7 = test_mad(ncv::mad_unroll<test_scalar_t, 7>, "madul7", n_tests, vec1, vec2, wei);
-        const test_scalar_t madul8 = test_mad(ncv::mad_unroll<test_scalar_t, 8>, "madul8", n_tests, vec1, vec2, wei);
-        const test_scalar_t madeig = test_mad(ncv::mad_eig<test_scalar_t>, "madeig", n_tests, vec1, vec2, wei);
+        const test_scalar_t mad    = test_mad(ncv::mad<test_scalar_t>, "mad", vec1, vec2, wei);
+        const test_scalar_t madul2 = test_mad(ncv::mad_unroll<test_scalar_t, 2>, "madul2", vec1, vec2, wei);
+        const test_scalar_t madul3 = test_mad(ncv::mad_unroll<test_scalar_t, 3>, "madul3", vec1, vec2, wei);
+        const test_scalar_t madul4 = test_mad(ncv::mad_unroll<test_scalar_t, 4>, "madul4", vec1, vec2, wei);
+        const test_scalar_t madul5 = test_mad(ncv::mad_unroll<test_scalar_t, 5>, "madul5", vec1, vec2, wei);
+        const test_scalar_t madul6 = test_mad(ncv::mad_unroll<test_scalar_t, 6>, "madul6", vec1, vec2, wei);
+        const test_scalar_t madul7 = test_mad(ncv::mad_unroll<test_scalar_t, 7>, "madul7", vec1, vec2, wei);
+        const test_scalar_t madul8 = test_mad(ncv::mad_unroll<test_scalar_t, 8>, "madul8", vec1, vec2, wei);
+        const test_scalar_t madeig = test_mad(ncv::mad_eig<test_scalar_t>, "madeig", vec1, vec2, wei);
         std::cout << std::endl;
 
         check(mad,      mad, "mad");
@@ -190,19 +173,18 @@ void test_mad(size_t size, size_t n_tests)
 
 int main(int argc, char* argv[])
 {
-        static const size_t min_size = 4;
-        static const size_t max_size = 64;
-        static const size_t n_tests = 1023 * 991 * 7;
+        static const size_t min_size = 4 * 1024 * 1024;
+        static const size_t max_size = 256 * 1024 * 1024;
 
         for (size_t size = min_size; size <= max_size; size *= 2)
         {
-                test_dot(size, n_tests);
+                test_dot(size);
         }
         std::cout << std::endl;
 
         for (size_t size = min_size; size <= max_size; size *= 2)
         {
-                test_mad(size, n_tests);
+                test_mad(size);
         }
 
 	return EXIT_SUCCESS;
