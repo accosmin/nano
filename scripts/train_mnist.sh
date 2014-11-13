54d95e836af717642d4ff8a7e4c953e233fda2b8
accosmin <accosmin@gmail.com>
2014-11-13 20:40:26 +0100

    update train script

--- scripts/train_mnist.sh
+++ scripts/train_mnist.sh
@@ -8,22 +8,21 @@ params=${params}${task_mnist}
 params=${params}" --loss classnll --trials 10 --threads ${max_threads}"
 
 # models
-conv_max="--model forward-network --model-params "
-conv_max=${conv_max}"conv:dims=16,rows=6,cols=6,type=full;act-snorm;pool-max;"
-conv_max=${conv_max}"conv:dims=32,rows=5,cols=5,type=full;act-snorm;pool-max;"
-conv_max=${conv_max}"conv:dims=64,rows=4,cols=4,type=full;act-snorm;"
+conv100_max="--model forward-network --model-params "
+conv100_max=${conv100_max}"conv:dims=16,rows=9,cols=9,mask=100;act-snorm;pool-max;"
+conv100_max=${conv100_max}"conv:dims=32,rows=5,cols=5,mask=100;act-snorm;pool-max;"
+conv100_max=${conv100_max}"conv:dims=64,rows=3,cols=3,mask=100;act-snorm;"
 
-conv_min=${conv_max//pool-max/pool-min}
-conv_avg=${conv_max//pool-max/pool-avg}
+conv50_max="--model forward-network --model-params "
+conv50_max=${conv50_max}"conv:dims=16,rows=9,cols=9,mask=50;act-snorm;pool-max;"
+conv50_max=${conv50_max}"conv:dims=32,rows=5,cols=5,mask=50;act-snorm;pool-max;"
+conv50_max=${conv50_max}"conv:dims=64,rows=3,cols=3,mask=50;act-snorm;"
 
-rconv_max=${conv_max//type=full/type=rand}
-rconv_min=${conv_min//type=full/type=rand}
-rconv_avg=${conv_avg//type=full/type=rand}
+conv25_max="--model forward-network --model-params "
+conv25_max=${conv25_max}"conv:dims=16,rows=9,cols=9,mask=25;act-snorm;pool-max;"
+conv25_max=${conv25_max}"conv:dims=32,rows=5,cols=5,mask=25;act-snorm;pool-max;"
+conv25_max=${conv25_max}"conv:dims=64,rows=3,cols=3,mask=25;act-snorm;"
 
-mconv_max=${conv_max//type=full/type=mask}
-mconv_min=${conv_min//type=full/type=mask}
-mconv_avg=${conv_avg//type=full/type=mask}
-        
 mlp0="--model forward-network --model-params "
 mlp1=${mlp0}"linear:dims=128;act-snorm;"
 mlp2=${mlp1}"linear:dims=64;act-snorm;"
@@ -31,12 +30,16 @@ mlp3=${mlp2}"linear:dims=32;act-snorm;"
 
 outlayer="linear:dims=10;"
 
+mbatch_lbfgs="--trainer minibatch --trainer-params opt=lbfgs,epoch=64,batch=1024,iters=8,eps=1e-6"
+
 # train models
-for model in `echo "conv_max conv_avg conv_min rconv_max rconv_avg rconv_min mconv_max mconv_avg mconv_min mlp0 mlp1 mlp2 mlp3"`
+for model in `echo "conv100_max conv50_max conv25_max mlp0 mlp1 mlp2 mlp3"`
 do
         for trainer in `echo "mbatch_lbfgs"`
 	do
 	        fn_train ${dir_exp_mnist} ${trainer}_${model} ${params} ${!trainer} ${avg_crit} ${!model}${outlayer}
+	        fn_train ${dir_exp_mnist} ${trainer}_${model}_l2n ${params} ${!trainer} ${l2n_crit} ${!model}${outlayer}
+                fn_train ${dir_exp_mnist} ${trainer}_${model}_var ${params} ${!trainer} ${var_crit} ${!model}${outlayer}
 	done
 
         #for trainer in `echo "mbatch_gd mbatch_cgd mbatch_lbfgs"` # batch_gd batch_cgd batch_lbfgs"`
@@ -57,7 +60,7 @@ exit
 # compare models
 bash plot_models.sh ${dir_exp_mnist}/models.pdf ${dir_exp_mnist}/*.state
 
-bash plot_models.sh ${dir_exp_mnist}/conv_max_models.pdf ${dir_exp_mnist}/conv_max_*.state
-bash plot_models.sh ${dir_exp_mnist}/conv_min_models.pdf ${dir_exp_mnist}/conv_min_*.state
-bash plot_models.sh ${dir_exp_mnist}/conv_avg_models.pdf ${dir_exp_mnist}/conv_avg_*.state
+# bash plot_models.sh ${dir_exp_mnist}/conv_max_models.pdf ${dir_exp_mnist}/conv_max_*.state
+# bash plot_models.sh ${dir_exp_mnist}/conv_min_models.pdf ${dir_exp_mnist}/conv_min_*.state
+# bash plot_models.sh ${dir_exp_mnist}/conv_avg_models.pdf ${dir_exp_mnist}/conv_avg_*.state
 
