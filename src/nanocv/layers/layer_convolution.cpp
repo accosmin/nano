4b4fc5af26e3af29c7773307f74a916eeb9ed2ec
Cosmin Atanasoaei <accosmin@pix4d.com>
2014-11-14 13:59:35 +0100

    cleanup convolution layer

--- src/nanocv/layers/layer_convolution.cpp
+++ src/nanocv/layers/layer_convolution.cpp
@@ -14,124 +14,6 @@ namespace ncv
                 return value > 0.5;
         }
 
-        template
-        <
-                typename tscalar,
-                typename tsize
-        >
-        static void _output(
-                const tscalar* mdata,
-                const tscalar* idata, tsize idims,
-                const tscalar* kdata, tsize krows, tsize kcols,
-                tscalar* odata, tsize odims, tsize orows, tsize ocols)
-        {
-                const tsize irows = orows + krows - 1;
-                const tsize icols = ocols + kcols - 1;
-                const tsize isize = irows * icols;
-
-                const tsize osize = orows * ocols;
-                const tsize ksize = krows * kcols;
-
-                auto mmap = tensor::make_matrix(mdata, odims, idims);
-
-                // output
-                for (tsize o = 0, k = 0; o < odims; o ++)
-                {
-                        auto omap = tensor::make_matrix(odata + o * osize, orows, ocols);
-
-                        omap.setZero();
-                        for (tsize i = 0; i < idims; i ++, k ++)
-                        {
-                                auto imap = tensor::make_matrix(idata + i * isize, irows, icols);
-                                auto kmap = tensor::make_matrix(kdata + k * ksize, krows, kcols);
-
-                                if (is_masked(mmap(o, i)))
-                                {
-                                        ncv::conv2d_dyn(imap, kmap, omap);
-                                }
-                        }
-                }
-        }
-
-        template
-        <
-                typename tscalar,
-                typename tsize
-        >
-        static void _igrad(
-                const tscalar* mdata,
-                tscalar* gidata, tsize idims,
-                const tscalar* kdata, tsize krows, tsize kcols,
-                const tscalar* odata, tsize odims, tsize orows, tsize ocols)
-        {
-                const tsize irows = orows + krows - 1;
-                const tsize icols = ocols + kcols - 1;
-                const tsize isize = irows * icols;
-
-                const tsize osize = orows * ocols;
-                const tsize ksize = krows * kcols;
-
-                auto mmap = tensor::make_matrix(mdata, odims, idims);
-                
-                // input gradient
-                tensor::make_vector(gidata, idims * isize).setZero();
-                for (tsize o = 0, k = 0; o < odims; o ++)
-                {
-                        auto omap = tensor::make_matrix(odata + o * osize, orows, ocols);
-                        
-                        for (tsize i = 0; i < idims; i ++, k ++)
-                        {
-                                auto gimap = tensor::make_matrix(gidata + i * isize, irows, icols);
-                                auto kmap = tensor::make_matrix(kdata + k * ksize, krows, kcols);
-
-                                if (is_masked(mmap(o, i)))
-                                {
-                                        ncv::corr2d_dyn(omap, kmap, gimap);
-                                }
-                        }
-                }
-        }
-
-        template
-        <
-                typename tscalar,
-                typename tsize
-        >
-        static void _pgrad(
-                const tscalar* mdata,
-                const tscalar* idata, tsize idims,
-                tscalar* gkdata, tsize krows, tsize kcols,
-                const tscalar* odata, tsize odims, tsize orows, tsize ocols)
-        {
-                const tsize irows = orows + krows - 1;
-                const tsize icols = ocols + kcols - 1;
-                const tsize isize = irows * icols;
-
-                const tsize osize = orows * ocols;
-                const tsize ksize = krows * kcols;
-
-                auto mmap = tensor::make_matrix(mdata, odims, idims);
-
-                // convolution kernel gradient
-                for (tsize o = 0, k = 0; o < odims; o ++)
-                {
-                        auto omap = tensor::make_matrix(odata + o * osize, orows, ocols);
-
-                        for (tsize i = 0; i < idims; i ++)
-                        {
-                                auto imap = tensor::make_matrix(idata + i * isize, irows, icols);
-                                auto gkmap = tensor::make_matrix(gkdata + k * ksize, krows, kcols);
-
-                                if (is_masked(mmap(o, i)))
-                                {
-                                        gkmap.setZero();
-                                        ncv::conv2d_dyn(imap, omap, gkmap);
-                                        k ++;
-                                }
-                        }
-                }
-        }
-
         conv_layer_t::conv_layer_t(const string_t& parameters)
                 :       layer_t(parameters)
         {
@@ -283,11 +165,23 @@ namespace ncv
                 assert(icols() == input.cols());
 
                 m_idata.copy_from(input);
-
-                _output(m_mdata.data(),
-                        m_idata.data(), idims(),
-                        m_kdata.data(), krows(), kcols(),
-                        m_odata.data(), odims(), orows(), ocols());
+                
+                for (size_t o = 0; o < odims(); o ++)
+                {
+                        auto omap = m_odata.plane_matrix(o);                        
+                        
+                        omap.setZero();                        
+                        for (size_t i = 0; i < idims(); i ++)
+                        {
+                                auto imap = m_idata.plane_matrix(i);
+                                auto kmap = m_kdata.plane_matrix(o * idims() + i);
+                                
+                                if (is_masked(m_mdata(o, i)))
+                                {
+                                        ncv::conv2d_dyn(imap, kmap, omap);
+                                }
+                        }
+                }
 
                 return m_odata;
         }        
@@ -299,11 +193,23 @@ namespace ncv
                 assert(ocols() == output.cols());
 
                 m_odata.copy_from(output);
-
-                _igrad(m_mdata.data(),
-                       m_idata.data(), idims(),
-                       m_kdata.data(), krows(), kcols(),
-                       m_odata.data(), odims(), orows(), ocols());
+                
+                m_idata.zero();                
+                for (size_t o = 0; o < odims(); o ++)
+                {
+                        auto omap = m_odata.plane_matrix(o);
+                        
+                        for (size_t i = 0; i < idims(); i ++)
+                        {
+                                auto gimap = m_idata.plane_matrix(i);
+                                auto kmap = m_kdata.plane_matrix(o * idims() + i);
+                                
+                                if (is_masked(m_mdata(o, i)))
+                                {
+                                        ncv::corr2d_dyn(omap, kmap, gimap);
+                                }
+                        }
+                }
 
                 return m_idata;
         }
@@ -315,11 +221,26 @@ namespace ncv
                 assert(ocols() == output.cols());
 
                 m_odata.copy_from(output);
-
-                _pgrad(m_mdata.data(),
-                       m_idata.data(), idims(),
-                       gradient, krows(), kcols(),
-                       m_odata.data(), odims(), orows(), ocols());
+                
+                const size_t ksize = krows() * kcols();
+                
+                for (size_t o = 0, k = 0; o < odims(); o ++)
+                {
+                        auto omap = m_odata.plane_matrix(o);
+                        
+                        for (size_t i = 0; i < idims(); i ++)
+                        {
+                                auto imap = m_idata.plane_matrix(i);
+                                auto gkmap = tensor::make_matrix(gradient + k * ksize, krows(), kcols());
+                                
+                                if (is_masked(m_mdata(o, i)))
+                                {
+                                        gkmap.setZero();
+                                        ncv::conv2d_dyn(imap, omap, gkmap);
+                                        k ++;
+                                }
+                        }
+                }
         }
 }
 
