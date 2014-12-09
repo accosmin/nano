5d821d79cb7edaa7dea352464ecbe0581fa97423
Cosmin Atanasoaei <accosmin@pix4d.com>
2014-12-09 13:02:26 +0100

    fix possible memory leak

--- src/common/singleton.hpp
+++ src/common/singleton.hpp
@@ -24,7 +24,7 @@ namespace ncv
                 {
                         std::call_once(m_once_flag, []()
                         {
-                                m_instance.reset(new this_object);
+                                singleton_t::m_instance.reset(new this_object);
                         });
                         return *m_instance.get();
                 }
