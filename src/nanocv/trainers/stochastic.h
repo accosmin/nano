e6cda80cedd8704f756f6a30c17346109da963c3
Cosmin Atanasoaei <accosmin@pix4d.com>
2014-12-12 11:50:15 +0100

    cleanup

--- src/nanocv/trainers/stochastic.h
+++ src/nanocv/trainers/stochastic.h
@@ -8,7 +8,7 @@ namespace ncv
         /// \brief stochastically train the given model
         ///
         trainer_result_t stochastic_train(
-                const model_t& model, const task_t& task, const sampler_t& tsampler, const sampler_t& vsampler, size_t nthreads,
-                const loss_t& loss, const string_t& criterion,
+                const model_t&, const task_t&, const sampler_t& tsampler, const sampler_t& vsampler, size_t nthreads,
+                const loss_t&, const string_t& criterion,
                 stochastic_optimizer optimizer, size_t epochs);
 }
