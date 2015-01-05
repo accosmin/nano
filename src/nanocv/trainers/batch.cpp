00ca02fbdf9ad104d576243c591bcf328cf217fe
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-01-05 13:33:18 +0100

    finer tuning of the regularization term

--- src/nanocv/trainers/batch.cpp
+++ src/nanocv/trainers/batch.cpp
@@ -87,7 +87,7 @@ namespace ncv
                 // tune the regularization factor (if needed)
                 if (accumulator_t::can_regularize(criterion))
                 {
-                        return log10_min_search(op, -4.0, +4.0, 0.5, 4).first;
+                        return log10_min_search(op, -4.0, +4.0, 0.2, 4).first;
                 }
 
                 else
