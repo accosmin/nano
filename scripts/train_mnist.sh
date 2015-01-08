7f9b264c649e2323db4850fb41a8575ccb36da2a
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-01-08 11:26:36 +0100

    update training script

--- scripts/train_mnist.sh
+++ scripts/train_mnist.sh
@@ -31,19 +31,19 @@ mlp3=${mlp2}"linear:dims=32;act-snorm;"
 outlayer="linear:dims=10;"
 
 # trainers
-stoch_sg="--trainer stochastic --trainer-params opt=sg,epoch=64"
-stoch_sga="--trainer stochastic --trainer-params opt=sga,epoch=64"
-stoch_sia="--trainer stochastic --trainer-params opt=sia,epoch=64"
-stoch_nag="--trainer stochastic --trainer-params opt=nag,epoch=64"
-stoch_adagrad="--trainer stochastic --trainer-params opt=adagrad,epoch=64"
+stoch_sg="--trainer stochastic --trainer-params opt=sg,epoch=32"
+stoch_sga="--trainer stochastic --trainer-params opt=sga,epoch=32"
+stoch_sia="--trainer stochastic --trainer-params opt=sia,epoch=32"
+stoch_nag="--trainer stochastic --trainer-params opt=nag,epoch=32"
+stoch_adagrad="--trainer stochastic --trainer-params opt=adagrad,epoch=32"
 
-mbatch_lbfgs="--trainer minibatch --trainer-params opt=lbfgs,epoch=64,eps=1e-4"
+mbatch_lbfgs="--trainer minibatch --trainer-params opt=lbfgs,epoch=32,eps=1e-4"
 
 batch_lbfgs="--trainer batch --trainer-params opt=lbfgs,iters=128,eps=1e-4"
 
 # train models
 #for model in `echo "mlp0 mlp1 mlp2 mlp3 conv100_max conv50_max conv25_max"`
-for model in `echo "conv100_max conv50_max conv25_max"`
+for model in `echo "mlp0 mlp1 mlp2 mlp3 conv100_max conv50_max conv25_max"`
 do
         #for trainer in `echo "mbatch_lbfgs batch_lbfgs"`
         for trainer in `echo "mbatch_lbfgs"`
@@ -53,8 +53,7 @@ do
 	        #fn_train ${dir_exp_mnist} ${trainer}_${model}_var ${params} ${!trainer} ${var_crit} ${!model}${outlayer}
         done
 
-        #for trainer in `echo "stoch_nag stoch_adagrad stoch_sg stoch_sga stoch_sia"`
-        for trainer in `echo "stoch_sia"`
+        for trainer in `echo "stoch_nag stoch_adagrad stoch_sg stoch_sga stoch_sia"`
         do
                 fn_train ${dir_exp_mnist} ${trainer}_${model} ${params} ${!trainer} ${avg_crit} ${!model}${outlayer}
         	#fn_train ${dir_exp_mnist} ${trainer}_${model}_l2n ${params} ${!trainer} ${l2n_crit} ${!model}${outlayer}
