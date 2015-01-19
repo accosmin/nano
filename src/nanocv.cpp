d2ff01b8678eda8c66582bc9fdeac0ed1466ea8c
Cosmin Atanasoaei <accosmin@gmail.com>
2015-01-19 22:06:35 +0100

    realistic synthetic dataset (to be continued)

--- src/nanocv.cpp
+++ src/nanocv.cpp
@@ -11,6 +11,7 @@
 #include "tasks/task_stl10.h"
 #include "tasks/task_svhn.h"
 #include "tasks/task_norb.h"
+#include "tasks/task_syn_dots.h"
 
 #include "layers/layer_activation_unit.h"
 #include "layers/layer_activation_tanh.h"
@@ -62,6 +63,7 @@ namespace ncv
                 task_manager_t::instance().add("stl10", stl10_task_t());
                 task_manager_t::instance().add("svhn", svhn_task_t());
                 task_manager_t::instance().add("norb", norb_task_t());
+                task_manager_t::instance().add("syn-dots", syn_dots_task_t());
 
                 // register layers
                 layer_manager_t::instance().add("act-unit", unit_activation_layer_t());
---
+++ src/nanocv/tasks/task_syn_dots.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "task.h"
+
+namespace ncv
+{
+        ///
+        /// \brief synthetic task to count dots, represented as random bright rectangles, overimposed on random images
+        ///
+        /// parameters:
+        ///     rows=16[8,32]           - patch size in pixels (rows)
+        ///     cols=16[8,32]           - patch size in pixels (columns)
+        ///     dims=4[2,8]             - number of outputs (= maximum number of dots in an image)
+        ///     color=rgba[,luma]       - color mode
+        ///     size=1024[256,16*1024]  - number of samples (training + validation)
+        ///
+        class syn_dots_task_t : public task_t
+        {
+        public:
+
+                NANOCV_MAKE_CLONABLE(syn_dots_task_t,
+                                     "synthetic test task to count dots, "\
+                                     "rows=12[8,32],cols=12[8,32],dims=4[2,8],color=rgba[,luma],size=1024[256,16*1024]")
+
+                // constructor
+                explicit syn_dots_task_t(const string_t& configuration = string_t());
+
+                // load images from the given directory
+                virtual bool load(const string_t&) override;
+
+                // access functions
+                virtual size_t n_rows() const override { return m_rows; }
+                virtual size_t n_cols() const override { return m_cols; }
+                virtual size_t n_outputs() const override { return m_outputs; }
+                virtual size_t n_folds() const override { return m_folds; }
+                virtual color_mode color() const override { return m_color; }
+
+        private:
+
+                // attributes
+                size_t          m_rows;
+                size_t          m_cols;
+                size_t          m_outputs;
+                size_t          m_folds;
+                color_mode      m_color;
+                size_t          m_size;
+        };
+}
