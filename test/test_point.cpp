d196fcb07a8d7018ab6b77ac373430ab7b521cc9
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-01-21 15:54:15 +0100

    refactor tests

--- test/test_point.cpp
+++ test/test_point.cpp
@@ -6,10 +6,21 @@
 
 #include "geom.h"
 
+namespace test
+{
+        void build_point(ncv::coord_t x, ncv::coord_t y)
+        {
+                const ncv::point_t point(x, y);
+
+                BOOST_CHECK_EQUAL(point.x(), x);
+                BOOST_CHECK_EQUAL(point.y(), y);
+        }
+}
+
 BOOST_AUTO_TEST_CASE(test_point)
 {
-        const ncv::point_t point(3, 7);
-
-        BOOST_CHECK_EQUAL(point.x(), 3);
-        BOOST_CHECK_EQUAL(point.y(), 7);
+        test::build_point(3, 7);
+        test::build_point(7, 3);
+        test::build_point(-5, -1);
+        test::build_point(-9, +1);
 }
--- test/test_rect.cpp
+++ test/test_rect.cpp
@@ -6,14 +6,32 @@
 
 #include "geom.h"
 
+namespace test
+{
+        void build_rect(ncv::coord_t x, ncv::coord_t y, ncv::coord_t w, ncv::coord_t h)
+        {
+                const ncv::rect_t rect(x, y, w, h);
+
+                BOOST_CHECK_EQUAL(rect.left(),          x);
+                BOOST_CHECK_EQUAL(rect.top(),           y);
+                BOOST_CHECK_EQUAL(rect.right(),         x + w);
+                BOOST_CHECK_EQUAL(rect.bottom(),        y + h);
+
+                BOOST_CHECK_EQUAL(rect.area(),          w * h);
+                BOOST_CHECK_EQUAL(rect.width(),         w);
+                BOOST_CHECK_EQUAL(rect.height(),        h);
+                BOOST_CHECK_EQUAL(rect.rows(),          h);
+                BOOST_CHECK_EQUAL(rect.cols(),          w);
+
+                BOOST_CHECK_EQUAL(rect.valid(),         rect.area() > 0);
+        }
+}
+
 BOOST_AUTO_TEST_CASE(test_rect_construction)
 {
-        const ncv::rect_t rect(1, 2, 8, 6);
-
-        BOOST_CHECK_EQUAL(rect.left(), 1);
-        BOOST_CHECK_EQUAL(rect.top(), 2);
-        BOOST_CHECK_EQUAL(rect.right(), 9);
-        BOOST_CHECK_EQUAL(rect.bottom(), 8);
+        test::build_rect(0, 0, 8, 6);
+        test::build_rect(1, 2, 3, 4);
+        test::build_rect(-1, -1, 7, 5);
 }
 
 BOOST_AUTO_TEST_CASE(test_rect_operations)
