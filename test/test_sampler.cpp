30b57e02f6dd26362dc4fe62c67d1cd1140d9730
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-01-22 11:15:07 +0100

    test sampler

--- test/test_sampler.cpp
+++ test/test_sampler.cpp
@@ -1,35 +1,43 @@
-#include "nanocv.h"
-#include "tasks/task_syn_dots.h"
-#include <algorithm>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "test_sampler"
 
-using namespace ncv;
+#include <boost/test/unit_test.hpp>
+#include "nanocv/tasks/task_syn_dots.h"
+#include "nanocv/sampler.h"
+#include "util/logger.h"
+#include "util/timer.h"
 
-bool check_samples(const samples_t& samples, ncv::fold_t fold)
+namespace test
 {
-        return std::find_if(samples.begin(), samples.end(),
-               [&] (const sample_t& sample) { return sample.m_fold != fold; }) == samples.end();
+        using namespace ncv;
+
+        bool check_fold(const samples_t& samples, ncv::fold_t fold)
+        {
+                return std::find_if(samples.begin(), samples.end(),
+                       [&] (const sample_t& sample) { return sample.m_fold != fold; }) == samples.end();
+        }
 }
 
-int main(int argc, char *argv[])
+BOOST_AUTO_TEST_CASE(test_sampler)
 {
-        ncv::init();
+        using namespace ncv;
 
         const size_t n_samples = 1000;
         const size_t n_rand_samples = n_samples / 4;
 
         syn_dots_task_t task("rows=28,cols=28,color=luma,dims=5,size=" + text::to_string(n_samples));
-        task.load("");
+        BOOST_CHECK_EQUAL(task.load(""), true);
 
         for (size_t f = 0; f < task.n_folds(); f ++)
         {
-                const ncv::timer_t timer;
-
                 const fold_t train_fold = {f, protocol::train};
                 const fold_t test_fold = {f, protocol::test};
 
                 const string_t header = "fold [" + text::to_string(f + 1) + "/" + text::to_string(task.n_folds()) + "]";
                 const string_t train_header = header + " protocol [" + text::to_string(protocol::train) + "]";
                 const string_t test_header = header + " protocol [" + text::to_string(protocol::test) + "]";
+
+                const ncv::timer_t timer;
 
                 // batch training samples
                 sampler_t sampler(task);
@@ -62,40 +70,21 @@ int main(int argc, char *argv[])
 
                 log_info() << "fold [" << (f + 1) << "/" << task.n_folds() << "]: sampled in " << timer.elapsed() << ".";
 
-                // check results
-                if (train_batch_samples.size() + test_batch_samples.size() != n_samples)
-                {
-                        log_error() << "invalid number of batch samples "
-                                    << (train_batch_samples.size() + test_batch_samples.size())
-                                    << "/" << n_samples << "!";
-                }
+                // check training & testing split
+                BOOST_CHECK_EQUAL(train_batch_samples.size() + test_batch_samples.size(), n_samples);
 
-                if (!check_samples(train_batch_samples, {f, protocol::train}))
-                {
-                        log_error() << "invalid batch training samples!";
-                }
-                if (!check_samples(train_urand_samples, {f, protocol::train}))
-                {
-                        log_error() << "invalid random uniform training samples!";
-                }
+                // check training samples
+                BOOST_CHECK(test::check_fold(train_batch_samples, {f, protocol::train}));
+                BOOST_CHECK(test::check_fold(train_urand_samples, {f, protocol::train}));
 
                 ncv::print(train_header + " batch", train_batch_samples);
                 ncv::print(train_header + " urand", train_urand_samples);
 
-                if (!check_samples(test_batch_samples, {f, protocol::test}))
-                {
-                        log_error() << "invalid batch testing samples!";
-                }
-                if (!check_samples(test_urand_samples, {f, protocol::test}))
-                {
-                        log_error() << "invalid random uniform testing samples!";
-                }
+                // check test samples
+                BOOST_CHECK(test::check_fold(test_batch_samples, {f, protocol::test}));
+                BOOST_CHECK(test::check_fold(test_urand_samples, {f, protocol::test}));
 
                 ncv::print(test_header + " batch", test_batch_samples);
                 ncv::print(test_header + " urand", test_urand_samples);
         }
-
-        // OK
-        log_info() << done;
-        return EXIT_SUCCESS;
 }
