a34e966339cf7e35bcc65e2e956504e29b2f89ef
Cosmin Atanasoaei <accosmin@gmail.com>
2015-01-28 20:11:44 +0100

    skip already downloaded tasks

--- scripts/download_tasks.h
+++ scripts/download_tasks.h
@@ -5,32 +5,32 @@ source common_train.sh
 # SVHN dataset
 mkdir ${dir_db_svhn}
 
-wget http://ufldl.stanford.edu/housenumbers/train_32x32.mat -P ${dir_db_svhn}
-wget http://ufldl.stanford.edu/housenumbers/extra_32x32.mat -P ${dir_db_svhn}
-wget http://ufldl.stanford.edu/housenumbers/test_32x32.mat -P ${dir_db_svhn}
+wget -N http://ufldl.stanford.edu/housenumbers/train_32x32.mat -P ${dir_db_svhn}
+wget -N http://ufldl.stanford.edu/housenumbers/extra_32x32.mat -P ${dir_db_svhn}
+wget -N http://ufldl.stanford.edu/housenumbers/test_32x32.mat -P ${dir_db_svhn}
 
 # MNIST dataset
 mkdir ${dir_db_mnist}
 
-wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -P ${dir_db_mnist}
-wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz -P ${dir_db_mnist}
-wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -P ${dir_db_mnist}
-wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz -P ${dir_db_mnist}
+wget -N http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz -P ${dir_db_mnist}
+wget -N http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz -P ${dir_db_mnist}
+wget -N http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz -P ${dir_db_mnist}
+wget -N http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz -P ${dir_db_mnist}
 
 # STL10 dataset
 mkdir ${dir_db_stl10}
 
-wget http://ai.stanford.edu/~acoates/stl10/stl10_matlab.tar.gz -P ${dir_db_stl10}
+wget -N http://ai.stanford.edu/~acoates/stl10/stl10_matlab.tar.gz -P ${dir_db_stl10}
 
 # CIFAR10 dataset
 mkdir ${dir_db_cifar10}
 
-wget http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz -P ${dir_db_cifar10} 
+wget -N http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz -P ${dir_db_cifar10} 
 
 # CIFAR100 dataset
 mkdir ${dir_db_cifar100}
 
-wget http://www.cs.toronto.edu/~kriz/cifar-100-binary.tar.gz -P ${dir_db_cifar100}
+wget -N http://www.cs.toronto.edu/~kriz/cifar-100-binary.tar.gz -P ${dir_db_cifar100}
 
 # NORB dataset
 mkdir ${dir_db_norb}
