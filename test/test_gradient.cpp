df6b6cd45cee556df2197ddc9091aafd7a0f8f33
Cosmin Atanasoaei <accosmin@gmail.com>
2015-01-28 19:34:32 +0100

    merge gradient tests to share more code

--- test/test_gradient.cpp
+++ test/test_gradient.cpp
@@ -1,5 +1,5 @@
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE "test_gradient_params"
+#define BOOST_TEST_MODULE "test_gradient"
 
 #include <boost/test/unit_test.hpp>
 #include "libnanocv/nanocv.h"
@@ -22,7 +22,8 @@ namespace test
         size_t n_failures2 = 0;
         size_t n_failures3 = 0;
 
-        void test_grad(const string_t& header, const string_t& loss_id, const model_t& model, accumulator_t& acc_params)
+        void test_grad_params(const string_t& header, const string_t& loss_id, const model_t& model,
+                accumulator_t& acc_params)
         {
                 random_t<size_t> rand(8, 16);
 
@@ -115,7 +116,7 @@ namespace test
                 }
         }
 
-        void test_grad(const string_t& header, const string_t& loss_id, const model_t& model)
+        void test_grad_params(const string_t& header, const string_t& loss_id, const model_t& model)
         {
                 // check all criteria
                 const strings_t criteria = criterion_manager_t::instance().ids();
@@ -125,12 +126,100 @@ namespace test
                         const size_t n_threads = 1 + (rand() % 2);
 
                         accumulator_t acc_params(model, n_threads, criterion, criterion_t::type::vgrad, 1.0);
-                        test_grad(header + "[criterion = " + criterion + "]", loss_id, model, acc_params);
+                        test_grad_params(header + "[criterion = " + criterion + "]", loss_id, model, acc_params);
+                }
+        }
+
+        void test_grad_inputs(const string_t& header, const string_t& loss_id, const model_t& model)
+        {
+                rmodel_t rmodel_inputs = model.clone();
+
+                const size_t n_tests = 256;
+
+                const rloss_t rloss = loss_manager_t::instance().get(loss_id);
+                const loss_t& loss = *rloss;
+
+                const size_t psize = model.psize();
+                const size_t isize = model.isize();
+                const size_t osize = model.osize();
+
+                vector_t params(psize);
+                vector_t target(osize);
+                tensor_t input(model.idims(), model.irows(), model.icols());
+
+                // optimization problem (wrt parameters & inputs): size
+                auto fn_inputs_size = [&] ()
+                {
+                        return isize;
+                };
+
+                // optimization problem (wrt parameters & inputs): function value
+                auto fn_inputs_fval = [&] (const vector_t& x)
+                {
+                        rmodel_inputs->load_params(params);
+
+                        const vector_t output = rmodel_inputs->output(x).vector();
+
+                        return loss.value(target, output);
+                };
+
+                // optimization problem (wrt parameters & inputs): function value & gradient
+                auto fn_inputs_grad = [&] (const vector_t& x, vector_t& gx)
+                {
+                        rmodel_inputs->load_params(params);
+
+                        const vector_t output = rmodel_inputs->output(x).vector();
+
+                        gx = rmodel_inputs->ginput(loss.vgrad(target, output)).vector();
+                        return loss.value(target, output);
+                };
+
+                // construct optimization problem: analytic gradient and finite difference approximation
+                const opt_problem_t problem_analytic_inputs(fn_inputs_size, fn_inputs_fval, fn_inputs_grad);
+                const opt_problem_t problem_aproxdif_inputs(fn_inputs_size, fn_inputs_fval);
+
+                for (size_t t = 0; t < n_tests; t ++)
+                {
+                        random_t<scalar_t> prgen(-1.0, +1.0);
+                        random_t<scalar_t> irgen(-0.1, +0.1);
+                        random_t<size_t> trgen(0, osize - 1);
+
+                        prgen(params.data(), params.data() + psize);
+                        target = ncv::class_target(trgen(), osize);
+                        irgen(input.data(), input.data() + isize);
+
+                        vector_t analytic_inputs_grad, aproxdif_inputs_grad;
+
+                        problem_analytic_inputs(input.vector(), analytic_inputs_grad);
+                        problem_aproxdif_inputs(input.vector(), aproxdif_inputs_grad);
+
+                        const scalar_t delta = (analytic_inputs_grad - aproxdif_inputs_grad).lpNorm<Eigen::Infinity>();
+
+                        // update statistics
+                        const thread_pool_t::lock_t lock(test::mutex);
+
+                        n_checks ++;
+                        if (!math::close(delta, scalar_t(0), math::epsilon1<scalar_t>()))
+                        {
+                                n_failures1 ++;
+                        }
+                        if (!math::close(delta, scalar_t(0), math::epsilon2<scalar_t>()))
+                        {
+                                n_failures2 ++;
+                        }
+                        if (!math::close(delta, scalar_t(0), math::epsilon3<scalar_t>()))
+                        {
+                                n_failures3 ++;
+
+                                BOOST_CHECK_LE(delta, math::epsilon3<scalar_t>());
+
+                                log_error() << header << ": error = " << delta << "/" << math::epsilon3<scalar_t>() << "!";
+                        }
                 }
         }
 }
 
-BOOST_AUTO_TEST_CASE(test_gradient_params)
+BOOST_AUTO_TEST_CASE(test_gradient)
 {
         using namespace ncv;
 
@@ -162,7 +251,8 @@ BOOST_AUTO_TEST_CASE(test_gradient_params)
                         }
 
                         // check with the given loss
-                        test::test_grad("[loss = " + loss_id + "]", loss_id, *model);
+                        test::test_grad_params("[loss = " + loss_id + "]", loss_id, *model);
+                        test::test_grad_inputs("[loss = " + loss_id + "]", loss_id, *model);
                 });
         };
 
