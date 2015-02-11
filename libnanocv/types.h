d769c803036e85354df759f6f04a2a50c502b7ff
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-02-11 11:37:37 +0100

    support for different scalar types

--- libnanocv/types.h
+++ libnanocv/types.h
@@ -11,7 +11,15 @@ namespace ncv
         typedef std::size_t                                     size_t;
         typedef std::vector<size_t>                             indices_t;
 
+#ifdef NANOCV_WITH_FLOAT
+        typedef float                                           scalar_t;
+#elseif MANOCV_WITH_DOUBLE
         typedef double                                          scalar_t;
+#elseif NANOCV_WIDTH_LONG_DOUBLE
+        typedef long double                                     scalar_t;
+#else
+        typedef double                                          scalar_t;
+#endif
         typedef std::vector<scalar_t>                           scalars_t;
 
         typedef tensor::vector_types_t<scalar_t>::tvector       vector_t;
