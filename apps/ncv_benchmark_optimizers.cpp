9dba5bae6b71330869a820c40ddeee881be91f18
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-02-18 17:29:20 +0100

    logging

--- apps/ncv_benchmark_optimizers.cpp
+++ apps/ncv_benchmark_optimizers.cpp
@@ -22,7 +22,7 @@ void test_optimizer(model_t& model, ttrainer trainer, const string_t& name, tabu
         stats_t<scalar_t> terrors;
         stats_t<scalar_t> verrors;
 
-        log_info() << "running " << name << " ...";
+        log_info() << "<<< running " << name << " ...";
 
         const size_t usec = ncv::measure_robustly_usec([&] ()
         {
