2304fd86c9b36f48688fb2d6d7e85dc9e793fff5
Cosmin Atanasoaei <accosmin@gmail.com>
2015-02-18 20:54:29 +0100

    more CGD methods to test

--- test/test_optimizers.cpp
+++ test/test_optimizers.cpp
@@ -73,6 +73,13 @@ namespace test
                 {
                         batch_optimizer::GD,
                         batch_optimizer::CGD,
+//                        batch_optimizer::CGD_CD,
+                        batch_optimizer::CGD_DY,
+//                        batch_optimizer::CGD_FR,
+//                        batch_optimizer::CGD_HS,
+                        batch_optimizer::CGD_LS,
+                        batch_optimizer::CGD_N,
+                        batch_optimizer::CGD_PR,
                         batch_optimizer::LBFGS
                 };
 
