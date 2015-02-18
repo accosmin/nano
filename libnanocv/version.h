e3179de46acc7e578616f5096ff162f15ab63cf9
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-02-18 12:13:53 +0100

    bump version

--- libnanocv/version.h
+++ libnanocv/version.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define NANOCV_MAJOR_VERSION            0
-#define NANOCV_MINOR_VERSION            1
+#define NANOCV_MINOR_VERSION            2
 #define NANOCV_REVISION_VERSION         0
