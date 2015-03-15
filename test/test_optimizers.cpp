f9fd9061e67319ca9fc0e2f5f331fa00cbf83272
Cosmin Atanasoaei <accosmin@gmail.com>
2015-03-15 17:09:24 +0100

    extend unit test to CGD methods that work reliably

--- test/test_optimizers.cpp
+++ test/test_optimizers.cpp
@@ -73,7 +73,7 @@ namespace test
                 const opt_opsize_t& fn_size, const opt_opfval_t& fn_fval, const opt_opgrad_t& fn_grad,
                 const std::vector<std::pair<vector_t, scalar_t>>& solutions)
         {
-                const size_t iterations = 128 * 1024;
+                const size_t iterations = 4 * 1024;
                 const scalar_t epsilon = std::numeric_limits<scalar_t>::epsilon();
 
                 const size_t trials = 8 * 1024;
@@ -96,13 +96,13 @@ namespace test
                 const auto optimizers =
                 {
 //                        batch_optimizer::GD,
-////                        batch_optimizer::CGD_CD,
-////                        batch_optimizer::CGD_DY,
-////                        batch_optimizer::CGD_FR,
+//                        batch_optimizer::CGD_CD,
+//                        batch_optimizer::CGD_DY,
+//                        batch_optimizer::CGD_FR,
 //                        batch_optimizer::CGD_HS,
-////                        batch_optimizer::CGD_LS,
-//                        batch_optimizer::CGD_PR,
-//                        batch_optimizer::CGD_N,
+//                        batch_optimizer::CGD_LS,
+                        batch_optimizer::CGD_PR,
+                        batch_optimizer::CGD_N,
                         batch_optimizer::LBFGS
                 };
 
