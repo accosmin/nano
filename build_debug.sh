ba18b910cc0b6918a5fe48b7005877d87250b8e6
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-04-23 09:47:31 +0200

    activate ASAN/LSAN/TSAN sanitizers for clang as well

--- build_debug.sh
+++ build_debug.sh
@@ -5,7 +5,7 @@ build_type=Debug
 bash build.sh --build-dir ./build-debug --build-type ${build_type} --asan OFF --lsan OFF --tsan OFF
 
 # does not work with clang!
-#bash build.sh --build-dir ./build-debug-asan --build-type ${build_type} --asan ON --lsan OFF --tsan OFF
-#bash build.sh --build-dir ./build-debug-lsan --build-type ${build_type} --asan OFF --lsan ON --tsan OFF
-#bash build.sh --build-dir ./build-debug-tsan --build-type ${build_type} --asan OFF --lsan OFF --tsan ON
+bash build.sh --build-dir ./build-debug-asan --build-type ${build_type} --asan ON --lsan OFF --tsan OFF
+bash build.sh --build-dir ./build-debug-lsan --build-type ${build_type} --asan OFF --lsan ON --tsan OFF
+bash build.sh --build-dir ./build-debug-tsan --build-type ${build_type} --asan OFF --lsan OFF --tsan ON
 
