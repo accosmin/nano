4ff39418e464d70e6fdcf3e16ea58f2b70677ef5
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-05-13 11:36:08 +0200

    cleanup implementation, added documentation

--- nanocv/manager.hpp
+++ nanocv/manager.hpp
@@ -10,6 +10,7 @@ namespace ncv
 {
         ///
         /// \brief manager: used to manage different object types associated with ID strings.
+        ///
         /// hint: use register_object<base, derived> to register objects to the manager.
         ///
         template
@@ -24,93 +25,85 @@ namespace ncv
                 typedef tstring                                                 string_t;
                 typedef typename clonable_t<tobject, tstring>::robject_t        robject_t;
 
-                // manage prototypes
+                ///
+                /// \brief add a new object with the given ID
+                ///
                 bool add(const tstring& id, const tobject& proto)
                 {
-                        return _add(id, proto);
+                        return m_protos.emplace(id, proto.clone()).second;
                 }
+
+                ///
+                /// \brief check if an objects was registered, given its ID
+                ///
                 bool has(const tstring& id) const
-                {
-                        return _has(id);
-                }
-                robject_t get(const tstring& id) const
-                {
-                        return _get(id);
-                }
-                robject_t get(const tstring& id, const tstring& params) const
-                {
-                        return _get(id, params);
-                }
-
-                // access functions
-                std::vector<tstring> ids() const { return _ids(); }
-                std::vector<tstring> descriptions() const { return _descriptions(); }
-
-        private:
-
-                // object prototypes
-                typedef std::map<tstring, robject_t>       	protos_t;
-                typedef typename protos_t::const_iterator       protos_const_it;
-                typedef typename protos_t::iterator		protos_it;
-                
-        private:
-                
-                bool _add(const tstring& id, const tobject& proto)
-                {
-                        return m_protos.insert(typename protos_t::value_type(id, proto.clone())).second;
-                }
-
-                bool _has(const tstring& id) const
                 {
                         return m_protos.find(id) != m_protos.end();
                 }
 
-                robject_t _get(const tstring& id) const
+                ///
+                /// \brief retrieve the object associated with the given ID
+                ///
+                robject_t get(const tstring& id) const
                 {
-                        const protos_const_it it = m_protos.find(id);
-                        if (it == m_protos.end())
-                        {
-                                throw std::runtime_error("invalid object id <" + id + ">!");
-                        }
+                        const auto it = m_protos.find(id);
+                        assert_it(id, it);
                         return it->second->clone();
                 }
 
-                robject_t _get(const tstring& id, const tstring& params) const
+                ///
+                /// \brief retrieve the object associated with the given ID, constructed from the given parameters
+                ///
+                robject_t get(const tstring& id, const tstring& params) const
                 {
-                        const protos_const_it it = m_protos.find(id);
-                        if (it == m_protos.end())
-                        {
-                                throw std::runtime_error("invalid object id <" + id + ">!");
-                        }
+                        const auto it = m_protos.find(id);
+                        assert_it(id, it);
                         return it->second->make(params);
                 }
 
-		template <typename TFunctor>
-                std::vector<tstring> _collect(const TFunctor& fun) const
+                ///
+                /// \brief get the IDs of all registered objects
+                ///
+                std::vector<tstring> ids() const
+                {
+                        return collect([] (const auto& it) { return it.first; });
+                }
+
+                ///
+                /// \brief get the descriptions of all registered objects
+                ///
+                std::vector<tstring> descriptions() const
+                {
+                        return collect([] (const auto& it) { return it.second->description(); });
+                }
+
+        private:
+
+                template <typename tfunctor>
+                std::vector<tstring> collect(const tfunctor& fun) const
 		{
                         std::vector<tstring> result;
-                        for (protos_const_it it = m_protos.cbegin(); it != m_protos.cend(); ++ it)
+                        for (const auto& it : m_protos)
                         {
                                 result.push_back(fun(it));
                         }
 
 			return result;
-		}
-
-                std::vector<tstring> _ids() const
-                {
-			return _collect([] (const protos_const_it& it) { return it->first; });
                 }
 
-                std::vector<tstring> _descriptions() const
-		{
-                        return _collect([] (const protos_const_it& it) { return it->second->description(); });
-		}
+                template <typename titerator>
+                void assert_it(const tstring& id, const titerator& it) const
+                {
+                          if (it == m_protos.end())
+                          {
+                                  throw std::runtime_error("invalid object id <" + id + ">!");
+                          }
+                }
 		
 	private:
 
                 // attributes
-                protos_t                m_protos;
+                std::map<tstring, robject_t>    m_protos;       ///< registered objects
         };
 
         ///
