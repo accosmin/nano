2dc8b11e4338b832ef465e49a3ebf3f6851fe42e
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-05-20 11:02:14 +0200

    check compiler flags in CMake (e.g. sanitizers, C++14)

--- build_debug.sh
+++ build_debug.sh
@@ -5,8 +5,7 @@ build_sys=ninja
 
 bash build.sh --build-dir ./build-debug --build-type ${build_type} --build-sys ${build_sys} --asan OFF --lsan OFF --tsan OFF
 
-# does not work with XCode's clang!
-#bash build.sh --build-dir ./build-debug-asan --build-type ${build_type} --build-sys ${build_sys} --asan ON --lsan OFF --tsan OFF
-#bash build.sh --build-dir ./build-debug-lsan --build-type ${build_type} --build-sys ${build_sys} --asan OFF --lsan ON --tsan OFF
-#bash build.sh --build-dir ./build-debug-tsan --build-type ${build_type} --build-sys ${build_sys} --asan OFF --lsan OFF --tsan ON
+bash build.sh --build-dir ./build-debug-asan --build-type ${build_type} --build-sys ${build_sys} --asan ON --lsan OFF --tsan OFF
+bash build.sh --build-dir ./build-debug-lsan --build-type ${build_type} --build-sys ${build_sys} --asan OFF --lsan ON --tsan OFF
+bash build.sh --build-dir ./build-debug-tsan --build-type ${build_type} --build-sys ${build_sys} --asan OFF --lsan OFF --tsan ON
 
