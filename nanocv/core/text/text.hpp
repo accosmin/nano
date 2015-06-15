06aacba23aae1de7a22622827e06380416eecfc1
Cosmin Atanasoaei <accosmin@gmail.com>
2015-06-15 21:50:03 +0200

    split sources into smaller independent libraries

---
+++ nanocv/core/text/text.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "to_string.hpp"
+#include "concatenate.hpp"
+#include "from_params.hpp"
+#include "from_string.hpp"
+
--- nanocv/text.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "text/to_string.hpp"
-#include "text/concatenate.hpp"
-#include "text/from_params.hpp"
-#include "text/from_string.hpp"
-
