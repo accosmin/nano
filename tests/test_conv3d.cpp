f457b11d1bc986863a14275dfbca4d82acfa22a6
Cosmin Atanasoaei <accosmin@gmail.com>
2015-06-29 20:24:55 +0200

    start unit test for 3D convolution

---
+++ tests/test_conv3d.cpp
@@ -0,0 +1,93 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "test_conv2d"
+
+#include <boost/test/unit_test.hpp>
+#include "nanocv/tensor.h"
+#include "nanocv/math/abs.hpp"
+#include "nanocv/math/conv3d.hpp"
+#include "nanocv/math/random.hpp"
+#include "nanocv/math/epsilon.hpp"
+#include "nanocv/tensor/conv2d_toeplitz.hpp"
+
+namespace test
+{
+        using namespace ncv;
+
+        template
+        <
+                typename top,
+                typename ttensori,
+                typename ttensork,
+                typename ttensoro
+        >
+        decltype(auto) test_output(const top& op, const ttensori& idata, const ttensork& kdata, ttensoro&& odata)
+        {
+                odata.setZero();
+
+                op(idata, kdata, odata);
+
+                return odata.vector().sum();
+        }
+
+        void test_conv3d(int isize, int idims, int ksize, int odims)
+        {
+                const int osize = isize - ksize + 1;
+
+                tensor_t idata(idims, isize, isize);
+                tensor_t kdata(odims * idims, ksize, ksize);
+                tensor_t odata(odims, osize, osize);
+
+                random_t<scalar_t> rng(-1.0, 1.0);
+
+                idata.setRandom(rng);
+                kdata.setRandom(rng);
+                odata.setRandom(rng);
+
+                idata.vector() /= isize;
+                kdata.vector() /= ksize;
+                odata.vector() /= osize;
+
+                const auto output_dyn = test_output(math::conv3d_output<tensor_t, tensor_t, tensor_t&>, idata, kdata, odata);
+
+                const scalar_t epsilon = math::epsilon1<scalar_t>();
+
+                BOOST_CHECK_LE(math::abs(output_dyn - output_dyn), epsilon);
+        }
+}
+
+BOOST_AUTO_TEST_CASE(test_conv3d)
+{
+        using namespace ncv;
+
+        const int min_isize = 4;
+        const int max_isize = 48;
+
+        const int min_ksize = 1;
+        const int max_ksize = 15;
+
+        const int min_idims = 4;
+        const int max_idims = 64;
+
+        const int min_odims = 4;
+        const int max_odims = 64;
+
+        const int n_tests = 16;
+
+        for (int isize = min_isize; isize <= max_isize; isize += 4)
+        {
+                for (int idims = min_idims; idims <= max_idims; idims *= 2)
+                {
+                        for (int ksize = min_ksize; ksize <= max_ksize; ksize ++)
+                        {
+                                for (int odims = min_odims; odims <= max_odims; odims *= 2)
+                                {
+                                        for (int t = 0; t < n_tests; t ++)
+                                        {
+                                                test::test_conv3d(isize, idims, ksize, odims);
+                                        }
+                                }
+                        }
+                }
+        }
+}
+
