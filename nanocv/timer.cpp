73840fe1e73bafb9e1d4f2a0268dbc9478d2157d
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-07-23 08:07:04 +0200

    hide some more of the implementation details

--- nanocv/timer.cpp
+++ nanocv/timer.cpp
@@ -10,6 +10,49 @@ namespace ncv
         typedef std::chrono::duration<std::size_t, std::micro>  microseconds_t;
         typedef std::chrono::duration<std::size_t>              seconds_t;
 
+        namespace
+        {
+                decltype(auto) now()
+                {
+                        return std::chrono::high_resolution_clock::now();
+                }
+
+                std::string miliseconds_to_string(std::size_t count)
+                {
+                        static const std::size_t size_second = 1000;
+                        static const std::size_t size_minute = 60 * size_second;
+                        static const std::size_t size_hour = 60 * size_minute;
+                        static const std::size_t size_day = 24 * size_hour;
+
+                        const std::size_t days = count / size_day; count -= days * size_day;
+                        const std::size_t hours = count / size_hour; count -= hours * size_hour;
+                        const std::size_t minutes = count / size_minute; count -= minutes * size_minute;
+                        const std::size_t seconds = count / size_second; count -= seconds * size_second;
+                        const std::size_t miliseconds = count;
+
+                        std::stringstream stream;
+                        if (days > 0)
+                        {
+                                stream << days << "d:";
+                        }
+                        if (days > 0 || hours > 0)
+                        {
+                                stream << std::setfill('0') << std::setw(2) << hours << "h:";
+                        }
+                        if (days > 0 || hours > 0 || minutes > 0)
+                        {
+                                stream << std::setfill('0') << std::setw(2) << minutes << "m:";
+                        }
+                        if (days > 0 || hours > 0 || minutes > 0 || seconds > 0)
+                        {
+                                stream << std::setfill('0') << std::setw(2) << seconds << "s:";
+                        }
+                        stream << std::setfill('0') << std::setw(3) << miliseconds << "ms";
+
+                        return stream.str();
+                }
+        }
+
         timer_t::timer_t()
                 : m_start(now())
         {
@@ -41,45 +84,5 @@ namespace ncv
         {
                 const auto duration = std::chrono::duration_cast<microseconds_t>(now() - m_start);
                 return duration.count();
-        }
-
-        timer_t::time_t timer_t::now()
-        {
-                return std::chrono::high_resolution_clock::now();
-        }
-
-        std::string timer_t::miliseconds_to_string(std::size_t count)
-        {
-                static const std::size_t size_second = 1000;
-                static const std::size_t size_minute = 60 * size_second;
-                static const std::size_t size_hour = 60 * size_minute;
-                static const std::size_t size_day = 24 * size_hour;
-
-                const std::size_t days = count / size_day; count -= days * size_day;
-                const std::size_t hours = count / size_hour; count -= hours * size_hour;
-                const std::size_t minutes = count / size_minute; count -= minutes * size_minute;
-                const std::size_t seconds = count / size_second; count -= seconds * size_second;
-                const std::size_t miliseconds = count;
-
-                std::stringstream stream;
-                if (days > 0)
-                {
-                        stream << days << "d:";
-                }
-                if (days > 0 || hours > 0)
-                {
-                        stream << std::setfill('0') << std::setw(2) << hours << "h:";
-                }
-                if (days > 0 || hours > 0 || minutes > 0)
-                {
-                        stream << std::setfill('0') << std::setw(2) << minutes << "m:";
-                }
-                if (days > 0 || hours > 0 || minutes > 0 || seconds > 0)
-                {
-                        stream << std::setfill('0') << std::setw(2) << seconds << "s:";
-                }
-                stream << std::setfill('0') << std::setw(3) << miliseconds << "ms";
-
-                return stream.str();
         }
 }
--- nanocv/timer.h
+++ nanocv/timer.h
@@ -44,16 +44,6 @@ namespace ncv
 
                 typedef std::chrono::high_resolution_clock::time_point  time_t;
 
-                ///
-                /// \brief current time point
-                ///
-                static time_t now();
-
-                ///
-                /// \brief transform miliseconds to string (days, hours, minutes, seconds, miliseconds)
-                ///
-                static std::string miliseconds_to_string(std::size_t count);
-
         private:
 
                 // attributes
