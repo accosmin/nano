b9e5250aac5a5a690904e62ce007374500b38e92
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-07-27 08:37:42 +0200

    cleanup benchmark programs, use larger kernels

--- apps/ncv_benchmark_conv2d.cpp
+++ apps/ncv_benchmark_conv2d.cpp
@@ -7,62 +7,66 @@
 
 using namespace ncv;
 
-template
-<
-        typename top,
-        typename tmatrix
->
-static void test_cpu(tabulator_t::row_t& row, top op, const tmatrix& idata, const tmatrix& kdata, tmatrix& odata)
+namespace
 {
-        const size_t trials = 1024;
-        row << ncv::measure_robustly_usec([&] ()
+        template
+        <
+                typename top,
+                typename tmatrix
+        >
+        void test_method(tabulator_t::row_t& row, top op, const tmatrix& idata, const tmatrix& kdata, tmatrix& odata)
         {
-                odata.setZero();
-                op(idata, kdata, odata);
-        }, trials / kdata.rows());
-}
+                const size_t trials = 1024;
+                row << ncv::measure_robustly_usec([&] ()
+                {
+                        odata.setZero();
+                        op(idata, kdata, odata);
+                }, trials / kdata.rows());
+        }
 
-void test_conv2d(tabulator_t::row_t& row, int isize, int ksize)
-{
-        const int osize = isize - ksize + 1;
+        void test_conv2d(tabulator_t::row_t& row, int isize, int ksize)
+        {
+                const int osize = isize - ksize + 1;
 
-        matrix_t idata(isize, isize);
-        matrix_t kdata(ksize, ksize);
-        matrix_t odata(osize, osize);
+                matrix_t idata(isize, isize);
+                matrix_t kdata(ksize, ksize);
+                matrix_t odata(osize, osize);
 
-        idata.setRandom();
-        kdata.setRandom();
-        odata.setRandom();
+                idata.setRandom();
+                kdata.setRandom();
+                odata.setRandom();
 
-        idata /= isize;
-        kdata /= ksize;
-        odata /= osize;
+                idata /= isize;
+                kdata /= ksize;
+                odata /= osize;
 
-        test_cpu(row, math::conv2d_eig_t(), idata, kdata, odata);
-        test_cpu(row, math::conv2d_cpp_t(), idata, kdata, odata);
-        test_cpu(row, math::conv2d_dot_t(), idata, kdata, odata);
-        test_cpu(row, math::conv2d_mad_t(), idata, kdata, odata);
-        test_cpu(row, math::conv2d_dyn_t(), idata, kdata, odata);
+                test_method(row, math::conv2d_eig_t(), idata, kdata, odata);
+                test_method(row, math::conv2d_cpp_t(), idata, kdata, odata);
+                test_method(row, math::conv2d_dot_t(), idata, kdata, odata);
+                test_method(row, math::conv2d_mad_t(), idata, kdata, odata);
+                test_method(row, math::conv2d_dyn_t(), idata, kdata, odata);
+        }
 }
 
 int main(int, char* [])
 {
-        const int min_isize = 12;
+        const int min_isize = 8;
         const int max_isize = 48;
         const int min_ksize = 3;
+        const int max_ksize = 15;
 
         tabulator_t table("size\\method");
         table.header() << "eig [us]"
-                        << "cpp [us]"
-                        << "dot [us]"
-                        << "mad [us]"
-                        << "dyn [us]";
+                       << "cpp [us]"
+                       << "dot [us]"
+                       << "mad [us]"
+                       << "dyn [us]";
 
         for (int isize = min_isize; isize <= max_isize; isize += 4)
         {
                 table.clear();
 
-                for (int ksize = min_ksize; ksize <= isize - min_ksize; ksize += 2)
+                for (int ksize = min_ksize; ksize <= std::min(isize - min_ksize, max_ksize); ksize += 2)
                 {
                         const string_t header = "(" +
                                 text::to_string(isize) + "x" + text::to_string(isize) + "@" +
--- apps/ncv_benchmark_corr2d.cpp
+++ apps/ncv_benchmark_corr2d.cpp
@@ -7,64 +7,68 @@
 
 using namespace ncv;
 
-template
-<
-        typename top,
-        typename tmatrix
->
-static void test_cpu(tabulator_t::row_t& row, top op, const tmatrix& idata, const tmatrix& kdata, tmatrix& odata)
+namespace
 {
-        const size_t trials = 1024;
-        row << ncv::measure_robustly_usec([&] ()
+        template
+        <
+                typename top,
+                typename tmatrix
+        >
+        void test_method(tabulator_t::row_t& row, top op, const tmatrix& idata, const tmatrix& kdata, tmatrix& odata)
         {
-                odata.setZero();
-                op(idata, kdata, odata);
-        }, trials / kdata.rows());
-}
+                const size_t trials = 1024;
+                row << ncv::measure_robustly_usec([&] ()
+                {
+                        odata.setZero();
+                        op(idata, kdata, odata);
+                }, trials / kdata.rows());
+        }
 
-void test_corr2d(tabulator_t::row_t& row, int isize, int ksize)
-{
-        const int osize = isize - ksize + 1;
+        void test_corr2d(tabulator_t::row_t& row, int isize, int ksize)
+        {
+                const int osize = isize - ksize + 1;
 
-        matrix_t idata(isize, isize);
-        matrix_t kdata(ksize, ksize);
-        matrix_t odata(osize, osize);
+                matrix_t idata(isize, isize);
+                matrix_t kdata(ksize, ksize);
+                matrix_t odata(osize, osize);
 
-        idata.setRandom();
-        kdata.setRandom();
-        odata.setRandom();
+                idata.setRandom();
+                kdata.setRandom();
+                odata.setRandom();
 
-        idata /= isize;
-        kdata /= ksize;
-        odata /= osize;
+                idata /= isize;
+                kdata /= ksize;
+                odata /= osize;
 
-        test_cpu(row, ncv::math::corr2d_egb_t(), odata, kdata, idata);
-        test_cpu(row, ncv::math::corr2d_egr_t(), odata, kdata, idata);
-        test_cpu(row, ncv::math::corr2d_cpp_t(), odata, kdata, idata);
-        test_cpu(row, ncv::math::corr2d_mdk_t(), odata, kdata, idata);
-        test_cpu(row, ncv::math::corr2d_mdo_t(), odata, kdata, idata);
-        test_cpu(row, ncv::math::corr2d_dyn_t(), odata, kdata, idata);
+                test_method(row, ncv::math::corr2d_egb_t(), odata, kdata, idata);
+                test_method(row, ncv::math::corr2d_egr_t(), odata, kdata, idata);
+                test_method(row, ncv::math::corr2d_cpp_t(), odata, kdata, idata);
+                test_method(row, ncv::math::corr2d_mdk_t(), odata, kdata, idata);
+                test_method(row, ncv::math::corr2d_mdo_t(), odata, kdata, idata);
+                test_method(row, ncv::math::corr2d_dyn_t(), odata, kdata, idata);
+        }
 }
 
 int main(int, char* [])
 {
-        const int min_isize = 12;
+        const int min_isize = 8;
         const int max_isize = 48;
         const int min_ksize = 3;
+        const int max_ksize = 15;
 
         tabulator_t table("size\\method");
         table.header() << "egb [us]"
-                        << "egr [us]"
-                        << "cpp [us]"
-                        << "mkd [us]"
-                        << "mko [us]"
-                        << "dyn [us]";
+                       << "egr [us]"
+                       << "cpp [us]"
+                       << "mkd [us]"
+                       << "mko [us]"
+                       << "dyn [us]";
 
         for (int isize = min_isize; isize <= max_isize; isize += 4)
         {
                 table.clear();
 
-                for (int ksize = min_ksize; ksize <= isize - min_ksize; ksize += 2)
+                for (int ksize = min_ksize; ksize <= std::min(isize - min_ksize, max_ksize); ksize += 2)
                 {
                         const string_t header = "(" +
                                 text::to_string(isize) + "x" + text::to_string(isize) + "@" +
