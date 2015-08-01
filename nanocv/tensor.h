1386c80d56a49f07c5028aed5174f3152fcc9980
Cosmin Atanasoaei <accosmin@gmail.com>
2015-08-01 20:18:47 +0200

    float tensors

--- nanocv/tensor.h
+++ nanocv/tensor.h
@@ -5,7 +5,7 @@
 
 namespace ncv
 {
-        // numerical types
+        // numerical types (default)
         typedef tensor::vector_types_t<scalar_t>::tvector       vector_t;
         typedef tensor::vector_types_t<scalar_t>::tvectors      vectors_t;
 
@@ -14,6 +14,16 @@ namespace ncv
 
         typedef tensor::tensor_t<scalar_t>                      tensor_t;
         typedef std::vector<tensor_t>                           tensors_t;
+
+        // numerical types (float - for speed/vectorization)
+        typedef tensor::vector_types_t<float>::tvector          fvector_t;
+        typedef tensor::vector_types_t<float>::tvectors         fvectors_t;
+
+        typedef tensor::matrix_types_t<float>::tmatrix          fmatrix_t;
+        typedef tensor::matrix_types_t<float>::tmatrices        fmatrices_t;
+
+        typedef tensor::tensor_t<float>                         ftensor_t;
+        typedef std::vector<ftensor_t>                          ftensors_t;
 }
 
 
