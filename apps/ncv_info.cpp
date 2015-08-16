79ec1bc8e06739ebbacc0e228c2112867a92e1ed
Cosmin Atanasoaei <accosmin@gmail.com>
2015-08-16 16:56:38 +0200

    adapt program to display a single type of registered objects

--- apps/ncv_info.cpp
+++ apps/ncv_info.cpp
@@ -1,6 +1,7 @@
 #include "nanocv/nanocv.h"
 #include "nanocv/tabulator.h"
 #include <iostream>
+#include <boost/program_options.hpp>
 
 namespace
 {
@@ -27,16 +28,58 @@ namespace
         }
 }
 
-int main(int, char* [])
+int main(int argc, char* argv[])
 {
         ncv::init();
 
-        print("loss",           ncv::get_losses());
-        print("task",           ncv::get_tasks());
-        print("layer",          ncv::get_layers());
-        print("model",          ncv::get_models());
-        print("trainer",        ncv::get_trainers());
-        print("criterion",      ncv::get_criteria());
+        // parse the command line
+        boost::program_options::options_description po_desc("", 160);
+        po_desc.add_options()("help,h",         "display the registered objects");
+        po_desc.add_options()("loss",           "loss functions");
+        po_desc.add_options()("task",           "tasks");
+        po_desc.add_options()("layer",          "layer types to built models");
+        po_desc.add_options()("model",          "model types");
+        po_desc.add_options()("trainer",        "training methods");
+        po_desc.add_options()("criterion",      "training criteria");
+
+        boost::program_options::variables_map po_vm;
+        boost::program_options::store(
+                boost::program_options::command_line_parser(argc, argv).options(po_desc).run(),
+                po_vm);
+        boost::program_options::notify(po_vm);
+
+        // check arguments and options
+        if (	po_vm.empty() ||
+                po_vm.count("help"))
+        {
+                std::cout << po_desc;
+                return EXIT_FAILURE;
+        }
+
+        if (po_vm.count("loss"))
+        {
+                print("loss", ncv::get_losses());
+        }
+        if (po_vm.count("task"))
+        {
+                print("task", ncv::get_tasks());
+        }
+        if (po_vm.count("layer"))
+        {
+                print("layer", ncv::get_layers());
+        }
+        if (po_vm.count("model"))
+        {
+                print("model", ncv::get_models());
+        }
+        if (po_vm.count("trainer"))
+        {
+                print("trainer", ncv::get_trainers());
+        }
+        if (po_vm.count("criterion"))
+        {
+                print("criterion", ncv::get_criteria());
+        }
 
         // OK
         return EXIT_SUCCESS;
