e2c1c735ad56c8f1ca38e60c505722c9a7c7ad27
Cosmin Atanasoaei <accosmin@gmail.com>
2015-08-25 22:32:26 +0200

    faster 2D convolution for small kernels

--- nanocv/math/conv2d_dyn.hpp
+++ nanocv/math/conv2d_dyn.hpp
@@ -29,33 +29,27 @@ namespace ncv
                                 const auto ocols = odata.cols();
 
                                 // decode at run-time the kernel size
-                                if (kcols < ocols)
+                                switch (kcols)
                                 {
-                                        switch (kcols)
-                                        {
-                                        case 3:         conv2d_doti_t<3>()(idata, kdata, odata); break;
-                                        case 5:         conv2d_doti_t<5>()(idata, kdata, odata); break;
-                                        case 7:         conv2d_doti_t<7>()(idata, kdata, odata); break;
-                                        case 9:         conv2d_doti_t<9>()(idata, kdata, odata); break;
-                                        case 11:        conv2d_doti_t<11>()(idata, kdata, odata); break;
-                                        case 13:        conv2d_doti_t<13>()(idata, kdata, odata); break;
-                                        case 15:        conv2d_doti_t<15>()(idata, kdata, odata); break;
-                                        default:        conv2d_dot_t()(idata, kdata, odata); break;
-                                        }
+                                case 3:         conv2d_doti_t<3>()(idata, kdata, odata); break;
+                                case 5:         conv2d_doti_t<5>()(idata, kdata, odata); break;
+                                case 7:         conv2d_doti_t<7>()(idata, kdata, odata); break;
+                                case 9:         conv2d_doti_t<9>()(idata, kdata, odata); break;
+                                case 11:        conv2d_doti_t<11>()(idata, kdata, odata); break;
+                                case 13:        conv2d_doti_t<13>()(idata, kdata, odata); break;
+                                case 15:        conv2d_doti_t<15>()(idata, kdata, odata); break;
+                                default:
+                                switch (ocols)
+                                {
+                                case 3:         conv2d_madi_t<3>()(idata, kdata, odata); break;
+                                case 5:         conv2d_madi_t<5>()(idata, kdata, odata); break;
+                                case 7:         conv2d_madi_t<7>()(idata, kdata, odata); break;
+                                case 9:         conv2d_madi_t<9>()(idata, kdata, odata); break;
+                                case 11:        conv2d_madi_t<11>()(idata, kdata, odata); break;
+                                case 13:        conv2d_madi_t<13>()(idata, kdata, odata); break;
+                                case 15:        conv2d_madi_t<15>()(idata, kdata, odata); break;
+                                default:        conv2d_mad_t()(idata, kdata, odata); break;
                                 }
-                                else
-                                {
-                                        switch (ocols)
-                                        {
-                                        case 3:         conv2d_madi_t<3>()(idata, kdata, odata); break;
-                                        case 5:         conv2d_madi_t<5>()(idata, kdata, odata); break;
-                                        case 7:         conv2d_madi_t<7>()(idata, kdata, odata); break;
-                                        case 9:         conv2d_madi_t<9>()(idata, kdata, odata); break;
-                                        case 11:        conv2d_madi_t<11>()(idata, kdata, odata); break;
-                                        case 13:        conv2d_madi_t<13>()(idata, kdata, odata); break;
-                                        case 15:        conv2d_madi_t<15>()(idata, kdata, odata); break;
-                                        default:        conv2d_mad_t()(idata, kdata, odata); break;
-                                        }
                                 }
                         }
                 };
