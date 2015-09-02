7d80b713e8de20b17b7b03658f509122f4aa741c
Cosmin Atanasoaei <accosmin@gmail.com>
2015-09-02 18:32:09 +0200

    script to draw digits using imagemagick

---
+++ scripts/make_digits.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+text="0123456789"
+name="digits"
+
+#font="DejaVu-Sans-Mono-Bold"
+# -font ${font}
+# -gravity center
+
+convert -fill black -pointsize 32 -background none label:${text} -flatten ${name}.png
