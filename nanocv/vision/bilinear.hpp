c825624bc3f727d0347314e69516b13ba5e7bfc6
Cosmin Atanasoaei <accosmin@gmail.com>
2015-09-04 22:09:33 +0200

    utilities to compute the image gradients

--- nanocv/vision/bilinear.hpp
+++ nanocv/vision/bilinear.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <algorithm>
 #include "nanocv/math/cast.hpp"
 
---
+++ nanocv/vision/gradient.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <cassert>
+#include "nanocv/math/clamp.hpp"
+
+namespace ncv
+{
+        ///
+        /// \brief compute the x gradient of the input matrix
+        ///
+        template
+        <
+                typename tmatrixi,
+                typename tmatrixo
+        >
+        void xgradient(const tmatrixi& srcplane, tmatrixo&& xplane)
+        {
+                assert(srcplane.rows() == xplane.rows());
+                assert(srcplane.cols() == xplane.cols());
+
+                const int rows = static_cast<int>(srcplane.rows());
+                const int cols = static_cast<int>(srcplane.cols());
+
+                for (int r = 0; r < rows; r ++)
+                {
+                        for (int c = 0; c < cols; c ++)
+                        {
+                                const int cn = math::clamp(c - 1, 0, cols - 1);
+                                const int cp = math::clamp(c + 1, 0, cols - 1);
+
+                                xplane(r, c) = srcplane(r, cp) - srcplane(r, cn);
+                        }
+                }
+        }
+
+        ///
+        /// \brief compute the y gradient of the input matrix
+        ///
+        template
+        <
+                typename tmatrixi,
+                typename tmatrixo
+        >
+        void ygradient(const tmatrixi& srcplane, tmatrixo&& yplane)
+        {
+                assert(srcplane.rows() == yplane.rows());
+                assert(srcplane.cols() == yplane.cols());
+
+                const int rows = static_cast<int>(srcplane.rows());
+                const int cols = static_cast<int>(srcplane.cols());
+
+                for (int r = 0; r < rows; r ++)
+                {
+                        const int rn = math::clamp(r - 1, 0, rows - 1);
+                        const int rp = math::clamp(r + 1, 0, rows - 1);
+
+                        for (int c = 0; c < cols; c ++)
+                        {
+                                yplane(r, c) = srcplane(rp, c) - srcplane(rn, c);
+                        }
+                }
+        }
+}
+
