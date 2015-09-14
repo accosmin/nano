566e5bd207ba78e0a41bf51a985274d51f9dbbee
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-09-14 08:50:12 +0200

    also test the running average vector

--- tests/test_average.cpp
+++ tests/test_average.cpp
@@ -5,6 +5,8 @@
 #include "libmath/abs.hpp"
 #include "libmath/epsilon.hpp"
 #include "liboptim/average_scalar.hpp"
+#include "liboptim/average_vector.hpp"
+#include <eigen3/Eigen/Core>
 
 namespace test
 {
@@ -14,22 +16,50 @@ namespace test
         <
                 typename tscalar
         >
+        tscalar average(const size_t range)
+        {
+                return static_cast<tscalar>(range) / static_cast<tscalar>(2);
+        }
+
+        template
+        <
+                typename tscalar
+        >
         void check_average(const size_t range)
         {
-                average_scalar_t<tscalar> running_average;
-
+                average_scalar_t<tscalar> runavg;
                 for (size_t i = 0; i <= range; i ++)
                 {
-                        running_average.update(static_cast<tscalar>(i), tscalar(1));
+                        runavg.update(static_cast<tscalar>(i), tscalar(1));
                 }
 
-                const tscalar real_average = static_cast<tscalar>(range) / static_cast<tscalar>(2);
+                const auto avg = average<tscalar>(range);
 
-                BOOST_CHECK_LE(math::abs(running_average.value() - real_average), math::epsilon1<tscalar>());
+                BOOST_CHECK_LE(math::abs(runavg.value() - avg),
+                               math::epsilon1<tscalar>());
+        }
+
+        template
+        <
+                typename tvector,
+                typename tscalar = typename tvector::Scalar
+        >
+        void check_average(const size_t dims, const size_t range)
+        {
+                average_vector_t<tscalar, tvector> runavg(dims);
+                for (size_t i = 0; i <= range; i ++)
+                {
+                        runavg.update(tvector::Constant(dims, static_cast<tscalar>(i)), tscalar(1));
+                }
+
+                const auto avg = average<tscalar>(range);
+
+                BOOST_CHECK_LE((runavg.value() - tvector::Constant(dims, avg)).template lpNorm<Eigen::Infinity>(),
+                               math::epsilon1<tscalar>());
         }
 }
 
-BOOST_AUTO_TEST_CASE(test_average)
+BOOST_AUTO_TEST_CASE(test_average_scalar)
 {
         test::check_average<double>(1);
         test::check_average<double>(5);
@@ -40,3 +70,16 @@ BOOST_AUTO_TEST_CASE(test_average)
         test::check_average<double>(14332);
         test::check_average<double>(123434);
 }
+
+BOOST_AUTO_TEST_CASE(test_average_vector)
+{
+        test::check_average<Eigen::VectorXd>(13, 1);
+        test::check_average<Eigen::VectorXd>(17, 5);
+        test::check_average<Eigen::VectorXd>(11, 17);
+        test::check_average<Eigen::VectorXd>(21, 85);
+        test::check_average<Eigen::VectorXd>(27, 187);
+        test::check_average<Eigen::VectorXd>(15, 1561);
+        test::check_average<Eigen::VectorXd>(19, 14332);
+        test::check_average<Eigen::VectorXd>(18, 123434);
+}
+
