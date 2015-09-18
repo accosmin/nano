8c35a9169293ca233d6765672df9abc7d30694b4
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-09-18 15:54:03 +0200

    improved measuring of fast function calls (e.g. of 10us-100us)

--- nanocv/core/measure.hpp
+++ nanocv/core/measure.hpp
@@ -47,6 +47,25 @@ namespace ncv
         }
 
         ///
+        /// \brief measure a function call (in microseconds)
+        ///
+        template
+        <
+                typename toperator
+        >
+        std::size_t measure_usec(const toperator& op, std::size_t count)
+        {
+                const timer_t timer;
+
+                for (std::size_t i = 0; i < count; i ++)
+                {
+                        op();
+                }
+
+                return std::max(std::size_t(1), timer.microseconds());
+        }
+
+        ///
         /// \brief robustly measure a function call (in microseconds)
         ///
         template
@@ -55,19 +74,22 @@ namespace ncv
         >
         std::size_t measure_robustly_usec(const toperator& op, std::size_t trials)
         {
-                std::size_t best_time = std::numeric_limits<std::size_t>::max();
+                const std::size_t min_usec = 10 * 1000;
 
+                // calibrate the number of function calls to achieve the minimum time resolution
+                const auto time1 = measure_usec(op, 1);
+                const auto count = std::max(std::size_t(1), (min_usec + time1 / 2) / time1);
+
+                auto best_time = std::numeric_limits<std::size_t>::max();
                 for (std::size_t t = 0; t < trials; t ++)
                 {
-                        const timer_t timer;
-                        op();
-                        const std::size_t time = timer.microseconds();
+                        const auto time = measure_usec(op, count);
                         if (time < best_time)
                         {
                                 best_time = time;
                         }
                 }
 
-                return best_time;
+                return (best_time + count / 2) / count;
         }
 }
