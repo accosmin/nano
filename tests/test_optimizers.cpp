d09243dc6846f12662c053f818a558f47d03fc34
Cosmin Atanasoaei <accosmin@gmail.com>
2015-09-22 21:38:30 +0200

    exhaustively testing the optimizers (to polish & make it faster)

--- tests/test_optimizers.cpp
+++ tests/test_optimizers.cpp
@@ -32,43 +32,52 @@ namespace test
         static void check_solution(const string_t& problem, const string_t& optimizer,
                 const vector_t& x0, const opt_state_t& state, const std::vector<std::pair<vector_t, scalar_t>>& solutions)
         {
+//                // find the closest solution
+//                size_t best_index = std::string::npos;
+//                scalar_t best_distance = std::numeric_limits<scalar_t>::max();
+
+//                for (size_t index = 0; index < solutions.size(); index ++)
+//                {
+//                        const scalar_t distance = (state.x - solutions[index].first).lpNorm<Eigen::Infinity>();
+//                        if (distance < best_distance)
+//                        {
+//                                best_distance = distance;
+//                                best_index = index;
+//                        }
+//                }
+
+//                // check accuracy
+//                BOOST_REQUIRE_LT(best_index, solutions.size());
+
+//                const scalar_t df = math::abs(state.f - solutions[best_index].second);
+//                const scalar_t dx = (state.x - solutions[best_index].first).lpNorm<Eigen::Infinity>();
+
+//                BOOST_CHECK_LE(df, math::epsilon3<scalar_t>());
+//                BOOST_CHECK_LE(dx, math::epsilon3<scalar_t>());
+
+//                // debugging
+//                BOOST_CHECK_MESSAGE(
+//                        dx < math::epsilon3<scalar_t>(),
+//                        "failed (x) after " << state.n_iterations() <<
+//                        " iterations for <" << problem << ">, <" << optimizer << "> and <" << x0.transpose() << ">!");
+//                BOOST_CHECK_MESSAGE(
+//                        df < math::epsilon3<scalar_t>(),
+//                        "failed (f) after " << state.n_iterations() <<
+//                        " iterations for <" << problem << ">, <" << optimizer << "> and <" << x0.transpose() << ">!");
+        }
+
+        static void check_convergence(const string_t& problem, const string_t& optimizer,
+                const vector_t& x0, const scalar_t epsilon, const opt_state_t& state)
+        {
                 // check convergence
-                const scalar_t dg = state.g.lpNorm<Eigen::Infinity>();
-                BOOST_CHECK_LE(dg, math::epsilon3<scalar_t>());
-
-                // find the closest solution
-                size_t best_index = std::string::npos;
-                scalar_t best_distance = std::numeric_limits<scalar_t>::max();
-
-                for (size_t index = 0; index < solutions.size(); index ++)
-                {
-                        const scalar_t distance = (state.x - solutions[index].first).lpNorm<Eigen::Infinity>();
-                        if (distance < best_distance)
-                        {
-                                best_distance = distance;
-                                best_index = index;
-                        }
-                }
-
-                // check accuracy
-                BOOST_REQUIRE_LT(best_index, solutions.size());
-
-                const scalar_t df = math::abs(state.f - solutions[best_index].second);
-                const scalar_t dx = (state.x - solutions[best_index].first).lpNorm<Eigen::Infinity>();
-
-                BOOST_CHECK_LE(df, math::epsilon3<scalar_t>());
-                BOOST_CHECK_LE(dx, math::epsilon3<scalar_t>());
+                const scalar_t dg = state.convergence_criteria();
+                BOOST_CHECK_LE(dg, epsilon);
 
                 // debugging
                 BOOST_CHECK_MESSAGE(
-                        dx < math::epsilon3<scalar_t>(),
-                        "failed (x) for <" << problem << ">, <" << optimizer << "> and <" << x0.transpose() << ">!");
-                BOOST_CHECK_MESSAGE(
-                        df < math::epsilon3<scalar_t>(),
-                        "failed (f) for <" << problem << ">, <" << optimizer << "> and <" << x0.transpose() << ">!");
-                BOOST_CHECK_MESSAGE(
-                        dg < math::epsilon3<scalar_t>(),
-                        "failed (g) for <" << problem << ">, <" << optimizer << "> and <" << x0.transpose() << ">!");
+                        dg < epsilon,
+                        "failed (g) for <" << problem << ", " << optimizer <<
+                        ">: x0 = " << x0.transpose() << ", iters = " << state.n_iterations() << "!");
         }
 
         static void check_problem(
@@ -76,7 +85,7 @@ namespace test
                 const opt_opsize_t& fn_size, const opt_opfval_t& fn_fval, const opt_opgrad_t& fn_grad,
                 const std::vector<std::pair<vector_t, scalar_t>>& solutions)
         {
-                const size_t iterations = 1024;
+                const size_t iterations = 1024 * 1024;
                 const scalar_t epsilon = 1e-6;
                 const size_t trials = 16 * 1024;
 
@@ -100,14 +109,14 @@ namespace test
                         min::batch_optimizer::GD,
 
                         min::batch_optimizer::CGD,
-//                        min::batch_optimizer::CGD_CD,
-//                        min::batch_optimizer::CGD_DY,
-//                        min::batch_optimizer::CGD_FR,
-//                        min::batch_optimizer::CGD_HS,
-//                        min::batch_optimizer::CGD_LS,
+                        min::batch_optimizer::CGD_CD,
+                        min::batch_optimizer::CGD_DY,
+                        min::batch_optimizer::CGD_FR,
+                        min::batch_optimizer::CGD_HS,
+                        min::batch_optimizer::CGD_LS,
                         min::batch_optimizer::CGD_N,
                         min::batch_optimizer::CGD_PRP,
-//                        min::batch_optimizer::CGD_DYCD,
+                        min::batch_optimizer::CGD_DYCD,
                         min::batch_optimizer::CGD_DYHS,
 
                         min::batch_optimizer::LBFGS
@@ -124,7 +133,8 @@ namespace test
                                         fn_size, fn_fval, fn_grad, nullptr, nullptr, nullptr,
                                         x0, optimizer, iterations, epsilon);
 
-                                // check solution
+                                // check result
+                                check_convergence(problem_name, text::to_string(optimizer), x0, epsilon, state);
                                 check_solution(problem_name, text::to_string(optimizer), x0, state, solutions);
                         }
                 }
@@ -143,19 +153,19 @@ BOOST_AUTO_TEST_CASE(test_optimizers)
 {
         using namespace ncv;        
 
-//        test::check_problems(ncv::make_beale_funcs());
+        test::check_problems(ncv::make_beale_funcs());
         test::check_problems(ncv::make_booth_funcs());
         test::check_problems(ncv::make_matyas_funcs());
-//        test::check_problems(ncv::make_trid_funcs(8));
+        test::check_problems(ncv::make_trid_funcs(8));
         test::check_problems(ncv::make_sphere_funcs(8));
-//        test::check_problems(ncv::make_powell_funcs(8));
-//        test::check_problems(ncv::make_mccormick_funcs());
+        test::check_problems(ncv::make_powell_funcs(8));
+        test::check_problems(ncv::make_mccormick_funcs());
         test::check_problems(ncv::make_himmelblau_funcs());
-//        test::check_problems(ncv::make_rosenbrock_funcs(7));
-//        test::check_problems(ncv::make_3hump_camel_funcs());
-//        test::check_problems(ncv::make_dixon_price_funcs(8));
+        test::check_problems(ncv::make_rosenbrock_funcs(7));
+        test::check_problems(ncv::make_3hump_camel_funcs());
+        test::check_problems(ncv::make_dixon_price_funcs(8));
         test::check_problems(ncv::make_sum_squares_funcs(8));
-//        test::check_problems(ncv::make_goldstein_price_funcs());
+        test::check_problems(ncv::make_goldstein_price_funcs());
         test::check_problems(ncv::make_rotated_ellipsoid_funcs(8));
 }
 
