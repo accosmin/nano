bfa5abdb196feddefb1058c986f8bf7b246600ee
Cosmin Atanasoaei <accosmin@gmail.com>
2015-09-23 21:17:59 +0200

    cleanup Beale test function

--- nanocv/func/function_beale.cpp
+++ nanocv/func/function_beale.cpp
@@ -14,26 +14,28 @@ namespace ncv
 
                         const opt_opfval_t fn_fval = [=] (const vector_t& x)
                         {
-                                const scalar_t a = x(0), b = x(1);
+                                const scalar_t a = x(0);
+                                const scalar_t b = x(1), b2 = b * b, b3 = b2 * b;
 
                                 const scalar_t z0 = 1.5 - a + a * b;
-                                const scalar_t z1 = 2.25 - a + a * b * b;
-                                const scalar_t z2 = 2.625 - a + a * b * b * b;
+                                const scalar_t z1 = 2.25 - a + a * b2;
+                                const scalar_t z2 = 2.625 - a + a * b3;
 
                                 return z0 * z0 + z1 * z1 + z2 * z2;
                         };
 
                         const opt_opgrad_t fn_grad = [=] (const vector_t& x, vector_t& gx)
                         {
-                                const scalar_t a = x(0), b = x(1);
+                                const scalar_t a = x(0);
+                                const scalar_t b = x(1), b2 = b * b, b3 = b2 * b;
 
                                 const scalar_t z0 = 1.5 - a + a * b;
-                                const scalar_t z1 = 2.25 - a + a * b * b;
-                                const scalar_t z2 = 2.625 - a + a * b * b * b;
+                                const scalar_t z1 = 2.25 - a + a * b2;
+                                const scalar_t z2 = 2.625 - a + a * b3;
 
                                 gx.resize(2);
-                                gx(0) = 2.0 * (z0 * (-1 + b) + z1 * (-1 + b * b) + z2 * (-1 + b * b * b));
-                                gx(1) = 2.0 * (z0 * (a) + z1 * (2 * a * b) + z2 * (3 * a * b * b));
+                                gx(0) = 2.0 * (z0 * (-1 + b) + z1 * (-1 + b2) + z2 * (-1 + b3));
+                                gx(1) = 2.0 * (z0 * (a) + z1 * (2 * a * b) + z2 * (3 * a * b2));
 
                                 return fn_fval(x);
                         };
