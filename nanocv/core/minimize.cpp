dd71400e2109b1bf078395d7bdcb6b3c4f85ce15
Cosmin Atanasoaei <accosmin@pix4d.com>
2015-10-02 10:06:17 +0200

    this improves the performance of AG-based methods, without decreasing it for the other methods

--- nanocv/core/minimize.cpp
+++ nanocv/core/minimize.cpp
@@ -291,7 +291,7 @@ namespace ncv
                         return { 0.0 };
 
                 default:
-                        return { 1e-3, 1e-2, 1e-1, 1.0 };
+                        return { 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0 };
                 }
         }
 
