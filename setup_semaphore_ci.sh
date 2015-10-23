f3c7ec9c13a031e964573db04a7e95c6885907ee
Cosmin Atanasoaei <accosmin@gmail.com>
2015-10-23 11:04:58 +0200

    semaphore ci (install only the required boost libraries)

--- setup_semaphore_ci.sh
+++ setup_semaphore_ci.sh
@@ -4,4 +4,5 @@ sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
 sudo apt-get update -qq
 sudo apt-get upgrade -qq
 sudo apt-get install -qq cmake ninja-build g++-4.9 g++-5 clang-3.5 clang-3.6
-sudo apt-get install -qq libarchive-dev libbz2-dev libdevil-dev libboost-all-dev libeigen3-dev
+sudo apt-get install -qq libarchive-dev libbz2-dev libdevil-dev libeigen3-dev
+sudo apt-get install -qq libboost-system-dev libboost-filesystem-dev libboost-serialization-dev libboost-program-options-dev libboost-test-dev
