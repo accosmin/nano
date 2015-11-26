ea72833d99fceb922a5cf82cdb423004355d59e2
Cosmin Atanasoaei <accosmin@gmail.com>
2015-11-26 21:38:15 +0100

    remove support for Semaphore CI

--- setup_semaphore_ci.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install -qq software-properties-common
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
-sudo apt-get update -qq
-sudo apt-get upgrade -qq
-sudo apt-get install -qq cmake ninja-build g++-4.9 g++-5 clang-3.5 clang-3.6
-sudo apt-get install -qq libarchive-dev libbz2-dev libdevil-dev libeigen3-dev
-sudo apt-get install -qq libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-test-dev
