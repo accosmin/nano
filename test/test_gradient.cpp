8f43868544530d62d4b446307822b223f3420252
Cosmin Atanasoaei <accosmin@gmail.com>
2015-11-29 22:27:58 +0100

    cleanup unit test

--- test/test_gradient.cpp
+++ test/test_gradient.cpp
@@ -14,76 +14,65 @@
 #include "cortex/accumulator.h"
 #include <set>
 
-namespace test
+namespace
 {
         using namespace cortex;
 
-        std::mutex mutex;
+        const strings_t conv_layer_ids { "", "conv" };
+        const strings_t pool_layer_ids { "", "pool-max", "pool-min", "pool-avg" };
+        const strings_t full_layer_ids { "", "linear" };
+        const strings_t actv_layer_ids { "", "act-unit", "act-tanh", "act-snorm", "act-splus" };
 
-        size_t n_checks = 0;
-        size_t n_failures1 = 0;
-        size_t n_failures2 = 0;
-        size_t n_failures3 = 0;
+        const color_mode cmd_color = color_mode::rgba;
+        const size_t cmd_irows = 8;
+        const size_t cmd_icols = 8;
+        const size_t cmd_outputs = 2;
+        const size_t cmd_max_layers = 2;
 
-        namespace
+        string_t make_model_description(
+                size_t n_layers,
+                const string_t& actv_layer_id,
+                const string_t& pool_layer_id,
+                const string_t& conv_layer_id,
+                const string_t& full_layer_id)
         {
-                const strings_t conv_layer_ids { "", "conv" };
-                const strings_t pool_layer_ids { "", "pool-max", "pool-min", "pool-avg" };
-                const strings_t full_layer_ids { "", "linear" };
-                const strings_t actv_layer_ids { "", "act-unit", "act-tanh", "act-snorm", "act-splus" };
+                string_t desc;
 
-                const color_mode cmd_color = color_mode::luma;
-                const size_t cmd_irows = 8;
-                const size_t cmd_icols = 8;
-                const size_t cmd_outputs = 2;
-                const size_t cmd_max_layers = 1;
-
-                string_t make_model_description(
-                        size_t n_layers,
-                        const string_t& actv_layer_id,
-                        const string_t& pool_layer_id,
-                        const string_t& conv_layer_id,
-                        const string_t& full_layer_id)
+                // convolution part
+                for (size_t l = 0; l < n_layers && !conv_layer_id.empty(); ++ l)
                 {
-                        string_t desc;
+                        math::random_t<size_t> rgen(2, 3);
 
-                        // convolution part
-                        for (size_t l = 0; l < n_layers && !conv_layer_id.empty(); ++ l)
+                        string_t params;
+                        params += "dims=" + text::to_string(rgen());
+                        params += (rgen() % 2 == 0) ? ",rows=2,cols=2" : ",rows=3,cols=3";
+
+                        desc += conv_layer_id + ":" + params + ";";
+                        if (l == 0)
                         {
-                                math::random_t<size_t> rgen(2, 3);
-
-                                string_t params;
-                                params += "dims=" + text::to_string(rgen());
-                                params += (rgen() % 2 == 0) ? ",rows=2,cols=2" : ",rows=3,cols=3";
-
-                                desc += conv_layer_id + ":" + params + ";";
-                                if (l == 0)
-                                {
-                                        desc += pool_layer_id + ";";
-                                }
-                                desc += actv_layer_id + ";";
+                                desc += pool_layer_id + ";";
                         }
-
-                        // fully-connected part
-                        for (size_t l = 0; l < n_layers && !full_layer_id.empty(); ++ l)
-                        {
-                                math::random_t<size_t> rgen(2, 3);
-
-                                string_t params;
-                                params += "dims=" + text::to_string(rgen());
-
-                                desc += full_layer_id + ":" + params + ";";
-                                desc += actv_layer_id + ";";
-                        }
-
-                        desc += "linear:dims=" + text::to_string(cmd_outputs) + ";";
-
-                        return desc;
+                        desc += actv_layer_id + ";";
                 }
+
+                // fully-connected part
+                for (size_t l = 0; l < n_layers && !full_layer_id.empty(); ++ l)
+                {
+                        math::random_t<size_t> rgen(2, 3);
+
+                        string_t params;
+                        params += "dims=" + text::to_string(rgen());
+
+                        desc += full_layer_id + ":" + params + ";";
+                        desc += actv_layer_id + ";";
+                }
+
+                desc += "linear:dims=" + text::to_string(cmd_outputs) + ";";
+
+                return desc;
         }
 
-        std::vector<std::pair<cortex::string_t, cortex::string_t> > make_grad_configs(
-                tensor_size_t& irows, tensor_size_t& icols, tensor_size_t& outputs, cortex::color_mode& color)
+        auto make_grad_configs()
         {
                 // evaluate the analytical gradient vs. the finite difference approximation for various:
                 //      * convolution layers
@@ -130,12 +119,20 @@ namespace test
                 }
 
                 // OK
-                irows = cmd_irows;
-                icols = cmd_icols;
-                outputs = cmd_outputs;
-                color = cmd_color;
                 return result;
         }
+}
+
+namespace test
+{
+        using namespace cortex;
+
+        std::mutex mutex;
+
+        size_t n_checks = 0;
+        size_t n_failures1 = 0;
+        size_t n_failures2 = 0;
+        size_t n_failures3 = 0;
 
         void test_grad_params(const string_t& header, const string_t& loss_id, const model_t& model,
                 accumulator_t& acc_params)
@@ -333,12 +330,7 @@ BOOST_AUTO_TEST_CASE(test_gradient)
 
         cortex::init();
 
-        tensor_size_t cmd_irows;
-        tensor_size_t cmd_icols;
-        tensor_size_t cmd_outputs;
-        color_mode cmd_color;
-
-        auto configs = test::make_grad_configs(cmd_irows, cmd_icols, cmd_outputs, cmd_color);
+        const auto configs = ::make_grad_configs();
 
         // test each configuration
         thread::pool_t pool;
