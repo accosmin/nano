8d31864086c5c63d0937a42c40001909df9f951e
Cosmin Atanasoaei <accosmin@gmail.com>
2016-01-07 20:55:42 +0100

    no need for -pthread for AppleClang

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -41,7 +41,6 @@ if(CMAKE_CXX_COMPILER_ID MATCHES GNU OR CMAKE_CXX_COMPILER_ID MATCHES Clang)
 
         # set flags
         set(CMAKE_CXX_FLAGS                     "-std=c++14 -pedantic -march=native -mtune=native")
-        set(CMAKE_CXX_FLAGS                     "${CMAKE_CXX_FLAGS} -pthread")
         set(CMAKE_CXX_FLAGS                     "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wconversion")
         set(CMAKE_CXX_FLAGS                     "${CMAKE_CXX_FLAGS} -Woverloaded-virtual")
         set(CMAKE_CXX_FLAGS                     "${CMAKE_CXX_FLAGS} -Wreorder")
@@ -54,6 +53,10 @@ if(CMAKE_CXX_COMPILER_ID MATCHES GNU OR CMAKE_CXX_COMPILER_ID MATCHES Clang)
         set(CMAKE_CXX_FLAGS_RELWITHDEBINFO      "-O2 -g -fno-omit-frame-pointer")       # -DEIGEN_NO_DEBUG")
         set(CMAKE_CXX_FLAGS_MINSIZEREL          "-Os -DNDEBUG")                         # -DEIGEN_NO_DEBUG")
         set(CMAKE_EXE_LINKER_FLAGS              "-flto")
+
+        if(NOT CMAKE_CXX_COMPILER_ID MATCHES AppleClang)
+                set(CMAKE_CXX_FLAGS             "${CMAKE_CXX_FLAGS} -pthread")
+        endif()        
 
         # set address sanitizer
         if(NANOCV_WITH_ASAN)
