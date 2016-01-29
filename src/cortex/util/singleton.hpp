57e62cfb5495f54aa37471e451c9df4a32018d71
Cosmin Atanasoaei <accosmin@gmail.com>
2016-01-29 19:06:15 +0100

    simplify implementation of singleton using C++11

--- src/cortex/util/singleton.hpp
+++ src/cortex/util/singleton.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <memory>
-#include <mutex>
-
 namespace cortex
 {
         template
@@ -10,23 +7,15 @@ namespace cortex
                 class tobject
         >
         class singleton_t
-	{
+        {
         public:
-
-                using this_object = tobject;
-                using this_mutex_t = std::once_flag;
-                using this_instance_t = std::unique_ptr<this_object>;
-
                 ///
                 /// \brief acccess the only instance
                 ///
-                static this_object& instance()
+                static tobject& instance()
                 {
-                        std::call_once(m_once_flag, []()
-                        {
-                                singleton_t::m_instance.reset(new this_object);
-                        });
-                        return *m_instance;
+                        static tobject the_instance;
+                        return the_instance;
                 }
 
         protected:
@@ -46,17 +35,5 @@ namespace cortex
                 /// \brief destructor
                 ///
                 virtual ~singleton_t() {}
-		
-	private:
-
-                // attributes
-                static this_instance_t  m_instance;
-                static this_mutex_t     m_once_flag;
-	};
-
-        template <class tobject>
-        typename singleton_t<tobject>::this_instance_t    singleton_t<tobject>::m_instance = nullptr;
-
-        template <class tobject>
-        typename singleton_t<tobject>::this_mutex_t       singleton_t<tobject>::m_once_flag;
+        };
 }
