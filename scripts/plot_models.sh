bd9e7e4d857890d55700c84913e140134a94bcb2
Cosmin Atanasoaei <accosmin@gmail.com>
2016-02-09 19:02:55 +0100

    fix whitespaces & tabs

--- scripts/plot_models.sh
+++ scripts/plot_models.sh
@@ -4,7 +4,7 @@ source common_plot.sh
 
 # input files: [output plot] [*.state (train loss, train error, train error variance, valid loss, valid error, valid error variance)]+
 ifiles=("$@")
-  
+
 if [ ${#ifiles[@]} -lt 2 ]
 then
         echo "plot_models.sh <output path> <training log file>+"
@@ -36,7 +36,7 @@ do
         index=${indices[$k]}
         title=${titles[$k]}
 
-	prepare_plot ${pfile}
+        prepare_plot ${pfile}
         echo "set origin 0.0,0.0" >> ${pfile}
         echo "set size 1.0,1.0" >> ${pfile}
         echo "set title \"${title}\"" >> ${pfile}
@@ -49,11 +49,11 @@ do
                 ifile=${ifiles[$i]}
                 label=`basename ${ifile} .state`
                 label=${label//_/-}
-                
+
                 echo -e -n "\t'${ifile}' using ${index} title '${label}' with linespoints ps 0.3" >> ${pfile}
-                
+
                 let ii=${i}+1
-                if [ $ii -eq ${#ifiles[*]} ] 
+                if [ $ii -eq ${#ifiles[*]} ]
                 then
                         echo "" >> ${pfile}
                 else
@@ -64,5 +64,5 @@ do
 done
 
 # export
-gnuplot ${pfile} 
+gnuplot ${pfile}
 rm -f ${pfile}
