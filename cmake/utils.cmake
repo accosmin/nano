8a08f87619aff9b8a573fcd7c791f2988eed76be
Cosmin Atanasoaei <accosmin@gmail.com>
2016-04-10 22:22:18 +0200

    preliminary cmake function to setup LTO

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -83,3 +83,9 @@ function (setup_gold)
         endif()
 endfunction()
 
+# setup LTO
+function(setup_lto)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -flto")
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -flto")
+endfunction()
+
