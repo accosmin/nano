68094326305393f6b58b75850cfe58f3bcd300b5
Cosmin Atanasoaei <catanasoaei@nexthink.com>
2016-05-06 15:39:53 +0200

    use AdaGrad with a fixed learning rate like in the original paper

--- src/math/stoch/adagrad.hpp
+++ src/math/stoch/adagrad.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "lrate.hpp"
 #include "stoch_loop.hpp"
 #include "math/average.hpp"
 
@@ -34,28 +33,25 @@ namespace nano
                                 return this->operator()(param.tunable(), problem, x0, params...);
                         };
 
-                        const auto config = nano::tune(op, make_alpha0s(), make_decays(), make_epsilons());
-                        return operator()(param, problem, x0, config.param0(), config.param1(), config.param2());
+                        const auto config = nano::tune(op, make_alpha0s(), make_epsilons());
+                        return operator()(param, problem, x0, config.param0(), config.param1());
                 }
 
                 ///
                 /// \brief minimize starting from the initial guess x0
                 ///
                 tstate operator()(const param_t& param, const tproblem& problem, const tvector& x0,
-                        const tscalar alpha0, const tscalar decay, const tscalar epsilon) const
+                        const tscalar alpha0, const tscalar epsilon) const
                 {
                         assert(problem.size() == x0.size());
 
                         // second-order gradient momentum
                         average_vector_t<tvector> gavg(x0.size());
 
-                        // learning rate schedule
-                        lrate_t<tscalar> lrate(alpha0, decay);
-
-                        const auto op_iter = [&] (tstate& cstate, const std::size_t iter)
+                        const auto op_iter = [&] (tstate& cstate, const std::size_t)
                         {
                                 // learning rate
-                                const tscalar alpha = lrate.get(iter);
+                                const tscalar alpha = alpha0;
 
                                 // descent direction
                                 gavg.update(cstate.g.array().square());
@@ -69,7 +65,7 @@ namespace nano
 
                         // OK, assembly the optimizer
                         return  stoch_loop(problem, param, tstate(problem, x0), op_iter,
-                                {{"alpha0", alpha0}, {"decay", decay} , {"epsilon", epsilon}});
+                                {{"alpha0", alpha0}, {"epsilon", epsilon}});
                 }
         };
 }
