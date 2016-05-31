5539545a4b405394c79c2375fc6e029529227070
Cosmin Atanasoaei <accosmin@gmail.com>
2016-05-31 23:06:16 +0200

    have the default scalar type configurable

--- src/scalar.h
+++ src/scalar.h
@@ -10,7 +10,13 @@ namespace nano
         using indices_t = std::vector<size_t>;
 
         // default scalar
+#if defined(NANO_FLOAT_SCALAR)
+        using scalar_t = float;
+#elif defined(NANO_DOUBLE_SCALAR)
         using scalar_t = double;
+#elif defined(NANO_LONG_DOUBLE_SCALAR)
+        using scalar_t = long double;
+#endif
         using scalars_t = std::vector<scalar_t>;
 }
 
