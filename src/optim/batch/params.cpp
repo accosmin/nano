8bed049276a4514971b351c91b2442fba857491a
Cosmin Atanasoaei <catanasoaei@nexthink.com>
2016-06-27 17:14:40 +0200

    refactor batch optimization: hide algorithms

---
+++ src/optim/batch/params.cpp
@@ -0,0 +1,125 @@
+#include "params.h"
+
+namespace nano
+{
+        static ls_initializer make_lsinit(const batch_optimizer optimizer)
+        {
+                switch (optimizer)
+                {
+                case batch_optimizer::LBFGS:
+                        return ls_initializer::unit;
+
+                case batch_optimizer::CGD: // fall through!
+                case batch_optimizer::CGD_PRP:
+                        return ls_initializer::quadratic;
+
+                case batch_optimizer::CGD_CD:
+                        return ls_initializer::quadratic;
+
+                case batch_optimizer::CGD_DY:
+                        return ls_initializer::quadratic;
+
+                case batch_optimizer::CGD_FR:
+                        return ls_initializer::quadratic;
+
+                case batch_optimizer::CGD_HS:
+                        return ls_initializer::quadratic;
+
+                case batch_optimizer::CGD_LS:
+                        return ls_initializer::quadratic;
+
+                case batch_optimizer::CGD_N:
+                        return ls_initializer::quadratic;
+
+                case batch_optimizer::CGD_DYCD:
+                        return ls_initializer::quadratic;
+
+                case batch_optimizer::CGD_DYHS:
+                        return ls_initializer::quadratic;
+
+                case batch_optimizer::GD:
+                        return ls_initializer::quadratic;
+
+                default:
+                        throw std::runtime_error("make_lsinit: optimization method not handled");
+                }
+        }
+
+        static ls_strategy make_lsstrat(const batch_optimizer optimizer)
+        {
+                switch (optimizer)
+                {
+                case batch_optimizer::LBFGS:
+                        return ls_strategy::interpolation;
+
+                case batch_optimizer::CGD: // fall through!
+                case batch_optimizer::CGD_PRP:
+                        return ls_strategy::interpolation;
+
+                case batch_optimizer::CGD_CD:
+                        return ls_strategy::interpolation;
+
+                case batch_optimizer::CGD_DY:
+                        return ls_strategy::backtrack_wolfe;
+
+                case batch_optimizer::CGD_FR:
+                        return ls_strategy::interpolation;
+
+                case batch_optimizer::CGD_HS:
+                        return ls_strategy::interpolation;
+
+                case batch_optimizer::CGD_LS:
+                        return ls_strategy::interpolation;
+
+                case batch_optimizer::CGD_N:
+                        return ls_strategy::interpolation;
+
+                case batch_optimizer::CGD_DYCD:
+                        return ls_strategy::backtrack_wolfe;
+
+                case batch_optimizer::CGD_DYHS:
+                        return ls_strategy::backtrack_wolfe;
+
+                case batch_optimizer::GD:
+                        return ls_strategy::backtrack_wolfe;
+
+                default:
+                        throw std::runtime_error("make_lsstrat: optimization method not handled");
+                }
+        }
+
+        batch_params_t::batch_params_t(
+                const std::size_t max_iterations,
+                const scalar_t epsilon,
+                const batch_optimizer optimizer,
+                const ls_initializer lsinit,
+                const ls_strategy lsstrat,
+                const opulog_t& ulog,
+                const std::size_t hsize) :
+                m_ulog(ulog),
+                m_max_iterations(max_iterations),
+                m_epsilon(epsilon),
+                m_optimizer(optimizer),
+                m_ls_initializer(lsinit),
+                m_ls_strategy(lsstrat),
+                m_hsize(hsize)
+        {
+        }
+
+        batch_params_t::batch_params_t(
+                const std::size_t max_iterations,
+                const scalar_t epsilon,
+                const batch_optimizer optimizer,
+                const opulog_t& ulog,
+                const std::size_t hsize) :
+                m_ulog(ulog),
+                m_max_iterations(max_iterations),
+                m_epsilon(epsilon),
+                m_optimizer(optimizer),
+                m_ls_initializer(make_lsinit(optimizer)),
+                m_ls_strategy(make_lsstrat(optimizer)),
+                m_hsize(hsize)
+        {
+        }
+}
+
