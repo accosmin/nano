67ecb8c21dc220b5c073f92eea716c63aeefb1b8
Cosmin Atanasoaei <catanasoaei@nexthink.com>
2016-07-29 12:36:11 +0200

    forgot to add file

---
+++ apps/stats.cpp
@@ -0,0 +1,25 @@
+#include "math/stats.hpp"
+#include "text/from_string.hpp"
+#include <iostream>
+#include <iomanip>
+
+int main(int argc, const char* argv[])
+{
+        if (argc == 1)
+        {
+                std::cout << "usage: <list of numbers>\n";
+                return EXIT_FAILURE;
+        }
+
+        nano::stats_t<double> stats;
+        for (int i = 1; i < argc; ++ i)
+        {
+                stats(nano::from_string<double>(argv[i]));
+        }
+
+        std::cout << std::fixed << std::setprecision(6);
+        std::cout << stats.avg() << "+/-" << stats.stdev() << " [" << stats.min() << ", " << stats.max() << "]\n";
+
+        // OK
+        return EXIT_SUCCESS;
+}
