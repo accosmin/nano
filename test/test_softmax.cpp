3170a9528daa28a0475142cc485974560f9fe121
Cosmin Atanasoaei <accosmin@gmail.com>
2016-08-23 22:39:43 +0200

    fix unit test for softmax (to check its gradient)

--- test/test_softmax.cpp
+++ test/test_softmax.cpp
@@ -1,44 +1,57 @@
 #include "utest.hpp"
+#include "tensor.h"
 #include "math/softmax.hpp"
 #include "math/epsilon.hpp"
-
-namespace test
-{
-        template
-        <
-                typename tvector,
-                typename tscalar = typename tvector::Scalar,
-                typename tsize = typename tvector::Index
-        >
-        void check_softmax(const tsize dims, const tscalar beta)
-        {
-                const tvector v = tvector::Random(dims);
-                const tscalar s = nano::softmax_value(v, beta);
-                const tscalar diff = std::fabs(v.template lpNorm<Eigen::Infinity>() - s);
-                std::cout << "beta = " << beta << ", diff = " << diff << std::endl;
-        }
-}
 
 NANO_BEGIN_MODULE(test_softmax)
 
 NANO_CASE(value)
 {
+        using namespace nano;
+
         for (auto test = 0; test < 10; ++ test)
         {
                 const auto dims = 10;
-                const auto v = Eigen::VectorXd::Random(dims);
+                const vector_t v = vector_t::Random(dims).array().abs();
 
-                for (auto beta = 1.0; beta < 1000.0; beta *= 2.0)
+                auto last_diff = std::numeric_limits<scalar_t>::max();
+                for (auto beta = scalar_t(1); beta < scalar_t(65); beta *= 2)
                 {
                         const auto s = nano::softmax_value(v, beta);
                         NANO_CHECK(std::isfinite(s));
-                        const auto diff = std::fabs(v.lpNorm<Eigen::Infinity>() - s);
-                        std::cout << "beta = " << beta << ", diff = " << diff << std::endl;
+
+                        const auto m = v.lpNorm<Eigen::Infinity>();
+                        const auto diff = std::fabs(m - s) / m;
+                        NANO_CHECK_LESS(diff, last_diff);
+                        last_diff = diff;
                 }
+                NANO_CHECK_LESS(last_diff, nano::epsilon3<scalar_t>());
         }
 }
 
-// todo: check why the error does not always decrease with 1/beta
-// todo: check also the gradient
+NANO_CASE(vgrad)
+{
+        // todo: check gradient
+        using namespace nano;
+
+        for (auto test = 0; test < 10; ++ test)
+        {
+                const auto dims = 10;
+                const vector_t v = vector_t::Random(dims).array().abs();
+
+                auto last_diff = std::numeric_limits<scalar_t>::max();
+                for (auto beta = scalar_t(1); beta < scalar_t(65); beta *= 2)
+                {
+                        const auto s = nano::softmax_value(v, beta);
+                        NANO_CHECK(std::isfinite(s));
+
+                        const auto m = v.lpNorm<Eigen::Infinity>();
+                        const auto diff = std::fabs(m - s) / m;
+                        NANO_CHECK_LESS(diff, last_diff);
+                        last_diff = diff;
+                }
+                NANO_CHECK_LESS(last_diff, nano::epsilon3<scalar_t>());
+        }
+}
 
 NANO_END_MODULE()
