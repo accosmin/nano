99fb310f17b9e63d983d410c30c75bf2c44d2087
Cosmin Atanasoaei <accosmin@gmail.com>
2016-09-05 19:30:32 +0200

    use a fixed number of samples (to easily compare trainers on different architectures)

--- apps/benchmark_trainers.cpp
+++ apps/benchmark_trainers.cpp
@@ -192,7 +192,7 @@ int main(int argc, const char* argv[])
         // create task
         const size_t rows = 16;
         const size_t cols = 16;
-        const size_t count = nano::logical_cpus() * 32 * 100;
+        const size_t count = 60000;
         const color_mode color = color_mode::rgb;
 
         charset_task_t task(charset::digit, color, rows, cols, count);
