2be8fa2ebc264982175fc032f2cf88174ff7b60b
Cosmin Atanasoaei <accosmin@gmail.com>
2016-09-13 12:33:02 +0200

    expand the set of linear algebra OpenCL kernels

--- src/opencl/kernels.h
+++ src/opencl/kernels.h
@@ -2,12 +2,29 @@
 
 namespace nano
 {
+        ///
+        /// \brief basic linear algebra kernels:
+        ///     c - constant
+        //      v - vector
+        ///     m - matrix
+        ///     p - plus (addition)
+        ///
         inline const char* opencl_kernels()
         {
                 return R"xxx(
 
+                // add a constant to a vector: z = x + c
+                __kernel void vpc(
+                        __global const float* x,
+                        const float c,
+                        __global float* z)
+                {
+                        const int i = get_global_id(0);
+                        z[i] = x[i] + c;
+                }
+
                 // add two vectors: z = x + y
-                __kernel void add_vv(
+                __kernel void vpv(
                         __global const float* x,
                         __global const float* y,
                         __global float* z)
@@ -16,11 +33,21 @@ namespace nano
                         z[i] = x[i] + y[i];
                 }
 
-                // multiply a matrix by a vector: y = A * x
-                __kernel void mul_mv(
+                // add two vectors with multiplication factors: z = a * x + b * y
+                __kernel void vcpvc(
+                        __global const float* x, const float a,
+                        __global const float* y, const float b,
+                        __global float* z)
+                {
+                        const int i = get_global_id(0);
+                        z[i] = a * x[i] + b * y[i];
+                }
+
+                // multiply a matrix by a vector: z = A * x
+                __kernel void mv(
                         __global const float* A, const int cols,
                         __global const float* x,
-                        __global float* y)
+                        __global float* z)
                 {
                         const int row = get_global_id(0);
                         float sum = 0;
@@ -28,14 +55,46 @@ namespace nano
                         {
                                 sum += A[row * cols + col] * x[col];
                         }
-                        y[row] = sum;
+                        z[row] = sum;
                 }
 
-                // multiply a matrix by a matrix: C = A * B
-                __kernel void mul_mm(
+                // multiply a matrix by a vector and add a constant: y = A * x + c
+                __kernel void mvpc(
+                        __global const float* A, const int cols,
+                        __global const float* x,
+                        const float c,
+                        __global float* z)
+                {
+                        const int row = get_global_id(0);
+                        float sum = 0;
+                        for (int col = 0; col < cols; ++ col)
+                        {
+                                sum += A[row * cols + col] * x[col];
+                        }
+                        z[row] = sum + c;
+                }
+
+                // multiply a matrix by a vector and add a vector: y = A * x + y
+                __kernel void mvpv(
+                        __global const float* A, const int cols,
+                        __global const float* x,
+                        __global const float* y,
+                        __global float* z)
+                {
+                        const int row = get_global_id(0);
+                        float sum = 0;
+                        for (int col = 0; col < cols; ++ col)
+                        {
+                                sum += A[row * cols + col] * x[col];
+                        }
+                        z[row] = sum + y[row];
+                }
+
+                // multiply a matrix by a matrix: Z = A * B
+                __kernel void mm(
                         __global const float* A, const int colsA,
                         __global const float* B, const int colsB,
-                        __global float* C)
+                        __global float* Z)
                 {
                         const int rowA = get_global_id(0);
                         const int colB = get_global_id(1);
@@ -44,7 +103,7 @@ namespace nano
                         {
                                 sum += A[rowA * colsA + colA] * B[colA * colsB + colB];
                         }
-                        C[rowA * colsB + colB] = sum;
+                        Z[rowA * colsB + colB] = sum;
                 }
 
                 )xxx";
--- test/test_opencl.cpp
+++ test/test_opencl.cpp
@@ -12,41 +12,108 @@ auto rng_size = nano::make_rng<tensor_size_t>(1, 137);
 auto rng_value = nano::make_rng<scalar_t>(scalar_t(-0.1), scalar_t(+0.1));
 auto n_tests = 11;
 
+scalar_t make_scalar()
+{
+        return rng_value();
+}
+
+vector_t make_vector(const tensor_size_t dims)
+{
+        vector_t x(dims);
+        tensor::set_random(rng_value, x);
+        return x;
+}
+
+matrix_t make_matrix(const tensor_size_t rows, const tensor_size_t cols)
+{
+        matrix_t x(rows, cols);
+        tensor::set_random(rng_value, x);
+        return x;
+}
+
 NANO_BEGIN_MODULE(test_opencl)
 
+// initialize OpenCL context
 opencl_manager_t theocl;
 NANO_REQUIRE_NOTHROW(theocl.init());
 NANO_REQUIRE_NOTHROW(theocl.select(CL_DEVICE_TYPE_GPU));
 
+// create supported kernels
 cl::Program program;
 NANO_REQUIRE_NOTHROW(program = theocl.make_program_from_text(opencl_kernels()));
 
+cl::Kernel kernel_vpc;
+cl::Kernel kernel_vpv;
+cl::Kernel kernel_vcpvc;
+
+cl::Kernel kernel_mv;
+cl::Kernel kernel_mvpc;
+cl::Kernel kernel_mvpv;
+
+cl::Kernel kernel_mm;
+
+NANO_REQUIRE_NOTHROW(kernel_vpc = theocl.make_kernel(program, "vpc"));
+NANO_REQUIRE_NOTHROW(kernel_vpv = theocl.make_kernel(program, "vpv"));
+NANO_REQUIRE_NOTHROW(kernel_vcpvc = theocl.make_kernel(program, "vcpvc"));
+
+NANO_REQUIRE_NOTHROW(kernel_mv = theocl.make_kernel(program, "mv"));
+NANO_REQUIRE_NOTHROW(kernel_mvpc = theocl.make_kernel(program, "mvpc"));
+NANO_REQUIRE_NOTHROW(kernel_mvpv = theocl.make_kernel(program, "mvpv"));
+
+NANO_REQUIRE_NOTHROW(kernel_mm = theocl.make_kernel(program, "mm"));
+
+// use this command queue to send tasks
 cl::CommandQueue& queue = theocl.command_queue();
 
-NANO_CASE(add_vv)
+// z = x + c
+NANO_CASE(vpc)
 {
-        cl::Kernel kernel;
-        NANO_REQUIRE_NOTHROW(kernel = theocl.make_kernel(program, "add_vv"));
-
         for (int test = 0; test < n_tests; ++ test)
         {
                 const auto dims = rng_size();
+                auto c = make_scalar();
+                auto x = make_vector(dims);
+                auto z = make_vector(dims);
 
-                vector_t x(dims), y(dims), z(dims);
-                tensor::set_random(rng_value, x, y, z);
+                cl::Buffer xbuffer = theocl.make_buffer(x, CL_MEM_READ_WRITE);
+                cl::Buffer zbuffer = theocl.make_buffer(z, CL_MEM_READ_ONLY);
+
+                kernel_vpc.setArg(0, xbuffer);
+                kernel_vpc.setArg(1, c);
+                kernel_vpc.setArg(2, zbuffer);
+
+                NANO_CHECK(theocl.write(xbuffer, x) == CL_SUCCESS);
+
+                queue.enqueueNDRangeKernel(kernel_vpc, cl::NullRange, cl::NDRange(size_t(dims)), cl::NullRange);
+                queue.finish();
+
+                NANO_CHECK(theocl.read(zbuffer, z) == CL_SUCCESS);
+                NANO_CHECK_EIGEN_CLOSE(x.array() + c, z.array(), nano::epsilon0<scalar_t>());
+        }
+}
+
+// z = x + y
+NANO_CASE(vpv)
+{
+        for (int test = 0; test < n_tests; ++ test)
+        {
+                const auto dims = rng_size();
+                auto x = make_vector(dims);
+                auto y = make_vector(dims);
+                auto z = make_vector(dims);
 
                 cl::Buffer xbuffer = theocl.make_buffer(x, CL_MEM_READ_WRITE);
                 cl::Buffer ybuffer = theocl.make_buffer(y, CL_MEM_READ_WRITE);
                 cl::Buffer zbuffer = theocl.make_buffer(z, CL_MEM_READ_ONLY);
 
-                kernel.setArg(0, xbuffer);
-                kernel.setArg(1, ybuffer);
-                kernel.setArg(2, zbuffer);
+                kernel_vpv.setArg(0, xbuffer);
+                kernel_vpv.setArg(1, ybuffer);
+                kernel_vpv.setArg(2, zbuffer);
 
                 NANO_CHECK(theocl.write(xbuffer, x) == CL_SUCCESS);
                 NANO_CHECK(theocl.write(ybuffer, y) == CL_SUCCESS);
 
-                queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(size_t(dims)), cl::NullRange);
+                queue.enqueueNDRangeKernel(kernel_vpv, cl::NullRange, cl::NDRange(size_t(dims)), cl::NullRange);
                 queue.finish();
 
                 NANO_CHECK(theocl.read(zbuffer, z) == CL_SUCCESS);
@@ -54,75 +121,169 @@ NANO_CASE(add_vv)
         }
 }
 
-NANO_CASE(mul_mv)
+// z = a * x + b * y
+NANO_CASE(vcpvc)
 {
-        cl::Kernel kernel;
-        NANO_REQUIRE_NOTHROW(kernel = theocl.make_kernel(program, "mul_mv"));
+        for (int test = 0; test < n_tests; ++ test)
+        {
+                const auto dims = rng_size();
+                auto a = make_scalar();
+                auto b = make_scalar();
+                auto x = make_vector(dims);
+                auto y = make_vector(dims);
+                auto z = make_vector(dims);
 
+                cl::Buffer xbuffer = theocl.make_buffer(x, CL_MEM_READ_WRITE);
+                cl::Buffer ybuffer = theocl.make_buffer(y, CL_MEM_READ_WRITE);
+                cl::Buffer zbuffer = theocl.make_buffer(z, CL_MEM_READ_ONLY);
+
+                kernel_vcpvc.setArg(0, xbuffer);
+                kernel_vcpvc.setArg(1, a);
+                kernel_vcpvc.setArg(2, ybuffer);
+                kernel_vcpvc.setArg(3, b);
+                kernel_vcpvc.setArg(4, zbuffer);
+
+                NANO_CHECK(theocl.write(xbuffer, x) == CL_SUCCESS);
+                NANO_CHECK(theocl.write(ybuffer, y) == CL_SUCCESS);
+
+                queue.enqueueNDRangeKernel(kernel_vcpvc, cl::NullRange, cl::NDRange(size_t(dims)), cl::NullRange);
+                queue.finish();
+
+                NANO_CHECK(theocl.read(zbuffer, z) == CL_SUCCESS);
+                NANO_CHECK_EIGEN_CLOSE((a * x + b * y), z, nano::epsilon0<scalar_t>());
+        }
+}
+
+// Z = A * x
+NANO_CASE(mv)
+{
         for (int test = 0; test < n_tests; ++ test)
         {
                 const auto rows = rng_size();
                 const auto cols = rng_size();
-
-                matrix_t A(rows, cols);
-                vector_t x(cols), y(rows);
-                tensor::set_random(rng_value, x, y, A);
+                auto A = make_matrix(rows, cols);
+                auto x = make_vector(cols);
+                auto z = make_vector(rows);
 
                 cl::Buffer Abuffer = theocl.make_buffer(A, CL_MEM_READ_WRITE);
                 cl::Buffer xbuffer = theocl.make_buffer(x, CL_MEM_READ_WRITE);
-                cl::Buffer ybuffer = theocl.make_buffer(y, CL_MEM_READ_ONLY);
+                cl::Buffer zbuffer = theocl.make_buffer(z, CL_MEM_READ_ONLY);
 
-                kernel.setArg(0, Abuffer);
-                kernel.setArg(1, static_cast<int>(cols));
-                kernel.setArg(2, xbuffer);
-                kernel.setArg(3, ybuffer);
+                kernel_mv.setArg(0, Abuffer);
+                kernel_mv.setArg(1, static_cast<int>(cols));
+                kernel_mv.setArg(2, xbuffer);
+                kernel_mv.setArg(3, zbuffer);
 
                 NANO_CHECK(theocl.write(Abuffer, A) == CL_SUCCESS);
                 NANO_CHECK(theocl.write(xbuffer, x) == CL_SUCCESS);
 
-                queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(size_t(rows)), cl::NullRange);
+                queue.enqueueNDRangeKernel(kernel_mv, cl::NullRange, cl::NDRange(size_t(rows)), cl::NullRange);
                 queue.finish();
 
-                NANO_CHECK(theocl.read(ybuffer, y) == CL_SUCCESS);
-                NANO_CHECK_EIGEN_CLOSE((A * x), y, nano::epsilon0<scalar_t>());
+                NANO_CHECK(theocl.read(zbuffer, z) == CL_SUCCESS);
+                NANO_CHECK_EIGEN_CLOSE((A * x), z, nano::epsilon0<scalar_t>());
         }
 }
 
-NANO_CASE(mul_mm)
+// Z = A * x + c
+NANO_CASE(mvpc)
 {
-        cl::Kernel kernel;
-        NANO_REQUIRE_NOTHROW(kernel = theocl.make_kernel(program, "mul_mm"));
+        for (int test = 0; test < n_tests; ++ test)
+        {
+                const auto rows = rng_size();
+                const auto cols = rng_size();
+                auto c = make_scalar();
+                auto A = make_matrix(rows, cols);
+                auto x = make_vector(cols);
+                auto z = make_vector(rows);
 
+                cl::Buffer Abuffer = theocl.make_buffer(A, CL_MEM_READ_WRITE);
+                cl::Buffer xbuffer = theocl.make_buffer(x, CL_MEM_READ_WRITE);
+                cl::Buffer zbuffer = theocl.make_buffer(z, CL_MEM_READ_ONLY);
+
+                kernel_mvpc.setArg(0, Abuffer);
+                kernel_mvpc.setArg(1, static_cast<int>(cols));
+                kernel_mvpc.setArg(2, xbuffer);
+                kernel_mvpc.setArg(3, c);
+                kernel_mvpc.setArg(4, zbuffer);
+
+                NANO_CHECK(theocl.write(Abuffer, A) == CL_SUCCESS);
+                NANO_CHECK(theocl.write(xbuffer, x) == CL_SUCCESS);
+
+                queue.enqueueNDRangeKernel(kernel_mvpc, cl::NullRange, cl::NDRange(size_t(rows)), cl::NullRange);
+                queue.finish();
+
+                NANO_CHECK(theocl.read(zbuffer, z) == CL_SUCCESS);
+                NANO_CHECK_EIGEN_CLOSE((A * x).array() + c, z.array(), nano::epsilon0<scalar_t>());
+        }
+}
+
+// Z = A * x + y
+NANO_CASE(mvpv)
+{
+        for (int test = 0; test < n_tests; ++ test)
+        {
+                const auto rows = rng_size();
+                const auto cols = rng_size();
+                auto A = make_matrix(rows, cols);
+                auto x = make_vector(cols);
+                auto y = make_vector(rows);
+                auto z = make_vector(rows);
+
+                cl::Buffer Abuffer = theocl.make_buffer(A, CL_MEM_READ_WRITE);
+                cl::Buffer xbuffer = theocl.make_buffer(x, CL_MEM_READ_WRITE);
+                cl::Buffer ybuffer = theocl.make_buffer(y, CL_MEM_READ_WRITE);
+                cl::Buffer zbuffer = theocl.make_buffer(z, CL_MEM_READ_ONLY);
+
+                kernel_mvpv.setArg(0, Abuffer);
+                kernel_mvpv.setArg(1, static_cast<int>(cols));
+                kernel_mvpv.setArg(2, xbuffer);
+                kernel_mvpv.setArg(3, ybuffer);
+                kernel_mvpv.setArg(4, zbuffer);
+
+                NANO_CHECK(theocl.write(Abuffer, A) == CL_SUCCESS);
+                NANO_CHECK(theocl.write(xbuffer, x) == CL_SUCCESS);
+                NANO_CHECK(theocl.write(ybuffer, y) == CL_SUCCESS);
+
+                queue.enqueueNDRangeKernel(kernel_mvpv, cl::NullRange, cl::NDRange(size_t(rows)), cl::NullRange);
+                queue.finish();
+
+                NANO_CHECK(theocl.read(zbuffer, z) == CL_SUCCESS);
+                NANO_CHECK_EIGEN_CLOSE((A * x + y), z, nano::epsilon0<scalar_t>());
+        }
+}
+
+// Z = A * B
+NANO_CASE(mm)
+{
         for (int test = 0; test < n_tests; ++ test)
         {
                 const auto rowsA = rng_size();
                 const auto colsA = rng_size();
                 const auto rowsB = colsA;
                 const auto colsB = rng_size();
-
-                matrix_t A(rowsA, colsA);
-                matrix_t B(rowsB, colsB);
-                matrix_t C(rowsA, colsB);
-                tensor::set_random(rng_value, A, B, C);
+                auto A = make_matrix(rowsA, colsA);
+                auto B = make_matrix(rowsB, colsB);
+                auto Z = make_matrix(rowsA, colsB);
 
                 cl::Buffer Abuffer = theocl.make_buffer(A, CL_MEM_READ_WRITE);
                 cl::Buffer Bbuffer = theocl.make_buffer(B, CL_MEM_READ_WRITE);
-                cl::Buffer Cbuffer = theocl.make_buffer(C, CL_MEM_READ_ONLY);
+                cl::Buffer Zbuffer = theocl.make_buffer(Z, CL_MEM_READ_ONLY);
 
-                kernel.setArg(0, Abuffer);
-                kernel.setArg(1, static_cast<int>(colsA));
-                kernel.setArg(2, Bbuffer);
-                kernel.setArg(3, static_cast<int>(colsB));
-                kernel.setArg(4, Cbuffer);
+                kernel_mm.setArg(0, Abuffer);
+                kernel_mm.setArg(1, static_cast<int>(colsA));
+                kernel_mm.setArg(2, Bbuffer);
+                kernel_mm.setArg(3, static_cast<int>(colsB));
+                kernel_mm.setArg(4, Zbuffer);
 
                 NANO_CHECK(theocl.write(Abuffer, A) == CL_SUCCESS);
                 NANO_CHECK(theocl.write(Bbuffer, B) == CL_SUCCESS);
 
-                queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(size_t(rowsA), size_t(colsB)), cl::NullRange);
+                queue.enqueueNDRangeKernel(kernel_mm, cl::NullRange, cl::NDRange(size_t(rowsA), size_t(colsB)), cl::NullRange);
                 queue.finish();
 
-                NANO_CHECK(theocl.read(Cbuffer, C) == CL_SUCCESS);
-                NANO_CHECK_EIGEN_CLOSE((A * B), C, nano::epsilon0<scalar_t>());
+                NANO_CHECK(theocl.read(Zbuffer, Z) == CL_SUCCESS);
+                NANO_CHECK_EIGEN_CLOSE((A * B), Z, nano::epsilon0<scalar_t>());
         }
 }
 
