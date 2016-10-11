f7c86090b3d5b47d8f6a3d284732494325a01d05
Cosmin Atanasoaei <accosmin@gmail.com>
2016-10-11 23:07:02 +0200

    use final instead of override

--- src/layers/activation.h
+++ src/layers/activation.h
@@ -30,33 +30,33 @@ namespace nano
                 virtual ~activation_layer_t() {}
 
                 // resize to process new tensors of the given type
-                virtual tensor_size_t resize(const tensor3d_t& tensor) override
+                virtual tensor_size_t resize(const tensor3d_t& tensor) final
                 {
                         return _resize(tensor);
                 }
 
                 // reset parameters
-                virtual void zero_params() override {}
-                virtual void random_params(scalar_t min, scalar_t max) override { NANO_UNUSED2(min, max); }
+                virtual void zero_params() final {}
+                virtual void random_params(scalar_t min, scalar_t max) final { NANO_UNUSED2(min, max); }
 
                 // serialize parameters
-                virtual scalar_t* save_params(scalar_t* params) const override { return params; }
-                virtual const scalar_t* load_params(const scalar_t* params) override { return params; }
+                virtual scalar_t* save_params(scalar_t* params) const final { return params; }
+                virtual const scalar_t* load_params(const scalar_t* params) final { return params; }
 
                 // process inputs (compute outputs & gradients)
-                virtual const tensor3d_t& output(const tensor3d_t& input) override { return _output(input); }
-                virtual const tensor3d_t& ginput(const tensor3d_t& output) override { return _ginput(output); }
-                virtual void gparam(const tensor3d_t& output, scalar_t*) override { return _gparam(output); }
+                virtual const tensor3d_t& output(const tensor3d_t& input) final { return _output(input); }
+                virtual const tensor3d_t& ginput(const tensor3d_t& output) final { return _ginput(output); }
+                virtual void gparam(const tensor3d_t& output, scalar_t*) final { return _gparam(output); }
 
                 // access functions
-                virtual tensor_size_t idims() const override { return m_data.size<0>(); }
-                virtual tensor_size_t irows() const override { return m_data.size<1>(); }
-                virtual tensor_size_t icols() const override { return m_data.size<2>(); }
-                virtual tensor_size_t odims() const override { return m_data.size<0>(); }
-                virtual tensor_size_t orows() const override { return m_data.size<1>(); }
-                virtual tensor_size_t ocols() const override { return m_data.size<2>(); }
-                virtual tensor_size_t psize() const override { return 0; }
-                virtual tensor_size_t flops() const override { return 10 * m_data.size(); }
+                virtual tensor_size_t idims() const final { return m_data.size<0>(); }
+                virtual tensor_size_t irows() const final { return m_data.size<1>(); }
+                virtual tensor_size_t icols() const final { return m_data.size<2>(); }
+                virtual tensor_size_t odims() const final { return m_data.size<0>(); }
+                virtual tensor_size_t orows() const final { return m_data.size<1>(); }
+                virtual tensor_size_t ocols() const final { return m_data.size<2>(); }
+                virtual tensor_size_t psize() const final { return 0; }
+                virtual tensor_size_t flops() const final { return 10 * m_data.size(); }
 
         private:
 
