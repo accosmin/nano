fd34c8e1f7ce802395a22c6cd55d70233d05e3e4
accosmin <accosmin@gmail.com>
2016-10-30 13:50:50 +0100

    consistency

--- test/test_batch.cpp
+++ test/test_batch.cpp
@@ -41,7 +41,7 @@ static void check_function(const function_t& function)
                         const auto& x0 = x0s[t];
                         const auto f0 = problem(x0);
                         const auto g_thres = epsilon2<scalar_t>();
-                        const auto x_thres = std::cbrt(epsilon2<scalar_t>());
+                        const auto x_thres = std::cbrt(g_thres);
 
                         // optimize
                         const auto params = batch_params_t(iterations, g_thres / 2);
