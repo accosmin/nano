12e96952f50277a603e6dd9572030cd09f92ab06
accosmin <accosmin@gmail.com>
2016-11-05 12:24:46 +0100

    fix setup of trainers for unit test

--- test/test_affine.cpp
+++ test/test_affine.cpp
@@ -9,9 +9,9 @@
 using namespace nano;
 
 const auto idims = tensor_size_t(3);
-const auto irows = tensor_size_t(10);
-const auto icols = tensor_size_t(10);
-const auto osize = tensor_size_t(10);
+const auto irows = tensor_size_t(5);
+const auto icols = tensor_size_t(7);
+const auto osize = tensor_size_t(4);
 const auto count = tensor_size_t(1000);
 const auto noise = epsilon2<scalar_t>();
 
@@ -45,7 +45,7 @@ NANO_CASE(training)
         NANO_REQUIRE(*model == task);
 
         // create loss
-        const std::map<string_t, string_t> lconfigs =
+        const std::vector<std::pair<string_t, string_t>> lconfigs =
         {
                 {"square", ""},
                 {"cauchy", ""}
@@ -55,22 +55,23 @@ NANO_CASE(training)
         const auto criterion = get_criteria().get("avg");
 
         // create trainers
+        const auto batch = 32;
         const auto epochs = 1000;
         const auto policy = trainer_policy::stop_early;
-        const std::map<string_t, string_t> tconfigs =
+        const std::vector<std::pair<string_t, string_t>> tconfigs =
         {
                 {"batch", to_params("opt", "gd", "epochs", epochs, "policy", policy)},
                 {"batch", to_params("opt", "cgd", "epochs", epochs, "policy", policy)},
                 {"batch", to_params("opt", "lbfgs", "epochs", epochs, "policy", policy)},
-                {"stoch", to_params("opt", "sg", "epochs", epochs, "policy", policy)},
-                {"stoch", to_params("opt", "sgm", "epochs", epochs, "policy", policy)},
-                {"stoch", to_params("opt", "ngd", "epochs", epochs, "policy", policy)},
-                {"stoch", to_params("opt", "adam", "epochs", epochs, "policy", policy)},
-                {"stoch", to_params("opt", "adagrad", "epochs", epochs, "policy", policy)},
-                {"stoch", to_params("opt", "adadelta", "epochs", epochs, "policy", policy)},
-                {"stoch", to_params("opt", "ag", "epochs", epochs, "policy", policy)},
-                {"stoch", to_params("opt", "agfr", "epochs", epochs, "policy", policy)},
-                {"stoch", to_params("opt", "aggr", "epochs", epochs, "policy", policy)}
+                {"stoch", to_params("opt", "sg", "epochs", epochs, "min_batch", batch, "max_batch", batch, "policy", policy)},
+                {"stoch", to_params("opt", "sgm", "epochs", epochs, "min_batch", batch, "max_batch", batch, "policy", policy)},
+                {"stoch", to_params("opt", "ngd", "epochs", epochs, "min_batch", batch, "max_batch", batch, "policy", policy)},
+                {"stoch", to_params("opt", "adam", "epochs", epochs, "min_batch", batch, "max_batch", batch, "policy", policy)},
+                {"stoch", to_params("opt", "adagrad", "epochs", epochs, "min_batch", batch, "max_batch", batch, "policy", policy)},
+                {"stoch", to_params("opt", "adadelta", "epochs", epochs, "min_batch", batch, "max_batch", batch, "policy", policy)},
+                {"stoch", to_params("opt", "ag", "epochs", epochs, "min_batch", batch, "max_batch", batch, "policy", policy)},
+                {"stoch", to_params("opt", "agfr", "epochs", epochs, "min_batch", batch, "max_batch", batch, "policy", policy)},
+                {"stoch", to_params("opt", "aggr", "epochs", epochs, "min_batch", batch, "max_batch", batch, "policy", policy)},
         };
 
         // check training
