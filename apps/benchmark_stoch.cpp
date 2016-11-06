9cb47744d65a906e9998e68d5ce116b479b21809
accosmin <accosmin@gmail.com>
2016-11-06 11:35:49 +0100

    more reasonable convergence criteria for benchmarking optimizers

--- apps/benchmark_stoch.cpp
+++ apps/benchmark_stoch.cpp
@@ -65,7 +65,7 @@ int main(int argc, const char* argv[])
         cmdline.add("", "trials",       "number of random trials for each test function", "100");
         cmdline.add("", "epochs",       "optimization: number of epochs", "100");
         cmdline.add("", "epoch-size",   "optimization: number of iterations per epoch", "100");
-        cmdline.add("", "epsilon",      "convergence criteria", std::sqrt(nano::epsilon3<scalar_t>()));
+        cmdline.add("", "epsilon",      "convergence criteria", nano::epsilon3<scalar_t>());
         cmdline.add("", "convex",       "use only convex test functions");
 
         cmdline.process(argc, argv);
