d302dced9272de9a2f57349dff7a3fe39ce0ee45
Cosmin Atanasoaei <accosmin@gmail.com>
2017-02-13 12:42:20 +0100

    simplify

--- src/layers/make_layers.h
+++ src/layers/make_layers.h
@@ -1,44 +1,39 @@
 #pragma once
 
-#include "stringi.h"
 #include "text/to_params.h"
 
 namespace nano
 {
         ///
-        /// \brief utilities to simplify creation of multi-layer models for benchmarking and testing.
+        /// \brief utilities to simplify creation of multi-layer models.
         ///
-
         inline string_t make_layer(const string_t& description)
         {
                 return description + ";";
         }
 
-        template <typename tsize>
-        string_t make_affine_layer(const tsize dims, const string_t& activation = "act-snorm")
+        inline string_t make_affine_layer(const tensor_size_t dims, const string_t& activation = "act-snorm")
         {
                 return  make_layer("affine:" + to_params("dims", dims)) +
                         make_layer(activation);
         }
 
-        template <typename tsize>
-        string_t make_output_layer(const tsize dims, const string_t& activation = "")
+        inline string_t make_output_layer(const tensor_size_t dims, const string_t& activation = "")
         {
                 return  make_affine_layer(dims, activation);
         }
 
-        string_t make_output_layer(const dim3d_t& dims, const string_t& activation = "")
+        inline string_t make_output_layer(const dim3d_t& dims, const string_t& activation = "")
         {
                 return make_output_layer(nano::size(dims), activation);
         }
 
-        template <typename tsize>
-        string_t make_conv_layer(
-                const tsize dims, const tsize rows, const tsize cols, const tsize conn,
-                const string_t& activation = "act-snorm", const tsize drow = 1, const tsize dcol = 1)
+        inline string_t make_conv_layer(
+                const tensor_size_t dims, const tensor_size_t rows, const tensor_size_t cols, const tensor_size_t conn,
+                const string_t& activation = "act-snorm",
+                const tensor_size_t drow = 1, const tensor_size_t dcol = 1)
         {
-                return  make_layer("conv:" + to_params(
-                                "dims", dims, "rows", rows, "cols", cols, "conn", conn, "drow", drow, "dcol", dcol)) +
+                return  make_layer("conv:" + to_params("dims", dims, "rows", rows, "cols", cols, "conn", conn, "drow", drow, "dcol", dcol)) +
                         make_layer(activation);
         }
 }
