42397a944402daccb76fd8f4ef43128658b899d2
Cosmin Atanasoaei <accosmin@gmail.com>
2017-02-22 12:13:54 +0100

    formatting

--- src/math/numeric.h
+++ src/math/numeric.h
@@ -87,7 +87,7 @@ namespace nano
                 typename tscalar_min,
                 typename tscalar_max
         >
-        tscalar clamp(tscalar value, tscalar_min min_value, tscalar_max max_value)
+        tscalar clamp(const tscalar value, const tscalar_min min_value, const tscalar_max max_value)
         {
                 return  value < static_cast<tscalar>(min_value) ? static_cast<tscalar>(min_value) :
                         (value > static_cast<tscalar>(max_value) ? static_cast<tscalar>(max_value) : value);
--- src/stoch/rmsprop.h
+++ src/stoch/rmsprop.h
@@ -7,7 +7,7 @@ namespace nano
         ///
         /// \brief stochastic RMSProp (AdaGrad with an exponentially weighted running average of the gradients)
         ///     see Lecture 6.5 - rmsprop, COURSERA: Neural Networks for Machine Learning
-        //      by Tieleman, T. and Hinton, G. (2012)
+        ///      by Tieleman, T. and Hinton, G. (2012)
         ///
         struct stoch_rmsprop_t final : public stoch_optimizer_t
         {
