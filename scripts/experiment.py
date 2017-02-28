4824ea2821a158eff2d7884cf32d0119c8b1dda6
Cosmin Atanasoaei <accosmin@gmail.com>
2017-02-28 10:26:19 +0100

    move plotting utilities to own Python script

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -1,6 +1,7 @@
 import os
 import re
 import time
+import plotter
 import subprocess
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
@@ -64,74 +65,12 @@ class experiment:
                 lfile.close()
                 self.log("|--->training done, see <", lpath, ">")
 
-        def get_csv(self, spath):
-                # state file with the following format:
-                #  (epoch, {train, valid, test} x {criterion, loss{average, variance, maximum}, error{average, variance, maximum}}, time)+
-                name = os.path.basename(spath).replace(".state", "")
-                name = name.replace(name[0 : name.find("_") + 1], "")
-                data = mlab.csv2rec(spath, delimiter = ' ', names = None)
-                return name, data
-
-        def get_csvs(self, spaths):
-                datas = []
-                names = []
-                for spath in spaths:
-                        name, data = self.get_csv(spath)
-                        datas.append(data)
-                        names.append(name)
-                return names, datas
-
         def plot_one(self, spath, ppath):
-                title, data = self.get_csv(spath)
-                with PdfPages(ppath) as pdf:
-                        for col in (0, 1, 4):
-                                # x axis - epoch/iteration index
-                                xname = data.dtype.names[0]
-                                xlabel = xname
-                                # y axis - train/validation/test datasets
-                                yname0 = data.dtype.names[col + 1]
-                                yname1 = data.dtype.names[col + 8]
-                                yname2 = data.dtype.names[col + 15]
-                                ylabel = yname0.replace("train_", "")
-                                # plot
-                                plt.xlabel(xlabel, fontsize = "smaller")
-                                plt.ylabel(ylabel, fontsize = "smaller")
-                                plt.title(title, weight = "bold")
-                                plt.plot(data[xname], data[yname0], "r--", label = yname0)
-                                plt.plot(data[xname], data[yname1], "g:", label = yname1)
-                                plt.plot(data[xname], data[yname2], "b-", label = yname2)
-                                plt.legend(fontsize = "smaller")
-                                pdf.savefig()
-                                plt.close()
+                plotter.plot_one(spath, ppath)
                 self.log("|--->plotting done, see <", ppath, ">")
 
-        def plot_many_wrt(self, spaths, names, datas, pdf, xcol, ycol):
-                colnames = datas[0].dtype.names
-                title = colnames[ycol + 1]
-                # x axis - epoch/iteration index
-                xname = colnames[xcol]
-                xlabel = xname
-                # y axis - train/validation/test datasets
-                yname = colnames[ycol + 1]
-                ylabel = yname.replace("train_", "").replace("valid_", "").replace("test_", "")
-                # plot
-                plt.xlabel(xlabel, fontsize = "smaller")
-                plt.ylabel(ylabel, fontsize = "smaller")
-                plt.title(title, weight = "bold")
-                for i, spath in enumerate(spaths):
-                        plt.plot(datas[i][xname], datas[i][yname], label = names[i])
-                plt.legend(fontsize = "smaller")
-                pdf.savefig()
-                plt.close()
-
         def plot_many(self, spaths, ppath):
-                names, datas = self.get_csvs(spaths)
-                with PdfPages(ppath) as pdf:
-                        for col in (0, 1, 4, 7, 8, 11, 14, 15, 18):
-                                # plot wrt epoch/iteration number
-                                self.plot_many_wrt(spaths, names, datas, pdf, 0, col)
-                                # plot wrt time
-                                self.plot_many_wrt(spaths, names, datas, pdf, 22, col)
+                plotter.plot_many(spaths, ppath)
                 self.log("|--->plotting done, see <", ppath, ">")
 
         def run_one(self, trial, mname, mparam, tname, tparam, cname, cparam, lname, lparam):
---
+++ scripts/plotter.py
@@ -0,0 +1,72 @@
+import os
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+def get_csv(spath):
+        # state file with the following format:
+        #  (epoch, {train, valid, test} x {criterion, loss{average, variance, maximum}, error{average, variance, maximum}}, time)+
+        name = os.path.basename(spath).replace(".state", "")
+        name = name.replace(name[0 : name.find("_") + 1], "")
+        data = mlab.csv2rec(spath, delimiter = ' ', names = None)
+        return name, data
+
+def get_csvs(spaths):
+        datas = []
+        names = []
+        for spath in spaths:
+                name, data = get_csv(spath)
+                datas.append(data)
+                names.append(name)
+        return names, datas
+
+def plot_one(spath, ppath):
+        title, data = get_csv(spath)
+        with PdfPages(ppath) as pdf:
+                for col in (0, 1, 4):
+                        # x axis - epoch/iteration index
+                        xname = data.dtype.names[0]
+                        xlabel = xname
+                        # y axis - train/validation/test datasets
+                        yname0 = data.dtype.names[col + 1]
+                        yname1 = data.dtype.names[col + 8]
+                        yname2 = data.dtype.names[col + 15]
+                        ylabel = yname0.replace("train_", "")
+                        # plot
+                        plt.xlabel(xlabel, fontsize = "smaller")
+                        plt.ylabel(ylabel, fontsize = "smaller")
+                        plt.title(title, weight = "bold")
+                        plt.plot(data[xname], data[yname0], "r--", label = yname0)
+                        plt.plot(data[xname], data[yname1], "g:", label = yname1)
+                        plt.plot(data[xname], data[yname2], "b-", label = yname2)
+                        plt.legend(fontsize = "smaller")
+                        pdf.savefig()
+                        plt.close()
+
+def plot_many_wrt(spaths, names, datas, pdf, xcol, ycol):
+        colnames = datas[0].dtype.names
+        title = colnames[ycol + 1]
+        # x axis - epoch/iteration index
+        xname = colnames[xcol]
+        xlabel = xname
+        # y axis - train/validation/test datasets
+        yname = colnames[ycol + 1]
+        ylabel = yname.replace("train_", "").replace("valid_", "").replace("test_", "")
+        # plot
+        plt.xlabel(xlabel, fontsize = "smaller")
+        plt.ylabel(ylabel, fontsize = "smaller")
+        plt.title(title, weight = "bold")
+        for i, spath in enumerate(spaths):
+                plt.plot(datas[i][xname], datas[i][yname], label = names[i])
+        plt.legend(fontsize = "smaller")
+        pdf.savefig()
+        plt.close()
+
+def plot_many(spaths, ppath):
+        names, datas = get_csvs(spaths)
+        with PdfPages(ppath) as pdf:
+                for col in (0, 1, 4, 7, 8, 11, 14, 15, 18):
+                        # plot wrt epoch/iteration number
+                        plot_many_wrt(spaths, names, datas, pdf, 0, col)
+                        # plot wrt time
+                        plot_many_wrt(spaths, names, datas, pdf, 22, col)
