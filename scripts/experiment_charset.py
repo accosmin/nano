c52085cc920fa8fa582c29d4b68f63c5300f514f
Cosmin Atanasoaei <accosmin@gmail.com>
2017-03-22 15:13:42 +0100

    fix experimentation script on synthetic dataset

--- scripts/experiment_charset.py
+++ scripts/experiment_charset.py
@@ -6,21 +6,19 @@ import experiment
 # - the model should predict the digit of a synthetic image
 cfg = config.config()
 exp = experiment.experiment(
-        cfg.app_train,
-        cfg.app_stats,
-        cfg.task_synth_charset(ctype = "digit", color = "rgb", irows = 16, icols = 16, count = 10000),
+        cfg.task_synth_charset(ctype = "digit", color = "rgb", irows = 24, icols = 24, count = 10000),
         cfg.expdir + "/charset/eval_trainers")
 
 # loss functions
-exp.add_losses(cfg.losses(), [
+exp.add_losses([
         "loss_classnll"])
 
 # criteria
-exp.add_criteria(cfg.criteria(), [
+exp.add_criteria([
         "crit_avg"])
 
 # trainers
-exp.add_trainers(cfg.trainers(), [
+exp.add_trainers([
         "batch_gd", "batch_cgd", "batch_lbfgs",
         "stoch_sg", "stoch_sgm", "stoch_ngd", "stoch_svrg", "stoch_asgd",
         "stoch_ag", "stoch_agfr", "stoch_aggr",
@@ -28,14 +26,18 @@ exp.add_trainers(cfg.trainers(), [
 
 # models
 mlp0 = "--model forward-network --model-params "
-mlp1 = mlp0 + "affine:dims=10;act-snorm;"
-mlp2 = mlp1 + "affine:dims=10;act-snorm;"
-mlp3 = mlp2 + "affine:dims=10;act-snorm;"
+mlp1 = mlp0 + "affine:dims=128;act-snorm;"
+mlp2 = mlp1 + "affine:dims=64;act-snorm;"
+mlp3 = mlp2 + "affine:dims=32;act-snorm;"
+mlp4 = mlp3 + "affine:dims=32;act-snorm;"
+mlp5 = mlp4 + "affine:dims=32;act-snorm;"
 
 convnet0 = "--model forward-network --model-params "
-convnet1 = convnet0 + "conv:dims=32,rows=5,cols=5,conn=1,drow=1,dcol=1;act-snorm;"
+convnet1 = convnet0 + "conv:dims=32,rows=7,cols=7,conn=1,drow=1,dcol=1;act-snorm;"
 convnet2 = convnet1 + "conv:dims=32,rows=5,cols=5,conn=4,drow=1,dcol=1;act-snorm;"
-convnet3 = convnet2 + "conv:dims=32,rows=3,cols=3,conn=4,drow=1,dcol=1;act-snorm;"
+convnet3 = convnet2 + "conv:dims=32,rows=5,cols=5,conn=4,drow=1,dcol=1;act-snorm;"
+convnet4 = convnet3 + "conv:dims=32,rows=3,cols=3,conn=4,drow=1,dcol=1;act-snorm;"
+convnet5 = convnet4 + "conv:dims=32,rows=3,cols=3,conn=4,drow=1,dcol=1;act-snorm;"
 
 outlayer = "affine:dims=10;act-snorm;"
 
@@ -43,31 +45,36 @@ exp.add_model("mlp0", mlp0 + outlayer)
 exp.add_model("mlp1", mlp1 + outlayer)
 exp.add_model("mlp2", mlp2 + outlayer)
 exp.add_model("mlp3", mlp3 + outlayer)
+exp.add_model("mlp4", mlp4 + outlayer)
 exp.add_model("convnet1", convnet1 + outlayer)
 exp.add_model("convnet2", convnet2 + outlayer)
 exp.add_model("convnet3", convnet3 + outlayer)
+exp.add_model("convnet4", convnet4 + outlayer)
+exp.add_model("convnet5", convnet5 + outlayer)
 
 # train all configurations
 trials = 10
-exp.run_all(trials = trials, epochs = 100, policy = "stop_early")
+exp.run_all(trials = trials, epochs = 1000, policy = "stop_early")
 
 # compare configurations
 for trial in range(trials):
         for mname in exp.models:
                 for cname in exp.criteria:
                         for lname in exp.losses:
-                                stoch_spaths = exp.filter(trial, mname, "stoch*", cname, lname, ".state")
-                                batch_spaths = exp.filter(trial, mname, "batch*", cname, lname, ".state")
-                                all_spaths = exp.filter(trial, mname, ".*", cname, lname, ".state")
-
                                 # compare stochastic trainers
-                                exp.plot_many(stoch_spaths, exp.get_path(trial, mname, "stoch", cname, lname, ".pdf"))
+                                exp.plot_many(
+                                        exp.filter(trial, mname, "stoch.*", cname, lname, "*state"),
+                                        exp.get_path(trial, mname, "stoch", cname, lname, ".pdf"))
 
                                 # compare batch trainers
-                                exp.plot_many(batch_spaths, exp.get_path(trial, mname, "batch", cname, lname, ".pdf"))
+                                exp.plot_many(
+                                        exp.filter(trial, mname, "batch.*", cname, lname, "*state"),
+                                        exp.get_path(trial, mname, "batch", cname, lname, ".pdf"))
 
                                 # compare all trainers
-                                exp.plot_many(all_spaths, exp.get_path(trial, mname, "all", cname, lname, ".pdf"))
+                                exp.plot_many(
+                                        exp.filter(trial, mname, ".*", cname, lname, "*state"),
+                                        exp.get_path(trial, mname, "all", cname, lname, ".pdf"))
 
 # summarize configurations
 exp.summarize(trials)
