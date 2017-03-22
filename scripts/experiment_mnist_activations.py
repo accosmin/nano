56c090727568a137fb4c711b74ac8b05531e2dda
Cosmin Atanasoaei <accosmin@gmail.com>
2017-03-22 15:22:46 +0100

    update scripts to experiment with all wave activations

--- scripts/experiment_mnist_activations.py
+++ scripts/experiment_mnist_activations.py
@@ -30,7 +30,7 @@ convnet2 = convnet1 + "conv:dims=128,rows=5,cols=5,conn=4,drow=1,dcol=1;act-snor
 convnet3 = convnet2 + "conv:dims=128,rows=3,cols=3,conn=4,drow=1,dcol=1;act-snorm;"
 convnet4 = convnet3 + "conv:dims=128,rows=3,cols=3,conn=4,drow=1,dcol=1;act-snorm;"
 
-for activation in ["snorm", "tanh", "sin", "ewave:alpha=1", "ewave:alpha=2", "ewave:alpha=3", "ewave:alpha=4"]:
+for activation in ["snorm", "tanh", "sin", "ewave:alpha=1", "ewave:alpha=2", "ewave:alpha=3", "ewave:alpha=4", "pwave"]:
         name = ("convnet4-act-" + activation).replace("-", "_").replace(":alpha=", "")
         params = (convnet4 + outlayer).replace("act-snorm", "act-" + activation)
         exp.add_model(name, params)
