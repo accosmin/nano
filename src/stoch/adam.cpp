93a1f5b5f0fd2f705027d2e0c5d9eb7f8884ffc3
Cosmin Atanasoaei <accosmin@gmail.com>
2017-03-22 17:18:18 +0100

    use a decaying learning rate for Adam (check the latest paper version)

--- src/stoch/adam.cpp
+++ src/stoch/adam.cpp
@@ -1,5 +1,6 @@
 #include "adam.h"
 #include "loop.h"
+#include "lrate.h"
 #include "text/to_params.h"
 #include "tensor/momentum.h"
 
@@ -14,12 +15,15 @@ namespace nano
         {
                 const auto beta1s = make_finite_space(scalar_t(0.900));
                 const auto beta2s = make_finite_space(scalar_t(0.99), scalar_t(0.999), scalar_t(0.9999), scalar_t(0.99999));
-                return stoch_tune(this, param, function, x0, make_alpha0s(), make_epsilons(), beta1s, beta2s);
+                return stoch_tune(this, param, function, x0, make_alpha0s(), make_decays(), make_epsilons(), beta1s, beta2s);
         }
 
         state_t stoch_adam_t::minimize(const stoch_params_t& param, const function_t& function, const vector_t& x0,
-                const scalar_t alpha0, const scalar_t epsilon, const scalar_t beta1, const scalar_t beta2) const
+                const scalar_t alpha0, const scalar_t decay, const scalar_t epsilon, const scalar_t beta1, const scalar_t beta2) const
         {
+                // learning rate schedule
+                lrate_t lrate(alpha0, decay, param.m_epoch_size);
+
                 // first-order momentum of the gradient
                 nano::momentum_t<vector_t> m(beta1, x0.size());
 
@@ -30,7 +34,7 @@ namespace nano
                 const auto optimizer = [&] (state_t& cstate, const state_t&)
                 {
                         // learning rate
-                        const scalar_t alpha = alpha0;
+                        const scalar_t alpha = lrate.get();
 
                         // descent direction
                         m.update(cstate.g);
@@ -49,7 +53,7 @@ namespace nano
                 };
 
                 return  stoch_loop(param, function, x0, optimizer, snapshot,
-                        to_params("alpha0", alpha0, "epsilon", epsilon, "beta1", beta1, "beta2", beta2));
+                        to_params("alpha0", alpha0, "decay", decay, "epsilon", epsilon, "beta1", beta1, "beta2", beta2));
         }
 }
 
