d279c52f617baf6e28bfd6b511d01a18251da28c
Cosmin Atanasoaei <accosmin@gmail.com>
2017-03-23 11:00:50 +0100

    use a decaying learning rate for svrg

--- src/stoch/svrg.cpp
+++ src/stoch/svrg.cpp
@@ -1,5 +1,6 @@
 #include "svrg.h"
 #include "loop.h"
+#include "lrate.h"
 #include "text/to_params.h"
 
 namespace nano
@@ -11,17 +12,20 @@ namespace nano
 
         state_t stoch_svrg_t::minimize(const stoch_params_t& param, const function_t& function, const vector_t& x0) const
         {
-                return stoch_tune(this, param, function, x0, make_alpha0s());
+                return stoch_tune(this, param, function, x0, make_alpha0s(), make_decays());
         }
 
         state_t stoch_svrg_t::minimize(const stoch_params_t& param, const function_t& function, const vector_t& x0,
-                const scalar_t alpha0) const
+                const scalar_t alpha0, const scalar_t decay) const
         {
+                // learning rate schedule
+                lrate_t lrate(alpha0, decay, param.m_epoch_size);
+
                 // assembly the optimizer
                 const auto optimizer = [&] (state_t& cstate, const state_t& sstate)
                 {
                         // learning rate
-                        const scalar_t alpha = alpha0;
+                        const scalar_t alpha = lrate.get();
 
                         // descent direction
                         function.stoch_eval(sstate.x, &cstate.d);// NB: reuse descent direction to store snapshot gradient!
@@ -38,7 +42,7 @@ namespace nano
                 };
 
                 return  stoch_loop(param, function, x0, optimizer, snapshot,
-                        to_params("alpha0", alpha0));
+                        to_params("alpha0", alpha0, "decay", decay));
         }
 }
 
