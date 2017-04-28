4b439edd725f170d4894de4f7c21cbbce6c73040
Cosmin Atanasoaei <accosmin@gmail.com>
2017-04-28 16:56:37 +0200

    inline filesystem-related utilities

--- src/text/filesystem.h
+++ src/text/filesystem.h
@@ -1,28 +1,55 @@
 #pragma once
 
-#include "arch.h"
-#include "stringi.h"
+#include <string>
 
 namespace nano
 {
+        /// todo: remove this file and use <filesystem> with C++17.
+
         ///
         /// \brief extracts file name from path (e.g. /usr/include/file.ext -> file.ext).
         ///
-        NANO_PUBLIC string_t filename(const string_t& path);
+        inline std::string filename(const std::string& path)
+        {
+                const auto pos = path.find_last_of("/\\");
+                return (pos == std::string::npos) ? path : path.substr(pos + 1);
+        }
 
         ///
         /// \brief extracts file extension from path (e.g. /usr/include/file.ext -> ext).
         ///
-        NANO_PUBLIC string_t extension(const string_t& path);
-
-        ///
-        /// \brief extracts file stem from path (e.g. /usr/include/file.ext -> file).
-        ///
-        NANO_PUBLIC string_t stem(const string_t& path);
+        inline std::string extension(const std::string& path)
+        {
+                const auto pos = path.find_last_of('.');
+                return (pos == std::string::npos) ? std::string() : path.substr(pos + 1);
+        }
 
         ///
         /// \brief extracts directory name from path (e.g. /usr/include/file.ext -> /usr/include/).
         ///
-        NANO_PUBLIC string_t dirname(const string_t& path);
-}
+        inline std::string dirname(const std::string& path)
+        {
+                const auto pos = path.find_last_of("/\\");
+                return (pos == std::string::npos) ? "./" : path.substr(0, pos + 1);
+        }
 
+        ///
+        /// \brief extracts file stem from path (e.g. /usr/include/file.ext -> file).
+        ///
+        inline std::string stem(const std::string& path)
+        {
+                const auto pos_dir = path.find_last_of("/\\");
+                const auto pos_ext = path.find_last_of('.');
+
+                if (pos_dir == std::string::npos)
+                {
+                        return  (pos_ext == std::string::npos) ?
+                                path : path.substr(0, pos_ext);
+                }
+                else
+                {
+                        return  (pos_ext == std::string::npos) ?
+                                path.substr(pos_dir + 1) : path.substr(pos_dir + 1, pos_ext - pos_dir - 1);
+                }
+        }
+}
