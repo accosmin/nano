bbc6c977b793f68f8cd400166fad7ed5034f3b1d
Cosmin Atanasoaei <accosmin@gmail.com>
2017-05-12 16:41:29 +0200

    inline classification targets

--- src/cortex/class.h
+++ src/cortex/class.h
@@ -8,26 +8,41 @@ namespace nano
         ///
         /// \brief target value of the positive class
         ///
-        NANO_PUBLIC scalar_t pos_target();
+        inline scalar_t pos_target() { return +1; }
 
         ///
         /// \brief target value of the negative class
         ///
-        NANO_PUBLIC scalar_t neg_target();
+        inline scalar_t neg_target() { return -1; }
 
         ///
         /// \brief check if a target value maps to a positive class
         ///
-        NANO_PUBLIC bool is_pos_target(const scalar_t target);
+        inline bool is_pos_target(const scalar_t target) { return target > 0; }
 
         ///
         /// \brief target value for multi-class single-label classification problems with [n_labels] classes
         ///
-        NANO_PUBLIC vector_t class_target(const tensor_size_t ilabel, const tensor_size_t n_labels);
+        inline vector_t class_target(const tensor_size_t ilabel, const tensor_size_t n_labels)
+        {
+                vector_t target = vector_t::Constant(n_labels, neg_target());
+                if (ilabel < n_labels)
+                {
+                        target(ilabel) = pos_target();
+                }
+                return target;
+        }
 
         ///
         /// \brief target value for multi-class multi-label classification problems based on the sign of the target
         ///
-        NANO_PUBLIC vector_t class_target(const vector_t& scores);
+        inline vector_t class_target(const vector_t& scores)
+        {
+                vector_t target(scores.size());
+                for (auto i = 0; i < scores.size(); ++ i)
+                {
+                        target(i) = is_pos_target(scores(i)) ? pos_target() : neg_target();
+                }
+                return target;
+        }
 }
-
