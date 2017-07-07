c9fbbfdbdcaf804db73e3e6462f2245ef6f995f4
Cosmin Atanasoaei <accosmin@gmail.com>
2017-07-07 15:34:30 +0200

    update experiment with proper convolution network

--- scripts/experiment_mnist_minibatch.py
+++ scripts/experiment_mnist_minibatch.py
@@ -18,14 +18,14 @@ exp.add_loss("slogistic")
 exp.add_iterator("default")
 
 # trainers
-stoch_params = "epochs=10,patience=32,epsilon=1e-6,batch={}"
+stoch_params = "epochs=100,patience=32,epsilon=1e-6,batch={}"
 minibatch_name = "batch{}"
 
 for size in [32, 64, 128, 256, 512, 1024]:
         exp.add_trainer("stoch_adadelta", stoch_params.format(size), minibatch_name.format(size))
 
 # models
-exp.add_model("mlp0", models.mlp0 + models.outlayer)
+exp.add_model("convnet6", models.convnet6 + models.outlayer)
 
 # train all configurations
 exp.train_all()
