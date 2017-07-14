7bd33efe09f1ec7d3a129694a493ec3276031d02
Cosmin Atanasoaei <accosmin@gmail.com>
2017-07-14 09:43:07 +0200

    consistent variable names

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -34,12 +34,12 @@ class experiment:
         def add_enhancer(self, name, parameters = "", config_name = None):
                 self.enhancers.append([config_name if config_name else name, self.cfg.config_enhancer(name, parameters)])
 
-        def path(self, trial, mname, tname, iname, lname, extension):
+        def path(self, trial, mname, tname, ename, lname, extension):
                 basepath = self.dir
                 basepath += "trial{}".format(trial) if not (trial is None) else "result"
                 basepath += "_M" + mname if mname else ""
                 basepath += "_T" + tname if tname else ""
-                basepath += "_I" + iname if iname else ""
+                basepath += "_E" + ename if ename else ""
                 basepath += "_L" + lname if lname else ""
                 basepath += extension
                 return basepath
@@ -58,18 +58,18 @@ class experiment:
                                 names.append(name)
                 return names
 
-        def filter_names(self, mname_reg, tname_reg, iname_reg, lname_reg):
+        def filter_names(self, mname_reg, tname_reg, ename_reg, lname_reg):
                 mnames = self.names(self.models, mname_reg)
                 tnames = self.names(self.trainers, tname_reg)
-                inames = self.names(self.enhancers, iname_reg)
+                enames = self.names(self.enhancers, ename_reg)
                 lnames = self.names(self.losses, lname_reg)
-                return mnames, tnames, inames, lnames
+                return mnames, tnames, enames, lnames
 
-        def filter_paths(self, trial, mname_reg, tname_reg, iname_reg, lname_reg, extension):
+        def filter_paths(self, trial, mname_reg, tname_reg, ename_reg, lname_reg, extension):
                 paths = []
-                mnames, tnames, inames, lnames = self.filter_names(mname_reg, tname_reg, iname_reg, lname_reg)
-                for mname, tname, iname, lname in [(u, x, y, z) for u in mnames for x in tnames for y in inames for z in lnames]:
-                        paths.append(self.path(trial, mname, tname, iname, lname, extension))
+                mnames, tnames, enames, lnames = self.filter_names(mname_reg, tname_reg, ename_reg, lname_reg)
+                for mname, tname, ename, lname in [(u, x, y, z) for u in mnames for x in tnames for y in enames for z in lnames]:
+                        paths.append(self.path(trial, mname, tname, ename, lname, extension))
                 return paths
 
         def reg2str(self, reg):
@@ -104,99 +104,99 @@ class experiment:
         def summarize_by_models(self, mname_reg = ".*"):
                 mname = self.reg2str(mname_reg)
 
-        def train_trials(self, mname, mparam, tname, tparam, iname, iparam, lname, lparam):
+        def train_trials(self, mname, mparam, tname, tparam, ename, eparam, lname, lparam):
                 # train the given configuration for multiple trials
                 for trial in range(self.trials):
                         os.makedirs(self.dir, exist_ok = True)
-                        mpath = self.path(trial, mname, tname, iname, lname, ".model")
-                        spath = self.path(trial, mname, tname, iname, lname, ".state")
-                        lpath = self.path(trial, mname, tname, iname, lname, ".log")
-                        ppath = self.path(trial, mname, tname, iname, lname, ".pdf")
+                        mpath = self.path(trial, mname, tname, ename, lname, ".model")
+                        spath = self.path(trial, mname, tname, ename, lname, ".state")
+                        lpath = self.path(trial, mname, tname, ename, lname, ".log")
+                        ppath = self.path(trial, mname, tname, ename, lname, ".pdf")
 
-                        param = self.task + " " + mparam + " " + tparam + " " + iparam + " " + lparam + " --model-file " + mpath
+                        param = self.task + " " + mparam + " " + tparam + " " + eparam + " " + lparam + " --model-file " + mpath
                         self.train_one(param, lpath)
                         self.plot_one(spath, ppath)
                         self.log()
 
                 # export the results from multiple trials as csv
-                cpath = self.path(None, mname, tname, iname, lname, ".csv")
-                spath = self.path(None, mname, tname, iname, lname, ".stats")
+                cpath = self.path(None, mname, tname, ename, lname, ".csv")
+                spath = self.path(None, mname, tname, ename, lname, ".stats")
                 with open(cpath, "w") as cfile, open(spath, "w") as sfile:
                         print(self.get_csv_header(), file = cfile)
                         print(self.get_csv_header(), file = sfile)
-                        values, errors, epochs, speeds, deltas = self.get_logs( mname, tname, iname, lname)
+                        values, errors, epochs, speeds, deltas = self.get_logs( mname, tname, ename, lname)
                         for value, error, epoch, speed, delta in zip(values, errors, epochs, speeds, deltas):
-                                print(self.get_csv_row(mname, tname, iname, lname, value, error, epoch, speed, delta), file = cfile)
+                                print(self.get_csv_row(mname, tname, ename, lname, value, error, epoch, speed, delta), file = cfile)
                         value_stats, error_stats, epoch_stats, speed_stats, delta_stats = self.get_log_stats(values, errors, epochs, speeds, deltas)
-                        print(self.get_csv_row(mname, tname, iname, lname, value_stats, error_stats, epoch_stats, speed_stats, delta_stats), file = sfile)
+                        print(self.get_csv_row(mname, tname, ename, lname, value_stats, error_stats, epoch_stats, speed_stats, delta_stats), file = sfile)
 
                 # display basic statistics for the results from multiple trials
-                lpath = self.path(None, mname, tname, iname, lname, ".log")
+                lpath = self.path(None, mname, tname, ename, lname, ".log")
                 self.tabulate(spath, lpath)
 
         def train_all(self):
                 # run all possible configurations
                 mdatas = self.models
                 tdatas = self.trainers
-                idatas = self.enhancers
+                edatas = self.enhancers
                 ldatas = self.losses
-                for mdata, tdata, idata, ldata in [(u, x, y, z) for u in mdatas for x in tdatas for y in idatas for z in ldatas]:
+                for mdata, tdata, edata, ldata in [(u, x, y, z) for u in mdatas for x in tdatas for y in edatas for z in ldatas]:
                         self.train_trials(
                                 self.name(mdata), self.config(mdata),
                                 self.name(tdata), self.config(tdata),
-                                self.name(idata), self.config(idata),
+                                self.name(edata), self.config(edata),
                                 self.name(ldata), self.config(ldata))
 
-        def summarize_trials(self, mname, mname_reg, tname, tname_reg, iname, iname_reg, lname, lname_reg, names):
+        def summarize_trials(self, mname, mname_reg, tname, tname_reg, ename, ename_reg, lname, lname_reg, names):
                 # compare configurations for each trial: plot the training state evaluation
                 for trial in range(self.trials):
                         self.plot_many(
-                                self.filter_paths(trial, mname_reg, tname_reg, iname_reg, lname_reg, ".state"),
-                                self.path(trial, mname, tname, iname, lname, ".pdf"))
+                                self.filter_paths(trial, mname_reg, tname_reg, ename_reg, lname_reg, ".state"),
+                                self.path(trial, mname, tname, ename, lname, ".pdf"))
 
                 # compare configurations across all trials: boxplot the results
-                ppath = self.path(None, mname, tname, iname, lname, ".pdf")
+                ppath = self.path(None, mname, tname, ename, lname, ".pdf")
 
                 self.plot_trial(
-                        self.filter_paths(None, mname_reg, tname_reg, iname_reg, lname_reg, ".csv"),
+                        self.filter_paths(None, mname_reg, tname_reg, ename_reg, lname_reg, ".csv"),
                         ppath, names)
 
                 # compare configurations across all trials: display basic statistics
-                spath = self.path(None, mname, tname, iname, lname, ".stats")
+                spath = self.path(None, mname, tname, ename, lname, ".stats")
                 with open(spath, "w") as sfile:
                         print(self.get_csv_header(), file = sfile)
-                        mnames, tnames, inames, lnames = self.filter_names(mname_reg, tname_reg, iname_reg, lname_reg)
-                        for mname, tname, iname, lname in [(u, x, y, z) for u in mnames for x in tnames for y in inames for z in lnames]:
-                                values, errors, epochs, speeds, deltas = self.get_logs(mname, tname, iname, lname)
+                        mnames, tnames, enames, lnames = self.filter_names(mname_reg, tname_reg, ename_reg, lname_reg)
+                        for mname, tname, ename, lname in [(u, x, y, z) for u in mnames for x in tnames for y in enames for z in lnames]:
+                                values, errors, epochs, speeds, deltas = self.get_logs(mname, tname, ename, lname)
                                 value_stats, error_stats, epoch_stats, speed_stats, delta_stats = self.get_log_stats(values, errors, epochs, speeds, deltas)
-                                print(self.get_csv_row(mname, tname, iname, lname, value_stats, error_stats, epoch_stats, speed_stats, delta_stats), file = sfile)
+                                print(self.get_csv_row(mname, tname, ename, lname, value_stats, error_stats, epoch_stats, speed_stats, delta_stats), file = sfile)
 
-                lpath = self.path(None, mname, tname, iname, lname, ".log")
+                lpath = self.path(None, mname, tname, ename, lname, ".log")
                 self.tabulate(spath, lpath)
 
         def summarize_by_models(self, mname_reg = ".*"):
                 mname = self.reg2str(mname_reg)
-                mnames, tnames, inames, lnames = self.filter_names(mname_reg, None, None, None)
-                for tname, iname, lname in [(x, y, z) for x in tnames for y in inames for z in lnames]:
-                        self.summarize_trials(mname, mname_reg, tname, tname, iname, iname, lname, lname, mnames)
+                mnames, tnames, enames, lnames = self.filter_names(mname_reg, None, None, None)
+                for tname, ename, lname in [(x, y, z) for x in tnames for y in enames for z in lnames]:
+                        self.summarize_trials(mname, mname_reg, tname, tname, ename, ename, lname, lname, mnames)
 
         def summarize_by_trainers(self, tname_reg = ".*"):
                 tname = self.reg2str(tname_reg)
-                mnames, tnames, inames, lnames = self.filter_names(None, tname_reg, None, None)
-                for mname, iname, lname in [(x, y, z) for x in mnames for y in inames for z in lnames]:
-                        self.summarize_trials(mname, mname, tname, tname_reg, iname, iname, lname, lname, tnames)
+                mnames, tnames, enames, lnames = self.filter_names(None, tname_reg, None, None)
+                for mname, ename, lname in [(x, y, z) for x in mnames for y in enames for z in lnames]:
+                        self.summarize_trials(mname, mname, tname, tname_reg, ename, ename, lname, lname, tnames)
 
-        def summarize_by_enhancers(self, iname_reg = ".*"):
-                iname = self.reg2str(iname_reg)
-                mnames, tnames, inames, lnames = self.filter_names(None, None, iname_reg, None)
+        def summarize_by_enhancers(self, ename_reg = ".*"):
+                ename = self.reg2str(ename_reg)
+                mnames, tnames, enames, lnames = self.filter_names(None, None, ename_reg, None)
                 for mname, tname, lname in [(x, y, z) for x in mnames for y in tnames for z in lnames]:
-                        self.summarize_trials(mname, mname, tname, tname, iname, iname_reg, lname, lname, inames)
+                        self.summarize_trials(mname, mname, tname, tname, ename, ename_reg, lname, lname, enames)
 
         def summarize_by_losses(self, lname_reg = ".*"):
                 lname = self.reg2str(lname_reg)
-                mnames, tnames, inames, lnames = self.filter_names(None, None, None, lname_reg)
-                for mname, tname, iname in [(x, y, z) for x in mnames for y in tnames for z in inames]:
-                        self.summarize_trials(mname, mname, tname, tname, iname, iname, lname, lname_reg, lnames)
+                mnames, tnames, enames, lnames = self.filter_names(None, None, None, lname_reg)
+                for mname, tname, ename in [(x, y, z) for x in mnames for y in tnames for z in enames]:
+                        self.summarize_trials(mname, mname, tname, tname, ename, ename, lname, lname_reg, lnames)
 
         def get_token(self, line, begin_delim, end_delim, start = 0):
                 begin = line.find(begin_delim, start) + len(begin_delim)
@@ -234,14 +234,14 @@ class experiment:
                                 return value, error, epoch, speed, self.get_seconds(delta)
                 print("invalid log file <", lpath, ">")
 
-        def get_logs(self, mname, tname, iname, lname):
+        def get_logs(self, mname, tname, ename, lname):
                 values = []
                 errors = []
                 epochs = []
                 speeds = []
                 deltas = []
                 for trial in range(self.trials):
-                        lpath = self.path(trial, mname, tname, iname, lname, ".log")
+                        lpath = self.path(trial, mname, tname, ename, lname, ".log")
                         value, error, epoch, speed, delta = self.get_log(lpath)
                         values.append(value)
                         errors.append(error)
@@ -262,5 +262,5 @@ class experiment:
         def get_csv_header(self, delim = ";"):
                 return delim.join(["model", "trainer", "enhancer", "loss", "test value", "test error", "epochs", "convergence speed", "duration (sec)"])
 
-        def get_csv_row(self, mname, tname, iname, lname, value, error, epoch, speed, delta, delim = ";"):
-                return delim.join([mname, tname, iname, lname, value, error, epoch, speed, delta])
+        def get_csv_row(self, mname, tname, ename, lname, value, error, epoch, speed, delta, delim = ";"):
+                return delim.join([mname, tname, ename, lname, value, error, epoch, speed, delta])
