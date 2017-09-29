e1d6ab06373bda0bd6295e3ef3a14f138d1fb65a
Cosmin Atanasoaei <accosmin@gmail.com>
2017-09-29 09:44:08 +0200

    cleanup & extend unit test

--- test/test_functions.cpp
+++ test/test_functions.cpp
@@ -6,41 +6,39 @@
 
 using namespace nano;
 
-static void test_function(const function_t& function)
-{
-        std::cout << function.name() << std::endl;
-
-        const auto trials = size_t(1000);
-
-        const auto dims = function.size();
-        NANO_CHECK_GREATER(dims, 0);
-        NANO_CHECK_GREATER_EQUAL(dims, function.min_size());
-        NANO_CHECK_GREATER_EQUAL(function.max_size(), dims);
-
-        auto rgen = make_rng(scalar_t(-10.0), scalar_t(+10.0));
-
-        for (size_t t = 0; t < trials; ++ t)
-        {
-                vector_t x0(dims), x1(dims);
-                nano::set_random(rgen, x0, x1);
-
-                if (    function.is_valid(x0) && std::isfinite(function.eval(x0)) &&
-                        function.is_valid(x1) && std::isfinite(function.eval(x1)))
-                {
-                        NANO_CHECK_LESS(function.grad_accuracy(x0), epsilon3<scalar_t>());
-                        if (function.is_convex())
-                        {
-                                NANO_CHECK(function.is_convex(x0, x1, 20));
-                        }
-                }
-        }
-}
-
 NANO_BEGIN_MODULE(test_functions)
 
 NANO_CASE(evaluate)
 {
-        foreach_test_function(make_functions(1, 4), test_function);
+        foreach_test_function(make_functions(1, 4), [] (const function_t& function)
+        {
+                std::cout << function.name() << std::endl;
+
+                const auto dims = function.size();
+                NANO_CHECK_LESS_EQUAL(dims, 4);
+                NANO_CHECK_GREATER_EQUAL(dims, 1);
+                NANO_CHECK_GREATER_EQUAL(dims, function.min_size());
+                NANO_CHECK_GREATER_EQUAL(function.max_size(), dims);
+
+                auto rgen = make_rng(scalar_t(-10.0), scalar_t(+10.0));
+
+                const auto trials = size_t(1000);
+                for (size_t t = 0; t < trials; ++ t)
+                {
+                        vector_t x0(dims), x1(dims);
+                        nano::set_random(rgen, x0, x1);
+
+                        if (    function.is_valid(x0) && std::isfinite(function.eval(x0)) &&
+                                function.is_valid(x1) && std::isfinite(function.eval(x1)))
+                        {
+                                NANO_CHECK_LESS(function.grad_accuracy(x0), epsilon3<scalar_t>());
+                                if (function.is_convex())
+                                {
+                                        NANO_CHECK(function.is_convex(x0, x1, 20));
+                                }
+                        }
+                }
+        });
 }
 
 NANO_END_MODULE()
