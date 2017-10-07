45bce7de1acd46a8b9ca0788906690c572e554dd
accosmin <accosmin@gmail.com>
2017-10-07 21:52:03 +0200

    add minibatch_t to support processing of 4D tensors

--- src/cortex.h
+++ src/cortex.h
@@ -46,7 +46,7 @@ namespace nano
         }
 
         ///
-        /// \brief dataset sample (minibatch)
+        /// \brief dataset sample.
         ///
         struct sample_t
         {
@@ -55,6 +55,33 @@ namespace nano
                 string_t        m_label;        ///< classification label (optional)
         };
 
+        ///
+        /// \brief dataset samples composing a minibatch.
+        ///
+        struct minibatch_t
+        {
+                minibatch_t() = default;
+
+                minibatch_t(const tensor_size_t count, const tensor3d_dims_t& idims, const tensor3d_dims_t& odims) :
+                        m_inputs(count, std::get<0>(idims), std::get<1>(idims), std::get<2>(idims)),
+                        m_targets(count, std::get<0>(odims), std::get<1>(odims), std::get<2>(odims)),
+                        m_labels(static_cast<size_t>(count))
+                {
+                }
+
+                operator bool() const
+                {
+                        return  m_inputs.size<0>() == m_targets.size<0>() &&
+                                m_inputs.size<0>() == static_cast<tensor_size_t>(m_labels.size());
+                }
+
+                tensor4d_t      m_inputs;       ///< inputs: count x planes x rows x columns
+                tensor4d_t      m_targets;      ///< desired (ideal) outputs: count x planes x rows x columns
+                strings_t       m_labels;       ///< classification labels (optional)
+        };
+
+        ///
+        /// \brief target value of the positive class
         ///
         /// \brief target value of the positive class
         ///
