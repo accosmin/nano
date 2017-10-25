3b30092584e29e78cd778bcc93d1da44add5b180
Cosmin Atanasoaei <accosmin@gmail.com>
2017-10-25 16:10:14 +0200

    update mnist models to make use of the new python api

--- scripts/models.py
+++ scripts/models.py
@@ -2,16 +2,24 @@ def make_layer(name, params, activation):
         return "{}:{};{}{}".format(name, params, activation, ";" if activation else "")
 
 def make_affine_layer(omaps, orows = 1, ocols = 1, activation = "act-snorm"):
-        return make_layer("affine", "omaps={},orows={},ocols={}".format(omaps, orows, ocols), activation)
+        name = "affine"
+        params = "omaps={},orows={},ocols={}".format(omaps, orows, ocols)
+        return make_layer(name, params, activation)
 
 def make_output_layer(omaps, orows = 1, ocols = 1):
         return make_affine_layer(omaps, orows, ocols, "")
 
 def make_norm_by_plane_layer():
-        return make_layer("norm", "type={}".format("plane"))
+        name = "norm"
+        params = "type={}".format("plane")
+        return make_layer(name, params)
 
 def make_norm_globally_layer():
-        return make_layer("norm", "type={}".format("global"))
+        name = "norm"
+        params = "type={}".format("global")
+        return make_layer(name, params)
 
 def make_conv3d_layer(omaps, krows, kcols, kconn = 1, kdrow = 1, kdcol = 1, activation = "act-snorm"):
-        return make_layer("conv3d", "omaps={},krows={},kcols={},kconn={},kdrow={},kdcol={}".format(omaps, krows, kcols, kconn, kdrow, kdcol), activation)
+        name = "conv3d"
+        params = "omaps={},krows={},kcols={},kconn={},kdrow={},kdcol={}".format(omaps, krows, kcols, kconn, kdrow, kdcol)
+        return make_layer(name, params, activation)
--- scripts/models_mnist.py
+++ scripts/models_mnist.py
@@ -1,16 +1,25 @@
-outlayer = "affine:dims=10;"
+import models
+
+outlayer = make_output_layer(omaps = 10)
+
+normlayer = make_norm_by_plane_layer()
 
 mlp0 = "--model forward-network --model-params "
-mlp1 = mlp0 + "affine:dims=128;act-snorm;"
-mlp2 = mlp1 + "affine:dims=128;act-snorm;"
-mlp3 = mlp2 + "affine:dims=128;act-snorm;"
-mlp4 = mlp3 + "affine:dims=128;act-snorm;"
-mlp5 = mlp4 + "affine:dims=128;act-snorm;"
+mlp1 = mlp0 + make_affine_layer(omaps = 128)
+mlp2 = mlp1 + make_affine_layer(omaps = 128)
+mlp3 = mlp2 + make_affine_layer(omaps = 128)
+mlp4 = mlp3 + make_affine_layer(omaps = 128)
+mlp5 = mlp4 + make_affine_layer(omaps = 128)
+mlp6 = mlp5 + make_affine_layer(omaps = 128)
+mlp7 = mlp6 + make_affine_layer(omaps = 128)
+mlp8 = mlp7 + make_affine_layer(omaps = 128)
 
 convnet0 = "--model forward-network --model-params "
-convnet1 = convnet0 + "conv:dims=32,rows=9,cols=9,conn=1,drow=1,dcol=1;act-snorm;"
-convnet2 = convnet1 + "conv:dims=48,rows=7,cols=7,conn=1,drow=1,dcol=1;act-snorm;"
-convnet3 = convnet2 + "conv:dims=64,rows=5,cols=5,conn=1,drow=1,dcol=1;act-snorm;"
-convnet4 = convnet3 + "conv:dims=64,rows=5,cols=5,conn=1,drow=1,dcol=1;act-snorm;"
-convnet5 = convnet4 + "conv:dims=96,rows=3,cols=3,conn=1,drow=1,dcol=1;act-snorm;"
-convnet6 = convnet5 + "conv:dims=96,rows=3,cols=3,conn=1,drow=1,dcol=1;act-snorm;"
+convnet1 = convnet0 + make_conv3d_layer(omaps = 32, krows = 9, kcols = 9)
+convnet2 = convnet1 + make_conv3d_layer(omaps = 48, krows = 7, kcols = 7)
+convnet3 = convnet2 + make_conv3d_layer(omaps = 64, krows = 5, kcols = 5)
+convnet4 = convnet3 + make_conv3d_layer(omaps = 64, krows = 5, kcols = 5)
+convnet5 = convnet4 + make_conv3d_layer(omaps = 96, krows = 3, kcols = 3)
+convnet6 = convnet5 + make_conv3d_layer(omaps = 96, krows = 3, kcols = 3)
+convnet7 = convnet6 + make_conv3d_layer(omaps = 96, krows = 3, kcols = 3)
+convnet8 = convnet7 + make_conv3d_layer(omaps = 128, krows = 1, kcols = 1)
