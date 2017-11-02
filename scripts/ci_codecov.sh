ec95be6e22cea37863d8620869b2cecc4db010c7
accosmin <accosmin@gmail.com>
2017-11-02 22:26:58 +0100

    fix codecov script

--- scripts/ci_codecov.sh
+++ scripts/ci_codecov.sh
@@ -5,4 +5,4 @@ cpus=$(./apps/info --sys-logical-cpus)
 cpus=${cpus/*./}
 
 ctest --output-on-failure -j $cpus -E "test_task_*"
-cd .. && bash <(curl -s https://codecov.io/bash)
+bash <(curl -s https://codecov.io/bash)
