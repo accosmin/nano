8176e4bb519630e6077e9ed2912c7f7992bb5627
accosmin <accosmin@gmail.com>
2017-11-03 22:04:29 +0100

    reuse ci test script

--- scripts/ci_codecov.sh
+++ scripts/ci_codecov.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cpus=$(./apps/info --sys-logical-cpus)
-cpus=${cpus/*./}
-
-ctest --output-on-failure -j $cpus -E "test_task_*"
+bash ci_test.sh
 cd .. && bash <(curl -s https://codecov.io/bash)
 rm -f *#*
