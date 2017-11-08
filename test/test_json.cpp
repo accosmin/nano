1a84873c2ea984d752fb265317131a26867ec1e4
accosmin <accosmin@gmail.com>
2017-11-08 22:10:00 +0100

    add json example to parse (future computation graph)

--- test/test_json.cpp
+++ test/test_json.cpp
@@ -41,4 +41,37 @@ NANO_CASE(encode_array)
         NANO_CHECK_EQUAL(json, "{param1:v,array:[object1:{name11:11,name12:12},object2:{name21:21,name22:22}],param3:42}");
 }
 
+NANO_CASE(decode)
+{
+        const nano::string_t json = R"XXX(
+{
+        nodes:
+        {
+                norm0:          {id:norm, type:plane},
+                conv0:          {id:conv3d, omaps:128, krows:7, kcols:7, kconn:1, kdrow:2, kdcol:1},
+                act0:           {id:act-snorm},
+                conv1:          {id:conv3d, omaps:256, krows:5, kcols:5, kconn:2, kdrow:1, kdcol:1},
+                act1:           {id:act-pwave},
+                conv2:          {id:conv3d, omaps:512, krows:3, kcols:3, kconn:4, kdrow:1, kdcol:1},
+                act2:           {id:act-sin},
+                affine1:        {id:affine, omaps:1024, orows:1, ocols:1},
+                act3:           {id:act-tanh},
+                output:         {id:affine, omaps:10, orows:1, ocols:1}
+        },
+        digraph:
+        {
+                norm0:          conv0,
+                conv0:          act0,
+                act0:           conv1,
+                conv1:          act1,
+                act1:           conv2,
+                conv2:          act2,
+                act2:           affine1,
+                affine1:        act3,
+                act3:           output
+        }
+}
+)XXX";
+}
+
 NANO_END_MODULE()
