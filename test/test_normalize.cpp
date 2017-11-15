2997652fd42297dc015ebd7bf658dc061441fa2e
Cosmin Atanasoaei <accosmin@gmail.com>
2017-11-15 16:38:27 +0100

    evaluate the gradient accuracy of the normalization kernel outside the computation node

--- test/test_normalize.cpp
+++ test/test_normalize.cpp
@@ -1,4 +1,5 @@
 #include "utest.h"
+#include "function.h"
 #include "math/stats.h"
 #include "math/epsilon.h"
 #include "layers/norm4d.h"
@@ -13,20 +14,59 @@ static auto get_stats(const ttensor& xdata)
         return stats;
 }
 
+auto make_buffers(const norm_params_t& params, const tensor_size_t count)
+{
+        auto idata = params.make_xdata(count); idata.setRandom();
+        auto odata = params.make_xdata(count); odata.setRandom();
+        return std::make_tuple(idata, odata);
+}
+
+template <typename top>
+struct wrt_inputs_function_t final : public function_t
+{
+        explicit wrt_inputs_function_t(const top& op) :
+                function_t("norm4d", op.params().xsize(), op.params().xsize(), op.params().xsize(), convexity::no, 1e+6),
+                m_op(op)
+        {
+                std::tie(m_idata, m_odata) = make_buffers(op.params(), 1);
+        }
+
+        scalar_t vgrad(const vector_t& x, vector_t* gx) const override
+        {
+                m_idata = map_tensor(x.data(), m_idata.dims());
+                m_op.output(m_idata, m_odata);
+                if (gx)
+                {
+                        gx->resize(x.size());
+                        auto idata = map_tensor(gx->data(), m_idata.dims());
+                        m_op.ginput(idata, m_odata);
+                }
+                return m_odata.array().square().sum() / 2;
+        }
+
+        mutable top             m_op;
+        mutable tensor4d_t      m_idata;
+        mutable tensor4d_t      m_odata;
+};
+
+auto make_wrt_inputs_function(const norm_params_t& params)
+{
+        return wrt_inputs_function_t<norm4d_t>(norm4d_t{params});
+}
+
 NANO_BEGIN_MODULE(test_normalize)
 
 NANO_CASE(globally)
 {
         const auto count = 9, xmaps = 3, xrows = 7, xcols = 5;
+        const auto params = norm_params_t{xmaps, xrows, xcols, norm_type::global};
+
+        NANO_CHECK(params.valid());
+        NANO_CHECK_EQUAL(params.xdims(), make_dims(xmaps, xrows, xcols));
+        NANO_CHECK_EQUAL(params.psize(), 0);
 
         tensor4d_t idata(count, xmaps, xrows, xcols);
         idata.random(-1, +1);
-
-        auto params = norm_params_t{xmaps, xrows, xcols, norm_type::global};
-        NANO_CHECK(params.valid());
-
-        NANO_CHECK_EQUAL(make_dims(xmaps, xrows, xcols), params.xdims());
-        NANO_CHECK_EQUAL(0, params.psize());
 
         auto odata = idata;
         norm4d_t norm(params);
@@ -42,18 +82,29 @@ NANO_CASE(globally)
         }
 }
 
+NANO_CASE(globally_ginput_accuracy)
+{
+        const auto xmaps = 3, xrows = 7, xcols = 5;
+        const auto params = norm_params_t{xmaps, xrows, xcols, norm_type::global};
+        NANO_REQUIRE(params.valid());
+
+        const auto ifunct = make_wrt_inputs_function(params);
+
+        vector_t ix(ifunct.size()); ix.setRandom();
+        NANO_CHECK_LESS(ifunct.grad_accuracy(ix), epsilon1<scalar_t>());
+}
+
 NANO_CASE(by_plane)
 {
         const auto count = 9, xmaps = 3, xrows = 7, xcols = 5;
+        const auto params = norm_params_t{xmaps, xrows, xcols, norm_type::plane};
+
+        NANO_CHECK(params.valid());
+        NANO_CHECK_EQUAL(params.xdims(), make_dims(xmaps, xrows, xcols));
+        NANO_CHECK_EQUAL(params.psize(), 0);
 
         tensor4d_t idata(count, xmaps, xrows, xcols);
         idata.random(-1, +1);
-
-        auto params = norm_params_t{xmaps, xrows, xcols, norm_type::plane};
-        NANO_CHECK(params.valid());
-
-        NANO_CHECK_EQUAL(make_dims(xmaps, xrows, xcols), params.xdims());
-        NANO_CHECK_EQUAL(0, params.psize());
 
         auto odata = idata;
         norm4d_t norm(params);
@@ -70,6 +121,18 @@ NANO_CASE(by_plane)
                         NANO_CHECK_LESS(std::fabs(stats.var() - scalar_t(1)), epsilon0<scalar_t>());
                 }
         }
+}
+
+NANO_CASE(by_plane_ginput_accuracy)
+{
+        const auto xmaps = 3, xrows = 7, xcols = 5;
+        const auto params = norm_params_t{xmaps, xrows, xcols, norm_type::plane};
+        NANO_REQUIRE(params.valid());
+
+        const auto ifunct = make_wrt_inputs_function(params);
+
+        vector_t ix(ifunct.size()); ix.setRandom();
+        NANO_CHECK_LESS(ifunct.grad_accuracy(ix), epsilon1<scalar_t>());
 }
 
  NANO_END_MODULE()
