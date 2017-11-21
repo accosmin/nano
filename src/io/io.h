5d3947659a0a12b52a77bb647d0b28b3c62b00a9
Cosmin Atanasoaei <accosmin@gmail.com>
2017-11-21 09:43:36 +0100

    add functions to save/load text to file (e.g. json configs)

--- src/io/io.h
+++ src/io/io.h
@@ -8,6 +8,7 @@
 namespace nano
 {
         using buffer_t = std::vector<char>;
+        using string_t = std::string;
 
         enum class io_status
         {
@@ -17,7 +18,7 @@ namespace nano
         };
 
         ///
-        /// \brief allocates a buffer of the given size
+        /// \brief allocates a buffer of the given size.
         ///
         template <typename tsize>
         buffer_t make_buffer(const tsize size)
@@ -26,17 +27,19 @@ namespace nano
         }
 
         ///
-        /// \brief maximum file/stream size in bytes (useful for indicating a read-until-EOF condition)
+        /// \brief maximum file/stream size in bytes (useful for indicating a read-until-EOF condition).
         ///
         NANO_PUBLIC std::streamsize max_streamsize();
 
         ///
-        /// \brief save buffer to file
+        /// \brief save binary buffer or string to file.
         ///
-        NANO_PUBLIC bool save_buffer(const std::string& path, const buffer_t& buffer);
+        NANO_PUBLIC bool save_buffer(const string_t& path, const buffer_t&);
+        NANO_PUBLIC bool save_string(const string_t& path, const string_t&);
 
         ///
-        /// \brief load buffer from file
+        /// \brief load binary buffer or string from file.
         ///
-        NANO_PUBLIC bool load_buffer(const std::string& path, buffer_t& buffer);
+        NANO_PUBLIC bool load_buffer(const string_t& path, buffer_t&);
+        NANO_PUBLIC bool load_string(const string_t& path, string_t&);
 }
