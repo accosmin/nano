f2e0c3eaee5a480cf2643abd65c4c954cd4ed07b
accosmin <accosmin@gmail.com>
2017-12-19 22:05:44 +0100

    refactor config script to generate .json configurations

--- scripts/config.py
+++ scripts/config.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 class config:
         def __init__(self):
@@ -13,70 +14,49 @@ class config:
                 self.app_stats = crtpath + "/../build-release/apps/stats"
                 self.app_tabulate = crtpath + "/../build-release/apps/tabulate"
 
-        # available losses: {name, command line parameters}+
+        # available losses
         def losses(self):
-                return {
-                        "cauchy" : "--loss cauchy",
-                        "classnll" : "--loss classnll",
-                        "sexponential" : "--loss s-exponential",
-                        "mexponential" : "--loss m-exponential",
-                        "slogistic" : "--loss s-logistic",
-                        "mlogistic" : "--loss m-logistic",
-                        "square" : "--loss square"
-                }
+                return ["cauchy",
+                        "square",
+                        "classnll",
+                        "s-logistic",
+                        "m-logistic",
+                        "s-exponential",
+                        "m-exponential"]
 
-        # configure enhancer with the given name and parameters
-        def config_loss(self, name, parameters = ""):
-                return self.losses().get(name)
-
-        # available enhancers: {name, command line parameters}+
+        # available enhancers
         def enhancers(self):
-                return {
-                        "default" : "--enhancer default",
-                        "noise" : "--enhancer noise",
-                        "warp" : "--enhancer warp",
+                return ["default",
+                        "noise",
+                        "warp",
+                        "noclass"]
+
                         "noclass" : "--enhancer noclass"
                 }
 
-        # configure enhancer with the given name and parameters
-        def config_enhancer(self, name, parameters = "none"):
-                return self.enhancers().get(name) + " --enhancer-params " + parameters
+        # available stochastic solvers
+        def stoch_solvers(self):
+                return ["ag", "agfr", "aggr",
+                        "sg", "sgm", "ngd", "svrg", "asgd",
+                        "adagrad", "adadelta", "adam", "rmsprop"]
 
-        # training methods: {name, command line parameters}+
-        def trainers(self):
-                return {
-                        "stoch_ag" : "--trainer stoch --trainer-params solver=ag",
-                        "stoch_agfr" : "--trainer stoch --trainer-params solver=agfr",
-                        "stoch_aggr" : "--trainer stoch --trainer-params solver=aggr",
-                        "stoch_sg" : "--trainer stoch --trainer-params solver=sg",
-                        "stoch_sgm" : "--trainer stoch --trainer-params solver=sgm",
-                        "stoch_ngd" : "--trainer stoch --trainer-params solver=ngd",
-                        "stoch_svrg" : "--trainer stoch --trainer-params solver=svrg",
-                        "stoch_asgd" : "--trainer stoch --trainer-params solver=asgd",
-                        "stoch_adagrad" : "--trainer stoch --trainer-params solver=adagrad",
-                        "stoch_adadelta" : "--trainer stoch --trainer-params solver=adadelta",
-                        "stoch_adam" : "--trainer stoch --trainer-params solver=adam",
-                        "stoch_rmsprop" : "--trainer stoch --trainer-params solver=rmsprop",
+        # available batch solvers
+        def batch_solvers(self):
+                return ["gd", "cgd", "lbfgs"]
 
-                        "batch_lbfgs" : "--trainer batch --trainer-params solver=lbfgs",
-                        "batch_cgd" : "--trainer batch --trainer-params solver=cgd",
-                        "batch_gd" : "--trainer batch --trainer-params solver=gd"
-                }
+        # configure stochastic trainer
+        def trainer_stoch(self, solver, epochs = 100, patience = 32, epsilon = 1e-6, batch = 32):
+                return json.dumps({
+                        "solver": solver, "epochs": epochs, "patience": patience, "epsilon": epsilon, "batch": batch})
 
-        # configure trainer with the given name and parameters
-        def config_trainer(self, name, parameters = ""):
-                return self.trainers().get(name) + "," + parameters
-
-        # configure synthetic tasks
-        def task_synth(self, name, params):
-                return "--task synth-{0} --task-params {1}".format(name, params)
-
-        def task_synth_charset(self, ctype = "digit", color = "rgb", irows = 16, icols = 16, count = 10000):
-                return self.task_synth("charset", "type={0},color={1},irows={2},icols={3},count={4}".format(ctype, color, irows, icols, count))
+        # configure batch trainer
+        def trainer_batch(self, solver, epochs = 100, patience = 32, epsilon = 1e-6):
+                return json.dumps({
+                        "solver": solver, "epochs": epochs, "patience": patience, "epsilon": epsilon})
 
         # configure tasks
         def task(self, name):
-                return "--task {0} --task-params dir={1}".format(name, self.dbdir + "/" + name)
+                return json.dumps({"task": name, "dir": os.path.join(self.dbdir, name)})
 
         def task_mnist(self):
                 return self.task("mnist")
@@ -89,3 +69,9 @@ class config:
 
         def task_wine(self):
                 return self.task("wine")
+
+        def task_synth_charset(self, ctype = "digit", color = "rgb", irows = 16, icols = 16, count = 10000):
+                return json.dumps({"task": "synth-charset", "type": ctype, "color": color, "irows": irows, "icols": icols, "count": count})
+
+        def task_synth_nparity(self, n = 32, count = 10000):
+                return json.dumps({"task": "synth-nparity", "n": n, "count": count})
