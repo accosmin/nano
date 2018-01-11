b85f31a3a938dd71e73197b16a9f8f332892abf4
accosmin <accosmin@gmail.com>
2018-01-11 21:11:14 +0100

    fix test script on ci

--- scripts/ci_ctest.sh
+++ scripts/ci_ctest.sh
@@ -3,4 +3,4 @@
 cpus=$(./apps/info --sys-logical-cpus)
 cpus=${cpus/*./}
 
-ctest --output-on-failure -j $cpus -E "test_task_iris|test_task_wine|test_task_cifar*|test_task_svhn|test_task_*mnist"
+ctest --output-on-failure -j $cpus -E "test_task_iris|test_task_wine|test_task_cifar|test_task_svhn|test_task_mnist|test_task_fashion"
