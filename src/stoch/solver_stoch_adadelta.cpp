749581a6baa0d4ab7d243fc4ebddf9142ba029e9
accosmin <accosmin@gmail.com>
2018-01-16 21:18:59 +0100

    use a learning rate for AdaDelta (to improve convergence even for very simple problems)

--- src/stoch/solver_stoch_adadelta.cpp
+++ src/stoch/solver_stoch_adadelta.cpp
@@ -7,11 +7,12 @@ strings_t stoch_adadelta_t::configs() const
 {
         strings_t configs;
 
+        for (const auto alpha0 : make_scalars(1e-3, 1e-2, 1e-1, 1e+0))
         for (const auto momentum : make_scalars(0.10, 0.50, 0.90))
-        for (const auto epsilon : make_scalars(1e-2, 1e-4, 1e-6))
+        for (const auto epsilon : make_scalars(1e-2, 1e-4, 1e-6, 1e-8))
         {
                 configs.push_back(json_writer_t().object(
-                        "momentum", momentum, "epsilon", epsilon).str());
+                        "alpha0", alpha0, "momentum", momentum, "epsilon", epsilon).str());
         }
 
         return configs;
@@ -19,12 +20,12 @@ strings_t stoch_adadelta_t::configs() const
 
 json_reader_t& stoch_adadelta_t::config(json_reader_t& reader)
 {
-        return reader.object("momentum", m_momentum, "epsilon", m_epsilon);
+        return reader.object("alpha0", m_alpha0, "momentum", m_momentum, "epsilon", m_epsilon);
 }
 
 json_writer_t& stoch_adadelta_t::config(json_writer_t& writer) const
 {
-        return writer.object("momentum", m_momentum, "epsilon", m_epsilon);
+        return writer.object("alpha0", m_alpha0, "momentum", m_momentum, "epsilon", m_epsilon);
 }
 
 solver_state_t stoch_adadelta_t::minimize(const stoch_params_t& param, const function_t& function, const vector_t& x0) const
@@ -38,18 +39,21 @@ solver_state_t stoch_adadelta_t::minimize(const stoch_params_t& param, const fun
         // assembly the solver
         const auto solver = [&] (solver_state_t& cstate, const solver_state_t&)
         {
+                // learning rate
+                const auto alpha = m_alpha0;
+
                 // descent direction
                 gavg.update(cstate.g.array().square());
 
                 cstate.d = -cstate.g.array() *
-                           (m_epsilon + davg.value().array().sqrt()) /
-                           (m_epsilon + gavg.value().array().sqrt());
+                           (m_epsilon + davg.value().array()).sqrt() /
+                           (m_epsilon + gavg.value().array()).sqrt();
 
                 davg.update(cstate.d.array().square());
 
                 // update solution
                 function.stoch_next();
-                cstate.stoch_update(function, 1);
+                cstate.stoch_update(function, alpha);
         };
 
         const auto snapshot = [&] (const solver_state_t& cstate, solver_state_t& sstate)
