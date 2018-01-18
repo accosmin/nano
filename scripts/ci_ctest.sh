6a40ec6e04b5d1c9a40aafab7b00312755d9c38a
accosmin <accosmin@gmail.com>
2018-01-18 10:38:10 +0100

    towards a self-contained experimentation script

--- scripts/ci_ctest.sh
+++ scripts/ci_ctest.sh
@@ -3,5 +3,5 @@
 cpus=$(./apps/info --sys-logical-cpus)
 cpus=${cpus/*./}
 
-python ../scripts/download_tasks.py --tasks "iris|wine|mnist|fashion-mnist"
+python3 ../scripts/experiment.py --download-tasks "iris|wine|mnist|fashion-mnist"
 ctest --output-on-failure -j $cpus -E "test_task_cifar|test_task_svhn"
