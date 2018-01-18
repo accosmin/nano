0a064f18cda9ae5b08235fec85348e21675936a9
accosmin <accosmin@gmail.com>
2018-01-18 08:43:17 +0100

    fix calling the task downloading script from ci

--- scripts/ci_ctest.sh
+++ scripts/ci_ctest.sh
@@ -3,5 +3,5 @@
 cpus=$(./apps/info --sys-logical-cpus)
 cpus=${cpus/*./}
 
-python download_tasks --tasks "iris|wine|mnist|fashion-mnist"
+python ../scripts/download_tasks.py --tasks "iris|wine|mnist|fashion-mnist"
 ctest --output-on-failure -j $cpus -E "test_task_cifar|test_task_svhn"
