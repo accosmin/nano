f6c3af3b05cc5c75ad1c159e56eb78ac19a87d92
accosmin <accosmin@gmail.com>
2018-01-18 10:46:14 +0100

    install urllib3 on the ci

--- scripts/ci_ctest.sh
+++ scripts/ci_ctest.sh
@@ -3,5 +3,6 @@
 cpus=$(./apps/info --sys-logical-cpus)
 cpus=${cpus/*./}
 
+pip3 install urllib3
 python3 ../scripts/experiment.py --download-tasks "iris|wine|mnist|fashion-mnist"
 ctest --output-on-failure -j $cpus -E "test_task_cifar|test_task_svhn"
