23efed428dccb5bb8b03e0fe235bb2415ba14a11
accosmin <accosmin@gmail.com>
2018-01-24 23:09:24 +0100

    utility to randomly sample a given number of values in a range

--- src/math/random.h
+++ src/math/random.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+#include <vector>
 #include <random>
 #include <limits>
 #include <cassert>
@@ -10,7 +12,7 @@ namespace nano
         ///
         /// \brief uniform random number generator in the [min, max] range.
         ///
-        template <typename tscalar, typename tvalid = typename std::is_arithmetic<tscalar>::type>
+        template <typename tscalar, typename = typename std::is_arithmetic<tscalar>::type>
         struct random_t
         {
                 using result_type = tscalar;
@@ -75,9 +77,9 @@ namespace nano
         };
 
         ///
-        /// \brief create a random number generator in the given [min, max] range
+        /// \brief create a random number generator in the given [min, max] range.
         ///
-        template <typename tscalar, typename tvalid = typename std::is_arithmetic<tscalar>::type>
+        template <typename tscalar, typename = typename std::is_arithmetic<tscalar>::type>
         random_t<tscalar> make_rng(
                 const tscalar min = std::numeric_limits<tscalar>::lowest(),
                 const tscalar max = std::numeric_limits<tscalar>::max())
@@ -86,12 +88,29 @@ namespace nano
         }
 
         ///
-        /// \brief create a random index generator in the given [0, size) range
+        /// \brief create a random index generator in the given [0, size) range.
         ///
-        template <typename tsize, typename tvalid = typename std::is_unsigned<tsize>::type>
+        template <typename tsize, typename = typename std::is_unsigned<tsize>::type>
         random_t<tsize> make_index_rng(const tsize size)
         {
                 assert(size > 0);
                 return random_t<tsize>(0, size - 1);
+        }
+
+        ///
+        /// \brief create a set of uniformly distributed values in the [base^min, base^max) range.
+        ///
+        template <typename tscalar, typename = typename std::is_floating_point<tscalar>::type>
+        std::vector<tscalar> get_random_values(const size_t count, const tscalar min, const tscalar max, const tscalar base = 1)
+        {
+                auto rng = make_rng(min, max);
+
+                std::vector<tscalar> values(count);
+                for (auto& value : values)
+                {
+                        value = std::pow(base, rng());
+                }
+
+                return values;
         }
 }
