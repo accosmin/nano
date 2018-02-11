5d6202d12631eae3c45fad38b82bfd41774b6912
accosmin <accosmin@gmail.com>
2018-02-11 11:29:48 +0100

    more robust NGD

--- src/stoch/solver_stoch_ngd.cpp
+++ src/stoch/solver_stoch_ngd.cpp
@@ -1,3 +1,4 @@
+#include "lrate.h"
 #include "solver_stoch_ngd.h"
 
 using namespace nano;
@@ -6,26 +7,30 @@ tuner_t stoch_ngd_t::configs() const
 {
         tuner_t tuner;
         tuner.add_base10("alpha0", -6, -1);
+        tuner.add_linear("decay", 0, 1);
         return tuner;
 }
 
 json_reader_t& stoch_ngd_t::config(json_reader_t& reader)
 {
-        return reader.object("alpha0", m_alpha0);
+        return reader.object("alpha0", m_alpha0, "decay", m_decay);
 }
 
 json_writer_t& stoch_ngd_t::config(json_writer_t& writer) const
 {
-        return writer.object("alpha0", m_alpha0);
+        return writer.object("alpha0", m_alpha0, "decay", m_decay);
 }
 
 solver_state_t stoch_ngd_t::minimize(const stoch_params_t& param, const function_t& function, const vector_t& x0) const
 {
+        // learning rate schedule
+        lrate_t lrate(m_alpha0, m_decay, param.m_epoch_size);
+
         // assembly the solver
         const auto solver = [&] (solver_state_t& cstate, const solver_state_t&)
         {
                 // learning rate
-                const scalar_t alpha = m_alpha0;
+                const scalar_t alpha = lrate.get();
 
                 // descent direction
                 const scalar_t norm = 1 / cstate.g.template lpNorm<2>();
