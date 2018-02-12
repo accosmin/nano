fc7dcf00b93f7ab7ddd3795d3944e5e0530fe31d
accosmin <accosmin@gmail.com>
2018-02-12 23:28:52 +0100

    fix tuning of the stochastic trainer & tune the minibatch size

--- src/trainers/trainer_stoch.cpp
+++ src/trainers/trainer_stoch.cpp
@@ -9,11 +9,62 @@
 
 using namespace nano;
 
+static trainer_result_t train(const task_t& task, const size_t fold, accumulator_t& acc,
+        const rstoch_solver_t& solver, const string_t& config,
+        const size_t epochs, const scalar_t epsilon, const size_t patience,
+        const timer_t& timer, const bool tuning)
+{
+        assert(solver);
+
+        size_t epoch = 0;
+        trainer_result_t result(config);
+
+        // setup solver
+        solver->config(config);
+
+        // setup minibatch iterator
+        size_t batch = 32;
+        json_reader_t(config).object("batch", batch);
+        auto iterator = iterator_t(task, {fold, protocol::train}, batch);
+
+        // logging operator
+        const auto fn_ulog = [&] (const solver_state_t& state)
+        {
+                // evaluate the current state
+                // NB: the training state is already estimated!
+                const auto train = measure(acc);
+                const auto valid = measure(state.x, task, {fold, protocol::valid}, acc);
+                const auto test  = tuning ? valid : measure(state.x, task, {fold, protocol::test}, acc);
+
+                // OK, update the optimum solution
+                const auto milis = timer.milliseconds();
+                const auto xnorm = state.x.lpNorm<2>();
+                const auto gnorm = state.convergence_criteria();
+                const auto ret = result.update(state, {milis, ++epoch, xnorm, gnorm, train, valid, test}, patience);
+
+                log_info() << std::setprecision(3)
+                        << (tuning ? "tune[" : "[") << epoch << "/" << epochs
+                        << ":train=" << train
+                        << ",valid=" << valid << "|" << nano::to_string(ret)
+                        << "," << config << ",g=" << gnorm << ",x=" << xnorm
+                        << "]" << timer.elapsed() << ".";
+
+                return !nano::is_done(ret);
+        };
+
+        // assembly optimization function & train the model
+        const auto function = stoch_function_t(acc, task, iterator);
+        const auto params = stoch_params_t{epochs, epoch_size(task, fold, batch), epsilon, fn_ulog};
+        solver->minimize(params, function, acc.params());
+
+        // OK
+        return result;
+}
+
 json_reader_t& stoch_trainer_t::config(json_reader_t& reader)
 {
         return reader.object("solver", m_solver,
                 "tune_epochs", m_tune_epochs, "epochs", m_epochs,
-                "batch", m_batch, "batch_ratio", m_batch_ratio,
                 "epsilon", m_epsilon, "patience", m_patience);
 }
 
@@ -21,81 +72,35 @@ json_writer_t& stoch_trainer_t::config(json_writer_t& writer) const
 {
         return writer.object("solver", m_solver, "solvers", join(get_stoch_solvers().ids()),
                 "tune_epochs", m_tune_epochs, "epochs", m_epochs,
-                "batch", m_batch, "batch_ratio", m_batch_ratio,
                 "epsilon", m_epsilon, "patience", m_patience);
-}
-
-size_t stoch_trainer_t::epoch_size(const task_t& task, const size_t fold) const
-{
-        const auto train_size = task.size({fold, protocol::train});
-        return idiv(train_size, m_batch);
 }
 
 void stoch_trainer_t::tune(const task_t& task, const size_t fold, accumulator_t& acc)
 {
-        const auto solver = get_stoch_solvers().get(m_solver);
-        if (!solver)
-        {
-                assert(solver);
-                log_error() << "unknown solver [" << m_solver << "]!";
-                return;
-        }
-
         const timer_t timer;
         trainer_result_t opt_result;
-        const auto param0 = acc.params();
+
+        const auto cores = physical_cpus();
+        const auto params = acc.params();
+        const auto solver = get_stoch_solvers().get(m_solver);
 
         auto tuner = solver->configs();
+        tuner.add_finite("batch", make_scalars(1 * cores, 2 * cores, 4 * cores, 8 * cores, 16 * cores, 32 * cores));
         const auto trials = 10 * tuner.n_params();
 
-        // tune the hyper-parameters
-        // todo: also tune the batch factor (that geometrically increases the minibatch size)
-        // todo: also tune the L2-regularization term
+        // tune the hyper-parameters: solver + minibatch
         for (size_t trial = 0; trial < trials; ++ trial)
         {
+                acc.params(params);
                 const auto config = tuner.get();
-
-                // minibatch iterator
-                auto iterator = iterator_t(task, {fold, protocol::train}, m_batch, m_batch_ratio);
-
-                size_t epoch = 0;
-                trainer_result_t result(config);
-
-                // logging operator
-                const auto fn_ulog = [&] (const solver_state_t& state)
-                {
-                        // evaluate the current state
-                        // NB: the training state is already estimated!
-                        const auto train = measure(acc);
-                        const auto valid = measure(state.x, task, {fold, protocol::valid}, acc);
-                        const auto test  = valid;
-
-                        // OK, update the optimum solution
-                        const auto milis = timer.milliseconds();
-                        const auto xnorm = state.x.lpNorm<2>();
-                        const auto gnorm = state.convergence_criteria();
-                        const auto ret = result.update(state, {milis, ++epoch, xnorm, gnorm, train, valid, test}, m_patience);
-
-                        log_info() << std::setprecision(3)
-                                << "tune[" << epoch << "/" << m_tune_epochs
-                                << ":train=" << train
-                                << ",valid=" << valid << "|" << nano::to_string(ret)
-                                << "," << config << ",batch=" << iterator.size() << ",g=" << gnorm << ",x=" << xnorm
-                                << "]" << timer.elapsed() << ".";
-
-                        return !nano::is_done(ret);
-                };
-
-                // assembly optimization function & train the model
-                const auto function = stoch_function_t(acc, task,  iterator);
-                const auto params = stoch_params_t{m_tune_epochs, epoch_size(task, fold), m_epsilon, fn_ulog};
-                solver->minimize(params, function, param0);
+                const auto epochs = m_tune_epochs;
+                const auto result = ::train(task, fold, acc, solver, config, epochs, m_epsilon, m_patience, timer, true);
 
                 // check if an improvement
                 if (result < opt_result)
                 {
                         opt_result = result;
-                        m_solver_config = config;
+                        m_tuned_config = config;
                 }
         }
 
@@ -106,55 +111,15 @@ void stoch_trainer_t::tune(const task_t& task, const size_t fold, accumulator_t&
 
 trainer_result_t stoch_trainer_t::train(const task_t& task, const size_t fold, accumulator_t& acc) const
 {
-        // minibatch iterator
-        auto iterator = iterator_t(task, {fold, protocol::train}, m_batch);
-
         const timer_t timer;
 
-        size_t epoch = 0;
-        trainer_result_t result(m_solver_config);
-
-        // logging operator
-        const auto fn_ulog = [&] (const solver_state_t& state)
-        {
-                // evaluate the current state
-                // NB: the training state is already estimated!
-                const auto train = measure(acc);
-                const auto valid = measure(state.x, task, {fold, protocol::valid}, acc);
-                const auto test  = measure(state.x, task, {fold, protocol::test}, acc);
-
-                // OK, update the optimum solution
-                const auto milis = timer.milliseconds();
-                const auto xnorm = state.x.lpNorm<2>();
-                const auto gnorm = state.convergence_criteria();
-                const auto ret = result.update(state, {milis, ++epoch, xnorm, gnorm, train, valid, test}, m_patience);
-
-                log_info() << std::setprecision(3)
-                        << "[" << epoch << "/" << m_epochs
-                        << ":train=" << train
-                        << ",valid=" << valid << "|" << nano::to_string(ret)
-                        << ",test=" << test
-                        << "," << m_solver_config << ",batch=" << iterator.size() << ",g=" << gnorm << ",x=" << xnorm
-                        << "]" << timer.elapsed() << ".";
-
-                return !nano::is_done(ret);
-        };
-
-        // assembly optimization function & train the model
-        const auto function = stoch_function_t(acc, task,  iterator);
-        const auto params = stoch_params_t{m_epochs, epoch_size(task, fold), m_epsilon, fn_ulog};
         const auto solver = get_stoch_solvers().get(m_solver);
-        if (!solver)
-        {
-                assert(solver);
-                log_error() << "unknown solver [" << m_solver << "]!";
-        }
-        else
-        {
-                solver->minimize(params, function, acc.params());
-        }
+        const auto config = m_tuned_config;
+        const auto epochs = m_epochs;
+        const auto result = ::train(task, fold, acc, solver, config, epochs, m_epsilon, m_patience, timer, false);
 
         assert(result);
-        log_info() << std::setprecision(3) << "<<< stoch-" << m_solver << ": " << result << "," << timer.elapsed() << ".";
+        log_info() << std::setprecision(3)
+                << "<<< stoch-" << m_solver << ": " << result << "," << timer.elapsed() << ".";
         return result;
 }
--- src/trainers/trainer_stoch.h
+++ src/trainers/trainer_stoch.h
@@ -19,16 +19,12 @@ namespace nano
 
         private:
 
-                size_t epoch_size(const task_t&, const size_t fold) const;
-
                 // attributes
                 string_t        m_solver{"sg"};
                 size_t          m_tune_epochs{8};
                 size_t          m_epochs{128};
-                size_t          m_batch{32};
-                scalar_t        m_batch_ratio{1};
                 size_t          m_patience{32};
                 scalar_t        m_epsilon{static_cast<scalar_t>(1e-6)};
-                string_t        m_solver_config;        ///< tuned hyper-parameters for the solver
+                string_t        m_tuned_config;         ///< tuned hyper-parameters (solver + minibatch)
         };
 }
