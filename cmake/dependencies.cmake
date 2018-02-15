19bdfb9869c202d94b359f436f21612385c97a14
accosmin <accosmin@gmail.com>
2018-02-15 22:08:57 +0100

    use eigen3 as submodule

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -12,4 +12,4 @@ include_directories(SYSTEM ${LibArchive_INCLUDE_DIRS})
 
 # Eigen
 find_package(Eigen3 3.3 REQUIRED)
-include_directories(${EIGEN3_INCLUDE_DIR})
+include_directories(${EIGEN3_INCLUDE_DIR} ${EIGEN3_INCLUDE_DIR}/../)
