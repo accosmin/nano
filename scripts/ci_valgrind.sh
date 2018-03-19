914c890c4d9993aa49f8e3883292d1eb8a232c82
accosmin <accosmin@gmail.com>
2018-03-19 19:44:18 +0100

    fix valgrind integration

--- scripts/ci_valgrind.sh
+++ scripts/ci_valgrind.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 valgrind --version
+python3 ../scripts/downloader.py --tasks "iris|wine|mnist|fashion-mnist"
 ctest --output-on-failure -T memcheck -E "test_task_cifar|test_task_svhn"
