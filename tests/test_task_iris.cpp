7becc220e17ab8de516911887b78652741dacf2a
accosmin <accosmin@gmail.com>
2018-04-13 21:54:18 +0200

    extend unit test to cover multiple folds for the iris & wine tasks

--- tests/test_task_iris.cpp
+++ tests/test_task_iris.cpp
@@ -22,11 +22,8 @@ NANO_CASE(loading)
         const auto odims = tensor3d_dim_t{3, 1, 1};
         const auto target_sum = scalar_t(2) - static_cast<scalar_t>(nano::size(odims));
 
-        const auto folds = size_t(1);
+        const auto folds = size_t(10);
         const auto samples = size_t(150);
-        const auto train_fold = fold_t{0, protocol::train};
-        const auto valid_fold = fold_t{0, protocol::valid};
-        const auto test_fold = fold_t{0, protocol::test};
 
         const auto task = nano::get_tasks().get("iris");
         NANO_REQUIRE(task);
@@ -39,37 +36,54 @@ NANO_CASE(loading)
 
         // check folds
         NANO_CHECK_EQUAL(task->fsize(), folds);
-        NANO_CHECK_EQUAL(task->size(), samples);
+        NANO_CHECK_EQUAL(task->size(), folds * samples);
 
-        NANO_CHECK_GREATER(task->size(train_fold), 5 * samples / 10);
-        NANO_CHECK_GREATER(task->size(valid_fold), samples / 10);
-        NANO_CHECK_GREATER(task->size(test_fold), samples / 10);
-        NANO_CHECK_EQUAL(task->size(train_fold) + task->size(valid_fold) + task->size(test_fold), samples);
+        for (size_t f = 0; f < folds; ++ f)
+        {
+                const auto train_fold = fold_t{f, protocol::train};
+                const auto valid_fold = fold_t{f, protocol::valid};
+                const auto test_fold = fold_t{f, protocol::test};
+
+                NANO_CHECK_GREATER(task->size(train_fold), 5 * samples / 10);
+                NANO_CHECK_GREATER(task->size(valid_fold), 1 * samples / 10);
+                NANO_CHECK_GREATER(task->size(test_fold),  1 * samples / 10);
+
+                NANO_CHECK_EQUAL(task->size(train_fold) + task->size(valid_fold) + task->size(test_fold), samples);
+        }
 
         // check samples
-        std::set<string_t> labels;
-        for (const auto fold : {train_fold, valid_fold, test_fold})
+        for (size_t f = 0; f < folds; ++ f)
         {
-                const auto size = task->size(fold);
-                for (size_t i = 0; i < size; ++ i)
+                const auto train_fold = fold_t{f, protocol::train};
+                const auto valid_fold = fold_t{f, protocol::valid};
+                const auto test_fold = fold_t{f, protocol::test};
+
+                for (const auto fold : {train_fold, valid_fold, test_fold})
                 {
-                        const auto sample = task->get(fold, i, i + 1);
-                        const auto& input = sample.idata(0);
-                        const auto& label = sample.label(0);
-                        const auto& target = sample.odata(0);
+                        std::set<string_t> labels;
 
-                        NANO_CHECK_EQUAL(input.dims(), idims);
-                        NANO_CHECK_EQUAL(target.dims(), odims);
-                        NANO_CHECK_CLOSE(target.vector().sum(), target_sum, epsilon0<scalar_t>());
+                        const auto size = task->size(fold);
+                        for (size_t i = 0; i < size; ++ i)
+                        {
+                                const auto sample = task->get(fold, i, i + 1);
+                                const auto& input = sample.idata(0);
+                                const auto& label = sample.label(0);
+                                const auto& target = sample.odata(0);
 
-                        labels.insert(label);
+                                NANO_CHECK_EQUAL(input.dims(), idims);
+                                NANO_CHECK_EQUAL(target.dims(), odims);
+                                NANO_CHECK_CLOSE(target.vector().sum(), target_sum, epsilon0<scalar_t>());
+
+                                labels.insert(label);
+                        }
+
+                        NANO_CHECK_EQUAL(labels.size(), static_cast<size_t>(nano::size(odims)));
                 }
         }
 
         const size_t max_duplicates = 0;
         NANO_CHECK_LESS_EQUAL(nano::check_duplicates(*task), max_duplicates);
         NANO_CHECK_LESS_EQUAL(nano::check_intersection(*task), max_duplicates);
-        NANO_CHECK_EQUAL(labels.size(), static_cast<size_t>(nano::size(odims)));
 }
 
 NANO_END_MODULE()
