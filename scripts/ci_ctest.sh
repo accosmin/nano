86297518f7d851fa355079b932e7f735e5f12968
accosmin <accosmin@gmail.com>
2018-05-25 21:42:00 +0200

    run unit tests for cifar as well

--- scripts/ci_ctest.sh
+++ scripts/ci_ctest.sh
@@ -3,5 +3,5 @@
 cpus=$(./apps/info --sys-logical-cpus)
 cpus=${cpus/*./}
 
-python3 ../scripts/downloader.py --tasks "iris|wine|mnist|fashion-mnist"
+python3 ../scripts/downloader.py --tasks "iris|wine|mnist|fashion-mnist|cifar10|cifar100"
 ctest --output-on-failure -j $cpus -E "test_task_cifar|test_task_svhn|test_trainer|test_solver"
