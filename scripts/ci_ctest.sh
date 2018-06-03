f7cef2399c8ff470a8ef8304e791cc983ecd19f9
accosmin <accosmin@gmail.com>
2018-06-03 18:50:03 +0200

    fix test script on Travis & Apple

--- scripts/ci_ctest.sh
+++ scripts/ci_ctest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cpus=$(nproc)
+cpus=$(./apps/info --sys-logical-cpus)
+cpus=${cpus/*./}
+#cpus=$(nproc)
 
 bash ../scripts/download_tasks.sh --iris --wine --mnist --fashion-mnist --cifar10 --cifar100
 ctest --output-on-failure -j $cpus -E "test_task_svhn|test_trainer|test_solver"
