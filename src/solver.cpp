f87d8d909c52fa67ca66d2dc7573bd5cb6730d8f
accosmin <accosmin@gmail.com>
2018-09-02 22:51:34 +0200

    remove lpnorm-approximation test function

--- src/solver.cpp
+++ src/solver.cpp
@@ -15,19 +15,19 @@ solver_factory_t& nano::get_solvers()
         std::call_once(flag, [] ()
         {
                 manager.add<solver_gd_t>("gd", "gradient descent");
-                manager.add<solver_nag_t>("nag", "Nesterov's accelerated gradient");
-                manager.add<solver_nagfr_t>("nagfr", "Nesterov's accelerated gradient with function value restarts");
-                manager.add<solver_naggr_t>("naggr", "Nesterov's accelerated gradient with gradient restarts");
-                manager.add<solver_cgd_prp_t>("cgd", "nonlinear conjugate gradient descent (default)");
-                manager.add<solver_cgd_n_t>("cgd-n", "nonlinear conjugate gradient descent (N)");
-                manager.add<solver_cgd_hs_t>("cgd-hs", "nonlinear conjugate gradient descent (HS)");
-                manager.add<solver_cgd_fr_t>("cgd-fr", "nonlinear conjugate gradient descent (FR)");
-                manager.add<solver_cgd_prp_t>("cgd-prp", "nonlinear conjugate gradient descent (PRP+)");
-                manager.add<solver_cgd_cd_t>("cgd-cd", "nonlinear conjugate gradient descent (CD)");
-                manager.add<solver_cgd_ls_t>("cgd-ls", "nonlinear conjugate gradient descent (LS)");
-                manager.add<solver_cgd_dy_t>("cgd-dy", "nonlinear conjugate gradient descent (DY)");
-                manager.add<solver_cgd_dycd_t>("cgd-dycd", "nonlinear conjugate gradient descent (DYCD)");
-                manager.add<solver_cgd_dyhs_t>("cgd-dyhs", "nonlinear conjugate gradient descent (DYHS)");
+                manager.add<solver_nag_t>("nag", "Nesterov's accelerated gradient (NAG)");
+                manager.add<solver_nagfr_t>("nagfr", "NAG with function value restarts");
+                manager.add<solver_naggr_t>("naggr", "NAG with gradient restarts");
+                manager.add<solver_cgd_prp_t>("cgd", "conjugate gradient descent (default)");
+                manager.add<solver_cgd_n_t>("cgd-n", "conjugate gradient descent (N)");
+                manager.add<solver_cgd_hs_t>("cgd-hs", "conjugate gradient descent (HS)");
+                manager.add<solver_cgd_fr_t>("cgd-fr", "conjugate gradient descent (FR)");
+                manager.add<solver_cgd_prp_t>("cgd-prp", "conjugate gradient descent (PRP+)");
+                manager.add<solver_cgd_cd_t>("cgd-cd", "conjugate gradient descent (CD)");
+                manager.add<solver_cgd_ls_t>("cgd-ls", "conjugate gradient descent (LS)");
+                manager.add<solver_cgd_dy_t>("cgd-dy", "conjugate gradient descent (DY)");
+                manager.add<solver_cgd_dycd_t>("cgd-dycd", "conjugate gradient descent (DYCD)");
+                manager.add<solver_cgd_dyhs_t>("cgd-dyhs", "conjugate gradient descent (DYHS)");
                 manager.add<solver_lbfgs_t>("lbfgs", "limited-memory BFGS");
                 manager.add<solver_quasi_dfp_t>("dfp", "quasi-newton method (DFP)");
                 manager.add<solver_quasi_sr1_t>("sr1", "quasi-newton method (SR1)");
