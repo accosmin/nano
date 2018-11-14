124080c6c6c3a0f70f1df726dc30a1601ce9770a
accosmin <accosmin@gmail.com>
2018-11-14 15:27:53 +0100

    enable testing of cal-housing task on travis

--- scripts/ci_ctest.sh
+++ scripts/ci_ctest.sh
@@ -4,5 +4,5 @@ cpus=$(./apps/info --sys-logical-cpus)
 cpus=${cpus/*./}
 #cpus=$(nproc)
 
-bash ../scripts/download_tasks.sh --iris --wine --mnist --fashion-mnist --cifar10 --cifar100
-ctest --output-on-failure -j $cpus -E "test_task_svhn|test_task_cal_housing"
+bash ../scripts/download_tasks.sh --iris --wine --mnist --fashion-mnist --cifar10 --cifar100 --cal-housing
+ctest --output-on-failure -j $cpus -E "test_task_svhn"
