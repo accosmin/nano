8411a9d9a2f9fa51c0caec6e4bf87894580b7889
accosmin <accosmin@gmail.com>
2018-12-03 22:31:14 +0100

    extend unit test for stumps

--- tests/test_stump.cpp
+++ tests/test_stump.cpp
@@ -5,7 +5,23 @@ using namespace nano;
 
 NANO_BEGIN_MODULE(test_stump)
 
-NANO_CASE(test0)
+NANO_CASE(getset)
+{
+        stump_t stump;
+        stump.feature(3);
+        stump.threshold(scalar_t(-2.5));
+
+        tensor4d_t outputs(2, 3, 1, 1);
+        outputs.tensor(0).constant(-1);
+        outputs.tensor(1).constant(+1);
+        stump.outputs(outputs);
+
+        NANO_CHECK_EQUAL(stump.feature(), 3);
+        NANO_CHECK_EQUAL(stump.threshold(), scalar_t(-2.5));
+        NANO_CHECK_EIGEN_CLOSE(stump.outputs().array(), outputs.array(), epsilon1<scalar_t>());
+}
+
+NANO_CASE(output)
 {
         tensor4d_t inputs(3, 1, 2, 3);
         inputs.vector(0) = vector_t::LinSpaced(6, -2, +3);
@@ -30,13 +46,8 @@ NANO_CASE(test0)
         NANO_CHECK_EQUAL(stump.output(inputs.tensor(2))(0), -1);
 }
 
-NANO_CASE(test1)
+NANO_CASE(scale)
 {
-        tensor4d_t inputs(3, 1, 2, 3);
-        inputs.vector(0) = vector_t::LinSpaced(6, -2, +3);
-        inputs.vector(1) = vector_t::LinSpaced(6, -3, +2);
-        inputs.vector(2) = vector_t::LinSpaced(6, -4, +1);
-
         stump_t stump;
         stump.feature(0);
         stump.threshold(scalar_t(-3.5));
@@ -46,16 +57,11 @@ NANO_CASE(test1)
         outputs.tensor(1).constant(+1);
         stump.outputs(outputs);
 
-        NANO_CHECK_EQUAL(stump.output(inputs.tensor(0)).size(), 3);
-        NANO_CHECK_EQUAL(stump.output(inputs.tensor(1)).size(), 3);
-        NANO_CHECK_EQUAL(stump.output(inputs.tensor(2)).size(), 3);
+        stump.scale(scalar_t(0.3));
 
-        NANO_CHECK_EQUAL(stump.output(inputs.tensor(0))(0), +1);
-        NANO_CHECK_EQUAL(stump.output(inputs.tensor(1))(0), +1);
-        NANO_CHECK_EQUAL(stump.output(inputs.tensor(2))(0), -1);
+        NANO_CHECK_EIGEN_CLOSE(stump.outputs().array(), outputs.array() * scalar_t(0.3), epsilon1<scalar_t>());
 }
 
-// todo: check stump_t::scale, stump_t::feature|threshold|outputs
 // todo: check stump_t::fit
 
 NANO_END_MODULE()
