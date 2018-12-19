367e6c0ce47029eaf7081bc09a70d8b0a71f5b55
accosmin <accosmin@gmail.com>
2018-12-19 09:51:00 +0100

    show valgring output only when errors are present

--- scripts/ci_valgrind.sh
+++ scripts/ci_valgrind.sh
@@ -3,6 +3,8 @@
 valgrind --version
 
 bash ../scripts/download_tasks.sh --iris --wine --mnist --fashion-mnist --cal-housing
+
+# NB: not using ctest directly because I cannot pass options to memcheck!
 #ctest --output-on-failure -T memcheck -E "test_task_cifar|test_task_svhn"
 
 exitcode=0
@@ -10,11 +12,17 @@ exitcode=0
 utests=$(ls tests/test_* | grep -v test_task_cifar | grep -v test_task_svhn)
 for utest in ${utests}
 do
-        valgrind --leak-check=full --track-origins=yes --error-exitcode=1 ${utest}
+        printf "Running %s ...\n" ${utest}
+        log=${utest/tests\//}.log
+        valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=50 --error-exitcode=1 \
+                --log-file=${log} ${utest}
+
         if [[ $? -gt 0 ]]
         then
+                cat ${log}
                 exitcode=1
         fi
+        printf "\n"
 done
 
 exit ${exitcode}
