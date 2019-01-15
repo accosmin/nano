aa65a2f5b15f11f5f60a11dd5014896845534dad
accosmin <accosmin@gmail.com>
2019-01-15 23:13:23 +0100

    simple tests for solver_state_t

--- tests/test_solver_state.cpp
+++ tests/test_solver_state.cpp
@@ -1,10 +1,9 @@
 #include <utest/utest.h>
-#include "solver.h"
-#include "core/numeric.h"
+#include "solvers/state.h"
+#include "functions/sphere.h"
 
 using namespace nano;
 
-// todo: check descent direction
 // todo: check convergence criterion
 // todo: check armijo condition
 // todo: check wolfe conditions
@@ -13,8 +12,59 @@ using namespace nano;
 
 UTEST_BEGIN_MODULE(test_solver_state)
 
-UTEST_CASE(state_descent)
+UTEST_CASE(state_valid1)
 {
+        solver_state_t state(11);
+        UTEST_CHECK(state);
+}
+
+UTEST_CASE(state_valid2)
+{
+        function_sphere_t function(7);
+
+        solver_state_t state(function, vector_t::Random(function.size()));
+        UTEST_CHECK(state);
+}
+
+UTEST_CASE(state_invalid1)
+{
+        solver_state_t state(11);
+        state.t = INFINITY;
+        UTEST_CHECK(!state);
+}
+
+UTEST_CASE(state_invalid2)
+{
+        solver_state_t state(11);
+        state.f = NAN;
+        UTEST_CHECK(!state);
+}
+
+UTEST_CASE(state_has_descent)
+{
+        function_sphere_t function(7);
+
+        solver_state_t state(function, vector_t::Random(function.size()));
+        state.d = -state.g;
+        UTEST_CHECK(state.has_descent());
+}
+
+UTEST_CASE(state_has_no_descent1)
+{
+        function_sphere_t function(7);
+
+        solver_state_t state(function, vector_t::Random(function.size()));
+        state.d.setZero();
+        UTEST_CHECK(!state.has_descent());
+}
+
+UTEST_CASE(state_has_no_descent1)
+{
+        function_sphere_t function(7);
+
+        solver_state_t state(function, vector_t::Random(function.size()));
+        state.d = state.g;
+        UTEST_CHECK(!state.has_descent());
 }
 
 UTEST_CASE(state_convergence)
